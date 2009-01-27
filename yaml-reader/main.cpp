276892778219cb7903fd277cd54940ff11787105
Jesse Beder <jbeder+github@gmail.com>
2009-01-27 20:16:30 +0000

    Included <cstring> for strcmp

--- yaml-reader/main.cpp
+++ yaml-reader/main.cpp
@@ -2,6 +2,7 @@
 #include "tests.h"
 #include <fstream>
 #include <iostream>
+#include <cstring>
 
 void run()
 {
@@ -27,7 +28,7 @@ int main(int argc, char **argv)
 {
 	bool verbose = false;
 	for(int i=1;i<argc;i++) {
-		if(strcmp(argv[i], "-v") == 0)
+		if(std::strcmp(argv[i], "-v") == 0)
 			verbose = true;
 	}
 
