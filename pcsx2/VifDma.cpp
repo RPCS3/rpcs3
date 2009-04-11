a388d2969c172b9266309e665d800f8148969ba6
refraction <refraction@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-04-11 13:23:05 +0000

    Fixed bug from Issue 144.
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@954 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/VifDma.cpp
+++ pcsx2/VifDma.cpp
@@ -607,7 +607,7 @@ static void VIFunpack(u32 *data, vifCode *v, int size, const unsigned int VIFdma
 			if ((unpackType == 0xC) && (vifRegs->cycle.cl == vifRegs->cycle.wl))  //No use when SSE is available
 			{
 				// v4-32
-				if ((vifRegs->mode == 0) && !(vifRegs->code & 0x10000000) && (vif->usn == 0))
+				if (v->size == (size >> 2) && (vifRegs->mode == 0) && !(vifRegs->code & 0x10000000) && (vif->usn == 0))
 				{
 					vifRegs->num -= size >> 4;
 					memcpy_fast((u8*)dest, cdata, size);
