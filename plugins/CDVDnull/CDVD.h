6cc85d6050568f29e6a7c83a730caf85b9841fc7
arcum42 <arcum42@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-05-02 04:12:29 +0000

    And that's CDVDnull. Given how barebones this plugin is, it's closer to making a null plugin then cleaning it up...
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1110 96395faa-99c1-11dd-bbfe-3dabce05a288

---
+++ plugins/CDVDnull/CDVD.h
@@ -0,0 +1,52 @@
+/*  CDVDnull
+ *  Copyright (C) 2002-2009  Pcsx2 Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+ #ifndef __CDVD_H__
+#define __CDVD_H__
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+extern "C"
+{
+#define CDVDdefs
+#include "PS2Edefs.h"
+}
+
+#ifdef __LINUX__
+#include <gtk/gtk.h>
+#else
+#include <windows.h>
+#include <windowsx.h>
+#endif
+
+#ifdef _MSC_VER
+#define EXPORT_C_(type) extern "C" __declspec(dllexport) type CALLBACK
+#else
+#define EXPORT_C_(type) extern "C" type
+#endif
+
+extern const unsigned char version;
+extern const unsigned char revision;
+extern const unsigned char build;
+extern const unsigned int minor;
+extern const char *LibName;
+
+extern void SysMessage(const char *fmt, ...);
+#endif /* __CDVD_H__ */
