465f35169f96c5e157a76058070ed12f19243d62
arcum42 <arcum42@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-05-02 04:39:05 +0000

    And that's dev9, which was about as barebones as CDVDnull.
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1111 96395faa-99c1-11dd-bbfe-3dabce05a288

---
+++ plugins/dev9null/DEV9.h
@@ -0,0 +1,39 @@
+#ifndef __DEV9_H__
+#define __DEV9_H__
+
+#include <stdio.h>
+
+#ifdef __LINUX__
+#include <gtk/gtk.h>
+#else
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#endif
+
+extern "C"
+{
+#define DEV9defs
+#include "PS2Edefs.h"
+}
+
+#ifdef _MSC_VER
+#define EXPORT_C_(type) extern "C" __declspec(dllexport) type CALLBACK
+#else
+#define EXPORT_C_(type) extern "C" type
+#endif
+
+extern const unsigned char version;
+extern const unsigned char revision;
+extern const unsigned char build;
+extern const unsigned int minor;
+extern const char *libraryName;
+
+extern void (*DEV9irq)();
+extern void SysMessage(const char *fmt, ...);
+
+extern FILE *dev9Log;
+void __Log(char *fmt, ...);
+
+
+#endif
