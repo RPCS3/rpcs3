366357228dd3f8dbda2889a6c4f88d1ac3e805b9
Jake.Stine <Jake.Stine@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-07-13 02:14:09 +0000

    Bugfixed thread affinity restoration during CpuSpeed detection; pcsx2 should use all your cores properly again. :)
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1502 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/x86/ix86/ix86_cpudetect.cpp
+++ pcsx2/x86/ix86/ix86_cpudetect.cpp
@@ -201,7 +201,7 @@ static void SetSingleAffinity()
 	}
 
 	HANDLE s_threadId = GetCurrentThread();
-	DWORD s_oldmask = SetThreadAffinityMask( s_threadId, (1UL<<i) );
+	s_oldmask = SetThreadAffinityMask( s_threadId, (1UL<<i) );
 
 	if( s_oldmask == ERROR_INVALID_PARAMETER )
 	{
