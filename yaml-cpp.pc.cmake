ff6e6bdfd50eb47759f3012093d3aabf04d4ee09
Jesse Beder <jbeder+github@gmail.com>
2009-07-15 20:37:11 +0000

    Added support for pkgconfig

---
+++ yaml-cpp.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@LIB_INSTALL_DIR@
+includedir=@INCLUDE_INSTALL_DIR@
+
+Name: Yaml-cpp
+Description: A YAML parser for C++
+Version: @YAML_CPP_VERSION@
+Requires:
+Libs: -L${libdir} -lyaml-cpp
+Cflags: -I${includedir}
