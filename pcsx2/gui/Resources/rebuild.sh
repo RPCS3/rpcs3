ce0e90e8e6c73f12c36009a3aabd7a937620e669
arcum42 <arcum42@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-09-07 00:50:36 +0000

    Correct the script, so that Linux is compilable. (If rebuild.sh is run before compiling.)
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1767 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/gui/Resources/rebuild.sh
+++ pcsx2/gui/Resources/rebuild.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-../../../Tools/bin2cpp/bin2cpp BackgroundLogo.png        BackgroundLogo        png_BackgroundLogo
-../../../Tools/bin2cpp/bin2cpp AppIcon.png               AppIcon               png_AppIcon
+../../../Tools/bin2cpp/bin2cpp BackgroundLogo.png        BackgroundLogo        res_BackgroundLogo
+../../../Tools/bin2cpp/bin2cpp AppIcon.png               AppIcon               res_AppIcon
 
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Cpu.png        ConfigIcon_Cpu        png_ConfigIcon_Cpu
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Paths.png      ConfigIcon_Paths      png_ConfigIcon_Paths
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Plugins.png    ConfigIcon_Plugins    png_ConfigIcon_Plugins
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Speedhacks.png ConfigIcon_Speedhacks png_ConfigIcon_Speedhacks
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Gamefixes.png  ConfigIcon_Gamefixes  png_ConfigIcon_Gamefixes
-../../../Tools/bin2cpp/bin2cpp ConfigIcon_Video.png      ConfigIcon_Video      png_ConfigIcon_Video
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Cpu.png        ConfigIcon_Cpu        res_ConfigIcon_Cpu
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Paths.png      ConfigIcon_Paths      res_ConfigIcon_Paths
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Plugins.png    ConfigIcon_Plugins    res_ConfigIcon_Plugins
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Speedhacks.png ConfigIcon_Speedhacks res_ConfigIcon_Speedhacks
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Gamefixes.png  ConfigIcon_Gamefixes  res_ConfigIcon_Gamefixes
+../../../Tools/bin2cpp/bin2cpp ConfigIcon_Video.png      ConfigIcon_Video      res_ConfigIcon_Video
+../../../Tools/bin2cpp/bin2cpp Dualshock.png      Dualshock   res_Dualshock
---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Run before compiling. Compile by loading up pcsx2/Linux/pcsx2.workspace
+# with Code::Blocks.
+
+cd pcsx2/gui/Resources
+./rebuild.sh
+cd ../../..
