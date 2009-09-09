cda84b645591807fe7627dc316de7f5a321ec418
arcum42 <arcum42@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-09-09 10:02:12 +0000

    Got rid of various obsolete files, and moved the codeblocks workspace file.
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1797 96395faa-99c1-11dd-bbfe-3dabce05a288

--- build-plugins.sh
+++ build-plugins.sh
@@ -3,22 +3,8 @@
 # Usage: sh build.sh [option]
 # option can be all (rebuilds everything), clean, or nothing (incremental build)
 # Modify the individual build.sh for specific program options like debug symbols
-# Uncomment if building by itself, rather then with all the plugins
+#This is just for building the plugins; pcsx2 is build using codeblocks.
 
-#Normal
-#export PCSX2OPTIONS="--prefix `pwd`"
-
-#Optimized, but a devbuild
-export PCSX2OPTIONS="--enable-devbuild --prefix `pwd`"
-
-#Debug version (which now implies a devbuild)
-#export PCSX2OPTIONS="--enable-debug --prefix `pwd`"
-
-#Normal, but unoptimized
-#export PCSX2OPTIONS="--disable-optimization --prefix `pwd`"
-
-#Normal, with warnings
-#export PCSX2OPTIONS="--enable-warnings --prefix `pwd`"
 
 #ZeroGS Normal mode
 export ZEROGSOPTIONS="--enable-sse2"
@@ -37,7 +23,7 @@ export PCSX2PLUGINS="`pwd`/bin/plugins"
 curdir=`pwd`
 
 echo
-echo "Building the Pcsx2 Suite."
+echo "Building the Pcsx2 Plugins."
 echo "Note: binaries generated are 32 bit, and require 32 bit versions of all dependencies."
 cd ${curdir}/plugins
 sh build.sh $option
@@ -45,17 +31,5 @@ sh build.sh $option
 if [ $? -ne 0 ]
 then
 echo Error with building plugins
-exit 1
-fi
-
-echo
-echo "Building Pcsx2."
-echo "Note: binaries generated are 32 bit, and require 32 bit versions of all dependencies."
-cd ${curdir}/pcsx2
-sh build.sh $option
-
-if [ $? -ne 0 ]
-then
-echo Error with building pcsx2
 exit 1
 fi
