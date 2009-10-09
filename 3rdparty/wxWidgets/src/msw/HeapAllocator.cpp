a4f180aa5dad4cbc34cde7318f11f71208849621
Jake.Stine <Jake.Stine@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-10-09 00:39:09 +0000

    wxWidgets: helps if I fix *both* string and object allocators.  I'm a commit spammer tonight. -_-
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1991 96395faa-99c1-11dd-bbfe-3dabce05a288

--- 3rdparty/wxWidgets/src/msw/HeapAllocator.cpp
+++ 3rdparty/wxWidgets/src/msw/HeapAllocator.cpp
@@ -56,7 +56,7 @@ void _allocateHeap_wxObject()
 	if( win32_object_heap == INVALID_HANDLE_VALUE )
 	{
 		//wxASSERT( win32_object_heap_refcount == 0 );
-		win32_object_heap = HeapCreate( HEAP_NO_SERIALIZE, 0x200000, 0 );
+		win32_object_heap = HeapCreate( 0, 0x200000, 0 );
 	}
 }
 
