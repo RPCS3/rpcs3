1cb9a908e847eb455cdfe18c715918a71f9dfc24
Jake.Stine <Jake.Stine@96395faa-99c1-11dd-bbfe-3dabce05a288>
2009-10-21 19:19:12 +0000

    Why didn't I think of this sooner?  Added a pre-alpha announce when starting PCSX2 0.9.7 for the first time. Also: Minor fixes to the console logger (Win7 report of corrupted cursor)
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@2052 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/x86/iMMI.cpp
+++ pcsx2/x86/iMMI.cpp
@@ -146,7 +146,7 @@ void recPLZCW()
 			SSE2_PSHUFD_XMM_to_XMM(regs&0xf, regs&0xf, 0x4e);
 		}
 		else if( regs >= 0 && (regs & MEM_MMXTAG) ) {
-			PSHUFWRtoR(regs, regs, 0x4e);
+			PSHUFWRtoR(regs&0xf, regs&0xf, 0x4e);
 			MOVD32MMXtoR(EAX, regs&0xf);
 			PSHUFWRtoR(regs&0xf, regs&0xf, 0x4e);
 			SetMMXstate();
