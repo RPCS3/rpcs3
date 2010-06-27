7dbbaf1c04c7c24c6e04980da64f9d4ab6da36fa
ramapcsx2 <ramapcsx2@96395faa-99c1-11dd-bbfe-3dabce05a288>
2010-06-27 12:56:35 +0000

    Small fix to the ee timing hack.
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3325 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/R5900.cpp
+++ pcsx2/R5900.cpp
@@ -552,10 +552,10 @@ __forceinline void CPU_INT( u32 n, s32 ecycle)
 		DevCon.Warning( "***** EE > Twice-thrown int on IRQ %d", n );
 	}
 
-	// EE events happen 1 cycle in the future instead of whatever was requested.
+	// EE events happen 8 cycles in the future instead of whatever was requested.
 	// This can be used on games with PATH3 masking issues for example, or when
 	// some FMV look bad.
-	if(CHECK_EETIMINGHACK) ecycle = 1;
+	if(CHECK_EETIMINGHACK) ecycle = 8;
 
 	cpuRegs.interrupt|= 1 << n;
 	cpuRegs.sCycle[n] = cpuRegs.cycle;
