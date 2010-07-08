57161ec785a4429aad45ba6303de717f192b5ba1
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2010-07-08 18:59:44 +0000

    [copyright] * Upgrade SGI license to 2.0, opensource compliant :) and GPL compatible :)
    Note: previous version (1.1) is not free. And therefore not compatible with GPL. Fortunately for us, there is a license clause that allow upgrading the license.
    
    
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3433 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/zzogl-pg/opengl/glprocs.h
+++ plugins/zzogl-pg/opengl/glprocs.h
@@ -5,29 +5,33 @@
 ** GLprocs utility for getting function addresses for OpenGL(R) 1.2,
 ** OpenGL 1.3, OpenGL 1.4, OpenGL 1.5 and OpenGL extension functions.
 **
-** Version:  1.1
+** SGI FREE SOFTWARE LICENSE B (Version 2.0, Sept. 18, 2008)
+** Copyright (C) 1991-2000 Silicon Graphics, Inc. All Rights Reserved.
 **
-** License Applicability. Except to the extent portions of this file are
-** made subject to an alternative license as permitted in the SGI Free
-** Software License B, Version 1.1 (the "License"), the contents of this
-** file are subject only to the provisions of the License. You may not use
-** this file except in compliance with the License. You may obtain a copy
-** of the License at Silicon Graphics, Inc., attn: Legal Services, 1600
-** Amphitheatre Parkway, Mountain View, CA 94043-1351, or at:
+** Permission is hereby granted, free of charge, to any person obtaining a
+** copy of this software and associated documentation files (the "Software"),
+** to deal in the Software without restriction, including without limitation
+** the rights to use, copy, modify, merge, publish, distribute, sublicense,
+** and/or sell copies of the Software, and to permit persons to whom the
+** Software is furnished to do so, subject to the following conditions:
 **
-** http://oss.sgi.com/projects/FreeB
+** The above copyright notice including the dates of first publication and
+** either this permission notice or a reference to
+** http://oss.sgi.com/projects/FreeB/
+** shall be included in all copies or substantial portions of the Software.
 **
-** Note that, as provided in the License, the Software is distributed on an
-** "AS IS" basis, with ALL EXPRESS AND IMPLIED WARRANTIES AND CONDITIONS
-** DISCLAIMED, INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTIES AND
-** CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A
-** PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+** SILICON GRAPHICS, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+** OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
 **
-** Original Code. The Original Code is: OpenGL Sample Implementation,
-** Version 1.2.1, released January 26, 2000, developed by Silicon Graphics,
-** Inc. The Original Code is Copyright (c) 1991-2000 Silicon Graphics, Inc.
-** Copyright in any portions created by third parties is as indicated
-** elsewhere herein. All Rights Reserved.
+** Except as contained in this notice, the name of Silicon Graphics, Inc.
+** shall not be used in advertising or otherwise to promote the sale, use or
+** other dealings in this Software without prior written authorization from
+** Silicon Graphics, Inc.
 **
 ** Additional Notice Provisions: This software was created using the
 ** OpenGL(R) version 1.2.1 Sample Implementation published by SGI, but has
