83d71700d995b9f290add8647aea4f734479a2d4
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2010-07-08 16:51:56 +0000

    [copyright] * same as previous commit except for zzogl
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3430 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/zzogl-pg/opengl/ZZoglCRTC.h
+++ plugins/zzogl-pg/opengl/ZZoglCRTC.h
@@ -1,3 +1,22 @@
+/*  ZZ Open GL graphics plugin
+ *  Copyright (c)2009 zeydlitz@gmail.com
+ *  Based on Zerofrog's ZeroGS KOSMOS (c)2005-2006
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 #ifndef ZZOGLCRTC_H_INCLUDED
 #define ZZOGLCRTC_H_INCLUDED
 
