fded5fc218ed1fd70028b9b8faa505a1f8209c3b
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2010-07-10 12:37:18 +0000

    [debian] * Add copyright information * Downgrade depend to recomment to avoid circular dependency
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3449 96395faa-99c1-11dd-bbfe-3dabce05a288

--- debian-unstable-upstream/create_pcsx2_tarball_from_svn_repository.sh
+++ debian-unstable-upstream/create_pcsx2_tarball_from_svn_repository.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# copyright (c) 2010 Gregory Hainaut
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 ######################################################################
 # Global Parameter
