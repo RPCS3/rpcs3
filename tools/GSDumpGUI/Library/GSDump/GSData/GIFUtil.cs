3064551c09c3f0ace7d4bdb10d7f9821a26721f1
sudonim1 <sudonim1@96395faa-99c1-11dd-bbfe-3dabce05a288>
2010-12-28 18:32:12 +0000

    GSDumpGUI: Forgot a file
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@4165 96395faa-99c1-11dd-bbfe-3dabce05a288

---
+++ tools/GSDumpGUI/Library/GSDump/GSData/GIFUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GSDumpGUI
+{
+    public class GIFUtil
+    {
+        public static UInt64 GetBit(UInt64 value, int lower, int count)
+        {
+            return (value >> lower) & (ulong)((1ul << count) - 1);
+        }
+    }
+}
