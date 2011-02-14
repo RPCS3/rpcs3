3f9e72f0b8b96151812e6cecec50f8eced616e1e
gregory.hainaut@gmail.com <gregory.hainaut@gmail.com@96395faa-99c1-11dd-bbfe-3dabce05a288>
2011-02-14 18:50:49 +0000

    linux: avoid some corner case in the linux launcher script
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@4298 96395faa-99c1-11dd-bbfe-3dabce05a288

--- bin/launch_pcsx2_linux.sh
+++ bin/launch_pcsx2_linux.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # This script is a small wrapper to the PCSX2 exectuable. The purpose is to
 # launch PCSX2 from the same repository every times.
 # Rationale: There is no guarantee on the directory when PCSX2 is launched from a shortcut.
 #            This behavior trigger the first time wizards everytime...
+
+if test $0 = "launch_pcsx2_linux.sh" || $0 = "sh" || $0 = "bash" ; then
+    echo "Error the script was directly 'called'"
+    echo "Use either /absolute_path/launch_pcsx2_linux.sh or ./relative_path/launch_pcsx2_linux.sh"
+    exit 1
+fi
 
 # Note: sh (dash on debian) does not support pushd, popd...
 # Save current directory
