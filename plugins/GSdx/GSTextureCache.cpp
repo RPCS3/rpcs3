0783f31f84c72282d511bb7492ec34259e5ba728
ramapcsx2 <ramapcsx2@96395faa-99c1-11dd-bbfe-3dabce05a288>
2011-04-15 17:12:22 +0000

    GSdx: Small mistake :p
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@4576 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/GSdx/GSTextureCache.cpp
+++ plugins/GSdx/GSTextureCache.cpp
@@ -637,7 +637,7 @@ GSTextureCache::Source* GSTextureCache::CreateSource(const GIFRegTEX0& TEX0, con
 		else if(tw < 1024)
 		{
 			// FIXME: timesplitters blurs the render target by blending itself over a couple of times
-			//hack = true;
+			hack = true;
 			//if(tw == 256 && th == 128 && (TEX0.TBP0 == 0 || TEX0.TBP0 == 0x00e00))
 			//{
 			//	delete src;
