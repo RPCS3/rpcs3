50474b6b9f8d2550399093a74a0da55e2b757695
Jesse Beder <jbeder+github@gmail.com>
2011-05-29 02:17:49 +0000

    Fixed includedir for the .pc.cmake file

--- yaml-cpp.pc.cmake
+++ yaml-cpp.pc.cmake
@@ -1,7 +1,7 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=${prefix}/@LIB_INSTALL_DIR@
-includedir=${prefix}/@INCLUDE_INSTALL_DIR@
+includedir=${prefix}/@INCLUDE_INSTALL_ROOT_DIR@
 
 Name: Yaml-cpp
 Description: A YAML parser and emitter for C++
