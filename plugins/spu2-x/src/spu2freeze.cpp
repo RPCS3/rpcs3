ddaea35d6e9b5bdbc7e48acf8a0976328e3afaac
sudonim1@gmail.com <sudonim1@gmail.com@96395faa-99c1-11dd-bbfe-3dabce05a288>
2011-06-22 11:49:49 +0000

    SPU2-X: Bump savestate version since I broke compatibility with r4754
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@4756 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/spu2-x/src/spu2freeze.cpp
+++ plugins/spu2-x/src/spu2freeze.cpp
@@ -25,7 +25,7 @@ namespace Savestate
 
 	// versioning for saves.
 	// Increment this when changes to the savestate system are made.
-	static const u32 SAVE_VERSION = 0x0007;
+	static const u32 SAVE_VERSION = 0x0008;
 
 	static void wipe_the_cache()
 	{
