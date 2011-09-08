0e197b8723d27a962830ef619ccf84f96bb412c8
Jesse Beder <jbeder+github@gmail.com>
2011-09-08 02:05:03 -0500

    Added back the streamable conversions

--- include/yaml-cpp/value/convert.h
+++ include/yaml-cpp/value/convert.h
@@ -25,6 +25,42 @@ namespace YAML
 			return true;
 		}
 	};
+	
+#define YAML_DEFINE_CONVERT_STREAMABLE(type)\
+	template<>\
+	struct convert<type> {\
+		static Value encode(const type& rhs) {\
+			std::stringstream stream;\
+			stream << rhs;\
+			return Value(stream.str());\
+		}\
+		\
+		static bool decode(const Value& value, type& rhs) {\
+			if(value.Type() != ValueType::Scalar)\
+				return false;\
+			std::stringstream stream(value.scalar());\
+			stream >> rhs;\
+			return !!stream;\
+		}\
+	}
+	
+	YAML_DEFINE_CONVERT_STREAMABLE(int);
+	YAML_DEFINE_CONVERT_STREAMABLE(unsigned);
+	YAML_DEFINE_CONVERT_STREAMABLE(short);
+	YAML_DEFINE_CONVERT_STREAMABLE(unsigned short);
+	YAML_DEFINE_CONVERT_STREAMABLE(long);
+	YAML_DEFINE_CONVERT_STREAMABLE(unsigned long);
+	YAML_DEFINE_CONVERT_STREAMABLE(long long);
+	YAML_DEFINE_CONVERT_STREAMABLE(unsigned long long);
+	
+	YAML_DEFINE_CONVERT_STREAMABLE(char);
+	YAML_DEFINE_CONVERT_STREAMABLE(unsigned char);
+	
+	YAML_DEFINE_CONVERT_STREAMABLE(float);
+	YAML_DEFINE_CONVERT_STREAMABLE(double);
+	YAML_DEFINE_CONVERT_STREAMABLE(long double);
+	
+#undef YAML_DEFINE_CONVERT_STREAMABLE
 }
 
 #endif // VALUE_CONVERT_H_62B23520_7C8E_11DE_8A39_0800200C9A66
--- src/value/convert.cpp
+++ src/value/convert.cpp
@@ -3,37 +3,6 @@
 namespace YAML
 {
 
-//#define YAML_DEFINE_CONVERT_STREAMABLE(type)\
-//	template<> Value convert(const type& rhs) {\
-//		std::stringstream stream;\
-//		stream << rhs;\
-//		return Value(stream.str());\
-//	}\
-//	template<> bool convert(const Value& value, type& rhs) {\
-//		if(value.Type() != ValueType::Scalar)\
-//			return false;\
-//		std::stringstream stream(value.scalar());\
-//		stream >> rhs;\
-//		return !!stream;\
-//	}
-//
-//	YAML_DEFINE_CONVERT_STREAMABLE(int)
-//	YAML_DEFINE_CONVERT_STREAMABLE(unsigned)
-//	YAML_DEFINE_CONVERT_STREAMABLE(short)
-//	YAML_DEFINE_CONVERT_STREAMABLE(unsigned short)
-//	YAML_DEFINE_CONVERT_STREAMABLE(long)
-//	YAML_DEFINE_CONVERT_STREAMABLE(unsigned long)
-//	YAML_DEFINE_CONVERT_STREAMABLE(long long)
-//	YAML_DEFINE_CONVERT_STREAMABLE(unsigned long long)
-//
-//	YAML_DEFINE_CONVERT_STREAMABLE(char)
-//	YAML_DEFINE_CONVERT_STREAMABLE(unsigned char)
-//
-//	YAML_DEFINE_CONVERT_STREAMABLE(float)
-//	YAML_DEFINE_CONVERT_STREAMABLE(double)
-//	YAML_DEFINE_CONVERT_STREAMABLE(long double)
-//	
-//#undef YAML_DEFINE_CONVERT_STREAMABLE
 //	
 //	template<typename K, typename V>
 //	Value convert<std::map<K, V> >(const std::map<K, V>& rhs) {
