a7bdf08c7e6fa9bf3343b20602b220fc146e7306
Jesse Beder <jbeder+github@gmail.com>
2011-09-09 19:25:11 -0500

    Map iterator works\!

--- util/value.cpp
+++ util/value.cpp
@@ -3,12 +3,21 @@
 
 int main()
 {
-	YAML::Value value(YAML::ValueType::Sequence);
+	YAML::Value value;
+	value["seq"] = YAML::Value(YAML::ValueType::Sequence);
 	for(int i=0;i<5;i++)
-		value.append(i);
+		value["seq"].append(i);
+	value["map"]["one"] = "I";
+	value["map"]["two"] = "II";
+	value["map"]["three"] = "III";
+	value["map"]["four"] = "IV";
 	
-	for(YAML::const_iterator it=value.begin();it!=value.end();++it) {
+	for(YAML::const_iterator it=value["seq"].begin();it!=value["seq"].end();++it) {
 		std::cout << it->as<int>() << "\n";
+	}
+
+	for(YAML::const_iterator it=value["map"].begin();it!=value["map"].end();++it) {
+		std::cout << it->first.as<std::string>() << " -> " << it->second.as<std::string>() << "\n";
 	}
 	
 	return 0;
