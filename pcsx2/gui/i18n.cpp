fff11cf207524c555cc7b892f163171425d0f41d
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2011-12-21 20:27:03 +0000

    i18n: add more fallback for some languages (thanks to pg) cmake: implement the new XDG_STD options It allow to move all pcsx2 data from $HOME/PCSX2 to $XDG_CONFIG_DIR/pcsx2. Clearly a matter of personnal preference. debian: drop the useless stable packet from the trunk. Only keep a copy on branch release
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5001 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/gui/i18n.cpp
+++ pcsx2/gui/i18n.cpp
@@ -163,20 +163,26 @@ static wxLanguage i18n_FallbackToAnotherLang( wxLanguage wxLangId )
 
 	switch(wxLangId)
 	{
-		case wxLANGUAGE_CHINESE_HONGKONG  : 
-		case wxLANGUAGE_CHINESE_MACAU     : return wxLANGUAGE_CHINESE_TRADITIONAL;
-		case wxLANGUAGE_CHINESE_SINGAPORE : return wxLANGUAGE_CHINESE_SIMPLIFIED;
-		case wxLANGUAGE_SWEDISH_FINLAND   : return wxLANGUAGE_SWEDISH;
-		case wxLANGUAGE_PORTUGUESE        : return wxLANGUAGE_PORTUGUESE_BRAZILIAN;
+		case wxLANGUAGE_CHINESE_HONGKONG     : 
+		case wxLANGUAGE_CHINESE_MACAU        : return wxLANGUAGE_CHINESE_TRADITIONAL;
+
+		case wxLANGUAGE_CHINESE_SINGAPORE    : return wxLANGUAGE_CHINESE_SIMPLIFIED;
+
+		case wxLANGUAGE_SAMI                 : 
+		case wxLANGUAGE_SWEDISH_FINLAND      : return wxLANGUAGE_SWEDISH;
+
+		case wxLANGUAGE_PORTUGUESE           : return wxLANGUAGE_PORTUGUESE_BRAZILIAN;
+
 		// Overkill 9000?
-		case wxLANGUAGE_GERMAN_AUSTRIAN   :
-		case wxLANGUAGE_GERMAN_BELGIUM    :
-		case wxLANGUAGE_GERMAN_LIECHTENSTEIN :
-		case wxLANGUAGE_GERMAN_LUXEMBOURG :
-		case wxLANGUAGE_GERMAN_SWISS      :
-                                            return wxLANGUAGE_GERMAN;
+		case wxLANGUAGE_GERMAN_AUSTRIAN      : 
+		case wxLANGUAGE_GERMAN_BELGIUM       : 
+		case wxLANGUAGE_GERMAN_LIECHTENSTEIN : 
+		case wxLANGUAGE_GERMAN_LUXEMBOURG    : 
+		case wxLANGUAGE_GERMAN_SWISS         : return wxLANGUAGE_GERMAN;
+
+		case wxLANGUAGE_ITALIAN_SWISS        : return wxLANGUAGE_ITALIAN;
 		
-		default                           : break;
+		default                              : break;
 	}
 	return wxLangId;
 }
