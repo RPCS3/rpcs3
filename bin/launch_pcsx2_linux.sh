d75da041f63bab8fb5604319fc5504cb0f8c2c02
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2012-04-16 15:36:16 +0000

    copyright: soundtouch was LGPL2.1 not LGPL2.1+, add an header to launch_pcsx2_linux.sh cmake: keep all library for the linking of plugins hex2h.pl: add svn:executable
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5157 96395faa-99c1-11dd-bbfe-3dabce05a288

--- bin/launch_pcsx2_linux.sh
+++ bin/launch_pcsx2_linux.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -e
 
+# PCSX2 - PS2 Emulator for PCs
+# Copyright (C) 2002-2011  PCSX2 Dev Team
+#
+# PCSX2 is free software: you can redistribute it and/or modify it under the terms
+# of the GNU Lesser General Public License as published by the Free Software Found-
+# ation, either version 3 of the License, or (at your option) any later version.
+#
+# PCSX2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with PCSX2.
+# If not, see <http://www.gnu.org/licenses/>.
+
 # This script is a small wrapper to the PCSX2 exectuable. The purpose is to
 # launch PCSX2 from the same repository every times.
 # Rationale: There is no guarantee on the directory when PCSX2 is launched from a shortcut.
--- plugins/spu2-x/src/WavFile.cpp
+++ plugins/spu2-x/src/WavFile.cpp
@@ -4,13 +4,14 @@
  * The file is based on WavFile.h from SoundTouch library. 
  * Original portions are (c) 2009 by Olli Parviainen (oparviai 'at' iki.fi)
  *
- * SPU2-X is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Found-
- * ation, either version 3 of the License, or (at your option) any later version.
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License, version 2.1
+ * as published by the Free Software Foundation.
  *
- * SPU2-X is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with SPU2-X.  If not, see <http://www.gnu.org/licenses/>.
