a4475a45957436b60a3cdee62b5407bf102e2419
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2013-07-01 16:43:35 +0000

    spu2x: * remove iconvert.cpp not used anymore * change copyrigh header from GPL to LGPL. Those 2 files are only basic .h. 1 licence by plugin is enough ;)
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5691 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/spu2-x/src/Linux/Alsa.h
+++ plugins/spu2-x/src/Linux/Alsa.h
@@ -1,22 +1,19 @@
-/*  ZeroSPU2
- *  Copyright (C) 2006-2007 zerofrog
+/* SPU2-X, A plugin for Emulating the Sound Processing Unit of the Playstation 2
+ * Developed and maintained by the Pcsx2 Development Team.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * Original portions from SPU2ghz are (c) 2008 by David Quintana [gigaherz]
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * SPU2-X is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Found-
+ * ation, either version 3 of the License, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * SPU2-X is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SPU2-X.  If not, see <http://www.gnu.org/licenses/>.
  */
-
- // Modified by arcum42@gmail.com
 
  #ifndef __LINUX_H__
  #define __LINUX_H__
@@ -29,10 +26,6 @@
 #include <sys/soundcard.h>
 #include <unistd.h>
 
-#define ALSA_PCM_NEW_HW_PARAMS_API
-#define ALSA_PCM_NEW_SW_PARAMS_API
-
-#define SOUNDSIZE 500000
 #define SAMPLE_RATE 48000L
 
 // Pull in from Alsa.cpp
