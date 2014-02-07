907964a7d15acb59185fb09351816c7b554f1047
refraction <refraction@96395faa-99c1-11dd-bbfe-3dabce05a288>
2014-02-07 20:34:36 +0000

    Langs: Change the Arabic name properly, update relevant files (thanks for the heads up Gregory :) )
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5889 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/gui/i18n.cpp
+++ pcsx2/gui/i18n.cpp
@@ -179,11 +179,10 @@ static wxLanguage i18n_FallbackToAnotherLang( wxLanguage wxLangId )
 		case wxLANGUAGE_ARABIC_SYRIA         :
 		case wxLANGUAGE_ARABIC_TUNISIA       :
 		case wxLANGUAGE_ARABIC_UAE           :
-		case wxLANGUAGE_ARABIC_YEMEN         : return wxLANGUAGE_ARABIC;
+		case wxLANGUAGE_ARABIC_YEMEN         : return wxLANGUAGE_ARABIC_SAUDI_ARABIA;
 
 		case wxLANGUAGE_CHINESE_HONGKONG     : 
 		case wxLANGUAGE_CHINESE_MACAU        : return wxLANGUAGE_CHINESE_TRADITIONAL;
-
 		case wxLANGUAGE_CHINESE_SINGAPORE    : return wxLANGUAGE_CHINESE_SIMPLIFIED;
 
 		// case wxLANGUAGE_SAMI_INARI_FINLAND    :
