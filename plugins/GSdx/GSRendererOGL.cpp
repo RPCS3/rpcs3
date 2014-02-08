1922598f0ff0c928b0c0c4cabd919b72053951f3
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2014-02-08 11:22:51 +0000

    the "don't let users shoot themself in the foot" commit
    UserHacks_AlphaStencil will take precedence on override_GL_ARB_shader_image_load_store
    
    
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5891 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/GSdx/GSRendererOGL.cpp
+++ plugins/GSdx/GSRendererOGL.cpp
@@ -223,7 +223,7 @@ void GSRendererOGL::DrawPrims(GSTexture* rt, GSTexture* ds, GSTextureCache::Sour
 	om_bsel.wrgba = ~GSVector4i::load((int)context->FRAME.FBMSK).eq8(GSVector4i::xffffffff()).mask();
 
 	if (DATE && om_bsel.wa && (!context->TEST.ATE || context->TEST.ATST == ATST_ALWAYS)) {
-		advance_DATE = GLLoader::found_GL_ARB_shader_image_load_store;
+		advance_DATE = GLLoader::found_GL_ARB_shader_image_load_store && !UserHacks_AlphaStencil;
 	}
 
 	// DATE
