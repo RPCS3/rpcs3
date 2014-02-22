5af621056a9bf5ec65bd50a946c72888583d7f5c
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2014-02-22 16:12:42 +0000

    gsdx ogl: new option for free driver
    reduce_gl_requirement_for_free_driver => set it to 1 to only required a 3.0 context (you still required the good extensions)
    
    
    
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5910 96395faa-99c1-11dd-bbfe-3dabce05a288

--- plugins/GSdx/GSWndOGL.cpp
+++ plugins/GSdx/GSWndOGL.cpp
@@ -138,7 +138,10 @@ bool GSWndOGL::Attach(void* handle, bool managed)
 
 	m_NativeDisplay = XOpenDisplay(NULL);
 
-	CreateContext(3, 3);
+	if (theApp.GetConfig("reduce_gl_requirement_for_free_driver", 0) == 1)
+		CreateContext(3, 0);
+	else
+		CreateContext(3, 3);
 
 	AttachContext();
 
