1327ce6d736f15220b200ea1f1d3e0f7509ff6f7
Jesse Beder <jbeder+github@gmail.com>
2014-03-23 14:47:30 -0500

    Convert core parser tests to gtest

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,16 +4,12 @@
 #include <vector>
 
 #include "emittertests.h"
-#include "parsertests.h"
 #include "tests.h"
 
 namespace Test {
 void RunAll() {
   bool passed = true;
   if (!RunEmitterTests())
-    passed = false;
-
-  if (!RunParserTests())
     passed = false;
 
   if (passed)
