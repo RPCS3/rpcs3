86895cbf457fdd77fa0c5848fb68a40a165c37e0
Gregory Hainaut <gregory.hainaut@gmail.com>
2014-04-06 15:11:19 +0200

    pcsx2 license: add missing nice header on various files
    Remains 3 files that I don't know the source
    pcsx2/windows/DwmSetup.cpp: *No copyright* UNKNOWN
    pcsx2/windows/SamplProf.cpp: *No copyright* UNKNOWN
    pcsx2/windows/VCprojects/IopSif.cpp: *No copyright* UNKNOWN
    
    Remains 1 files in common that I don't know the source
    common/include/comptr.h: *No copyright* UNKNOWN
    
    Remains too much files in plugins that I don't know the source :(

--- plugins/cdvdGigaherz/src/Windows/config.cpp
+++ plugins/cdvdGigaherz/src/Windows/config.cpp
@@ -4,7 +4,7 @@
 //This library is free software; you can redistribute it and/or
 //modify it under the terms of the GNU Lesser General Public
 //License as published by the Free Software Foundation; either
-//version 2.1 of the License, or (at your option) any later version.
+//version 3.0 of the License, or (at your option) any later version.
 //
 //This library is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
