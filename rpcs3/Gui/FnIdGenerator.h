770ebaf271d363b2f00ce044338e459f53a74220
Oil <vasvasilew@yandex.ru>
2014-04-09 23:57:26 +0400

    Implemented small tool for generating id of HLE-Function. Fixed small issue in PADManager dialog.

---
+++ rpcs3/Gui/FnIdGenerator.h
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "TextInputDialog.h"
+#include "../Crypto/aes.h"
+#include "../Crypto/sha1.h"
+
+class FnIdGenerator : public wxDialog
+{
+private:
+	Array<std::string> m_func_name;
+	Array<u32> m_func_id;
+	wxListView* m_list;
+
+public:
+	FnIdGenerator(wxWindow* parent);
+	~FnIdGenerator();
+
+	void OnInput(wxCommandEvent &event);
+	void OnClear(wxCommandEvent &event);
+	u32 GenerateFnId(const std::string& func_name);
+	void UpdateInformation();
+	void PrintId();
+};
