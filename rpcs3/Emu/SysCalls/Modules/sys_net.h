ee2e0b3bbfb5cab8f0060f7253ef2111386dc676
darkf <lw9k123@gmail.com>
2014-04-24 22:02:33 -0700

    sys_net: Implement sockaddr_in, inet_addr, listen

--- rpcs3/Emu/SysCalls/Modules/sys_net.h
+++ rpcs3/Emu/SysCalls/Modules/sys_net.h
@@ -14,3 +14,12 @@ struct sys_net_sockaddr
 	u8 sa_family; // sa_family_t
 	u8 sa_data[14];
 };
+
+struct sys_net_sockaddr_in
+{
+	u8 sin_len;
+	u8 sin_family;
+	be_t<u16> sin_port;
+	be_t<u32> sa_addr; // struct in_addr
+	u8 unused[8];
+};
