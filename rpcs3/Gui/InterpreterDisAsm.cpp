92d556524bfacfad84fc3e5c7247d1d25506493c
Peter Tissen <dravorek@gmail.com>
2014-04-28 21:18:11 +0200

    well let's hope Clang doesn't mind implicit unsigned int to u64

--- rpcs3/Gui/InterpreterDisAsm.cpp
+++ rpcs3/Gui/InterpreterDisAsm.cpp
@@ -455,7 +455,7 @@ void InterpreterDisAsmFrame::Show_Val(wxCommandEvent& WXUNUSED(event))
 
 	if(diag->ShowModal() == wxID_OK)
 	{
-		u64 pc = CPU ? CPU->PC : 0x0;
+		unsigned long pc = CPU ? CPU->PC : 0x0;
 		p_pc->GetValue().ToULong(&pc, 16);
 		Emu.GetMarkedPoints().push_back(pc);
 		remove_markedPC.push_back(Emu.GetMarkedPoints().size()-1);
