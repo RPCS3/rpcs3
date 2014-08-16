e360746265c05e215ce6f665962e8b176ee5d71c
luxsie <877033040@qq.com>
2014-08-17 00:03:31 +0800

    Give Log Console (ConLogFrame) a Context Menu with Copy and Clear actions.

--- rpcs3/Gui/ConLogFrame.h
+++ rpcs3/Gui/ConLogFrame.h
@@ -9,6 +9,8 @@ class LogFrame
 	wxAuiNotebook m_tabs;
 	wxTextCtrl *m_log;
 	wxTextCtrl *m_tty;
+	//Copy Action in Context Menu
+	wxTextDataObject* m_tdo;
 
 public:
 	LogFrame(wxWindow* parent);
@@ -21,6 +23,8 @@ private:
 	virtual void Task(){};
 
 	void OnQuit(wxCloseEvent& event);
+	void OnRightClick(wxMouseEvent& event);	//Show context menu
+	void OnContextMenu(wxCommandEvent& event);	//After select
 
 	DECLARE_EVENT_TABLE();
 };
