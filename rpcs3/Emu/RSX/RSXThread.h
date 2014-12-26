97d50a0692c0b5f3c613d07218bd8169ce033ec3
raven02 <jacky.kktsui@yahoo.com.hk>
2014-12-26 13:59:47 +0800

    Formatting RSX thread

--- rpcs3/Emu/RSX/RSXThread.h
+++ rpcs3/Emu/RSX/RSXThread.h
@@ -519,12 +519,14 @@ protected:
 		m_line_stipple_factor = 1;
 		m_vertex_data_base_offset = 0;
 		m_vertex_data_base_index = 0;
+		
+		// Construct Stipple Pattern
 		for (size_t i = 0; i < 32; i++) {
 			m_polygon_stipple_pattern[i] = 0xFFFFFFFF;
 		}
 
 		// Construct Textures
-		for(int i=0; i<16; i++)
+		for (int i = 0; i < 16; i++)
 		{
 			m_textures[i] = RSXTexture(i);
 		}
@@ -611,7 +613,7 @@ protected:
 		m_clear_surface_mask = 0;
 		m_begin_end = 0;
 
-		for(uint i=0; i<m_textures_count; ++i)
+		for (uint i = 0; i < m_textures_count; ++i)
 		{
 			m_textures[i].Init();
 		}
@@ -622,8 +624,8 @@ protected:
 
 	u32 OutOfArgsCount(const uint x, const u32 cmd, const u32 count, const u32 args_addr);
 	void DoCmd(const u32 fcmd, const u32 cmd, const u32 args_addr, const u32 count);
-	void nativeRescale(float width, float height);
-	
+	void NativeRescale(float width, float height);
+
 	virtual void OnInit() = 0;
 	virtual void OnInitThread() = 0;
 	virtual void OnExitThread() = 0;
@@ -634,9 +636,9 @@ protected:
 
 	void LoadVertexData(u32 first, u32 count)
 	{
-		for(u32 i=0; i<m_vertex_count; ++i)
+		for (u32 i = 0; i< m_vertex_count; ++i)
 		{
-			if(!m_vertex_data[i].IsEnabled()) continue;
+			if (!m_vertex_data[i].IsEnabled()) continue;
 
 			m_vertex_data[i].Load(first, count, m_vertex_data_base_offset, m_vertex_data_base_index);
 		}
