8ce3943e09c03fd0d823c61039d09f9164b09403
Raul Tambre <raul.tambre@gmail.com>
2015-02-13 16:44:26 +0200

    Small formatting changes

--- rpcs3/Emu/SysCalls/Modules/cellNetCtl.cpp
+++ rpcs3/Emu/SysCalls/Modules/cellNetCtl.cpp
@@ -80,6 +80,7 @@ int cellNetCtlNetStartDialogLoadAsync(vm::ptr<CellNetCtlNetStartDialogParam> par
 
 	// TODO: Actually sign into PSN
 	sysutilSendSystemCommand(CELL_SYSUTIL_NET_CTL_NETSTART_FINISHED, 0);
+
 	return CELL_OK;
 }
 
@@ -95,6 +96,7 @@ int cellNetCtlNetStartDialogUnloadAsync(vm::ptr<CellNetCtlNetStartDialogResult> 
 	cellNetCtl->Warning("cellNetCtlNetStartDialogUnloadAsync(result_addr=0x%x)", result.addr());
 
 	sysutilSendSystemCommand(CELL_SYSUTIL_NET_CTL_NETSTART_UNLOADED, 0);
+
 	return CELL_OK;
 }
 
