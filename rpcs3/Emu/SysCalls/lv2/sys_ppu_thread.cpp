73d9b58dfc3d94cc70c2c5263122318b2a975110
luxsie <luxsie@163.com>
2015-02-18 23:56:46 +0800

    sys_ppu_thread_create: minimum stack size and stack allocation unit applied.

--- rpcs3/Emu/SysCalls/lv2/sys_ppu_thread.cpp
+++ rpcs3/Emu/SysCalls/lv2/sys_ppu_thread.cpp
@@ -162,6 +162,15 @@ PPUThread* ppu_thread_create(u32 entry, u64 arg, s32 prio, u32 stacksize, bool i
 {
 	PPUThread& new_thread = *(PPUThread*)&Emu.GetCPU().AddThread(CPU_THREAD_PPU);
 
+	// Note: I haven't figured out the minimum stack size of PPU Thread.
+	//       Maybe it can be done with pthread_attr_getstacksize function.
+	//       And i toke 4096 (PTHREAD_STACK_MIN, and the smallest allocation unit) for this.
+	if ((stacksize % 4096) || (stacksize == 0)) {
+		// If not times of smallest allocation unit, round it up to the nearest one.
+		// And regard zero as a same condition.
+		stacksize = 4096 * ((u32)(stacksize / 4096) + 1);
+	}
+
 	u32 id = new_thread.GetId();
 	new_thread.SetEntry(entry);
 	new_thread.SetPrio(prio);
