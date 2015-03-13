d209cc56718221043912737597f766faef44e0e2
Nekotekina <nekotekina@gmail.com>
2015-03-13 18:36:03 +0300

    Fix

--- rpcs3/Emu/Memory/vm_var.h
+++ rpcs3/Emu/Memory/vm_var.h
@@ -120,12 +120,12 @@ namespace vm
 
 		template<typename AT> operator _ptr_base<void, 1, AT>() const
 		{
-			return _ptr_base<void, 1, AT>::make(m_data.addr);
+			return _ptr_base<void, 1, AT>::make(m_data);
 		}
 
 		template<typename AT> operator _ptr_base<const void, 1, AT>() const
 		{
-			return _ptr_base<const void, 1, AT>::make(m_data.addr);
+			return _ptr_base<const void, 1, AT>::make(m_data);
 		}
 		
 		operator T&()
