469926c5e1c44f8dc40d76f6f58b8c544f976152
Nekotekina <nekotekina@gmail.com>
2015-03-14 23:28:59 +0300

    _sys_heap*, _sys_malloc+, _sys_strcmp implemented

--- rpcs3/Emu/SysCalls/Modules/sysPrxForUser.h
+++ rpcs3/Emu/SysCalls/Modules/sysPrxForUser.h
@@ -2,25 +2,21 @@
 
 struct HeapInfo
 {
-	u32 heap_addr;
-	u32 align;
-	u32 size;
+	const std::string name;
 
-	HeapInfo(u32 _heap_addr, u32 _align, u32 _size)
-		: heap_addr(_heap_addr)
-		, align(_align)
-		, size(_size)
+	HeapInfo(const char* name)
+		: name(name)
 	{
 	}
 };
 
-typedef s32(spu_printf_cb_t)(u32 arg);
+using spu_printf_cb_t = vm::ptr<s32(u32 arg)>;
 
 // Aux
-extern vm::ptr<spu_printf_cb_t> spu_printf_agcb;
-extern vm::ptr<spu_printf_cb_t> spu_printf_dgcb;
-extern vm::ptr<spu_printf_cb_t> spu_printf_atcb;
-extern vm::ptr<spu_printf_cb_t> spu_printf_dtcb;
+extern spu_printf_cb_t spu_printf_agcb;
+extern spu_printf_cb_t spu_printf_dgcb;
+extern spu_printf_cb_t spu_printf_atcb;
+extern spu_printf_cb_t spu_printf_dtcb;
 
 // Functions
 vm::ptr<void> _sys_memset(vm::ptr<void> dst, s32 value, u32 size);
