30acfb89c3dc78d3a17a76d629e3583ac3c65cf7
luxsie <luxsie@163.com>
2015-04-10 19:12:08 +0800

    CppCheck: Orz. fix out-bound reference of NONZEROCOEFFS in CellCameraAttributes.

--- rpcs3/Emu/SysCalls/Modules/cellCamera.h
+++ rpcs3/Emu/SysCalls/Modules/cellCamera.h
@@ -314,7 +314,8 @@ struct CellCameraAttributes
 	u32 LED;
 	u32 AUDIOGAIN;
 	u32 QS;
-	u32 NONZEROCOEFFS[1];
+// Syphurith: Found using cppcheck. This is originally stated as an single-element array, however references its [0] and [1]. So i changed this to 2 u32 units.
+	u32 NONZEROCOEFFS[2];
 	u32 YUVFLAG;
 	u32 JPEGFLAG;
 	u32 BACKLIGHTCOMP;
