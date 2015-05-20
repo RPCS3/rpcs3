31ea9068dedf61c899263a4ebbe26213b4ba48f6
vlj <vljn.ovi@gmail.com>
2015-05-21 01:23:46 +0200

    d3d12: Use true default value for vertex decompiler

--- rpcs3/Emu/RSX/D3D12/D3D12VertexProgramDecompiler.cpp
+++ rpcs3/Emu/RSX/D3D12/D3D12VertexProgramDecompiler.cpp
@@ -172,7 +172,7 @@ void D3D12VertexProgramDecompiler::insertMainStart(std::stringstream & OS)
 	for (const ParamType PT : m_parr.params[PF_PARAM_NONE])
 	{
 		for (const ParamItem &PI : PT.items)
-			OS << "	" << PT.type << " " << PI.name << " = float4(0., 0., 0., 1.);" << std::endl;
+			OS << "	" << PT.type << " " << PI.name << " = " << PI.value << ";" << std::endl;
 	}
 
 	for (const ParamType PT : m_parr.params[PF_PARAM_IN])
