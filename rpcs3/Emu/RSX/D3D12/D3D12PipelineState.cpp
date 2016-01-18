19cc11cf911ffb4747d6d17d3a8e9226031d28d0
Vincent Lejeune <vljn.ovi@gmail.com>
2016-01-18 20:25:48 +0100

    more

--- rpcs3/Emu/RSX/D3D12/D3D12PipelineState.cpp
+++ rpcs3/Emu/RSX/D3D12/D3D12PipelineState.cpp
@@ -56,7 +56,7 @@ void D3D12GSRender::load_program()
 
 	u32 input_mask = rsx::method_registers[NV4097_SET_VERTEX_ATTRIB_INPUT_MASK];
 	u32 modulo_mask = rsx::method_registers[NV4097_SET_FREQUENCY_DIVIDER_OPERATION];
-
+	vertex_program.rsx_vertex_inputs.clear();
 	for (u8 index = 0; index < rsx::limits::vertex_count; ++index)
 	{
 		bool enabled = !!(input_mask & (1 << index));
