b038f59256f79b6ecdb041ae8f51ea6d69f279a6
raven02 <jacky.kktsui@yahoo.com.hk>
2016-05-25 23:41:56 +0800

    Write frag/vert program as .glsl and .spirv

--- rpcs3/Emu/RSX/D3D12/D3D12PipelineState.h
+++ rpcs3/Emu/RSX/D3D12/D3D12PipelineState.h
@@ -130,7 +130,7 @@ struct D3D12Traits
 			}
 		}
 
-		fs::file(fs::get_config_dir() + "FragmentProgram" + std::to_string(ID) + ".hlsl", fs::rewrite).write(shader);
+		fs::file(fs::get_config_dir() + "dev_hdd1/shader/" + "FragmentProgram" + std::to_string(ID) + ".hlsl", fs::rewrite).write(shader);
 		fragmentProgramData.id = (u32)ID;
 	}
 
@@ -141,7 +141,7 @@ struct D3D12Traits
 		std::string shaderCode = VS.Decompile();
 		vertexProgramData.Compile(shaderCode, Shader::SHADER_TYPE::SHADER_TYPE_VERTEX);
 		vertexProgramData.vertex_shader_input_count = RSXVP.rsx_vertex_inputs.size();
-		fs::file(fs::get_config_dir() + "VertexProgram" + std::to_string(ID) + ".hlsl", fs::rewrite).write(shaderCode);
+		fs::file(fs::get_config_dir() + "dev_hdd1/shader/" + "VertexProgram" + std::to_string(ID) + ".hlsl", fs::rewrite).write(shaderCode);
 		vertexProgramData.id = (u32)ID;
 	}
 
