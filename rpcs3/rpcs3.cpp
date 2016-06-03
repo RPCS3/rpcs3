c89da87a0eec48764e452a320e7238f85823cc4b
raven02 <jacky.kktsui@yahoo.com.hk>
2016-06-03 20:06:37 +0800

    Rename DX12 as DirectX 12

--- rpcs3/rpcs3.cpp
+++ rpcs3/rpcs3.cpp
@@ -96,7 +96,7 @@ cfg::map_entry<std::function<std::shared_ptr<GSRender>()>> g_cfg_gs_render(cfg::
 	{ "Null", PURE_EXPR(std::make_shared<NullGSRender>()) },
 	{ "OpenGL", PURE_EXPR(std::make_shared<GLGSRender>()) },
 #ifdef _MSC_VER
-	{ "DX12", PURE_EXPR(std::make_shared<D3D12GSRender>()) },
+	{ "DirectX 12", PURE_EXPR(std::make_shared<D3D12GSRender>()) },
 	{ "Vulkan", PURE_EXPR(std::make_shared<VKGSRender>()) },
 #endif
 });
