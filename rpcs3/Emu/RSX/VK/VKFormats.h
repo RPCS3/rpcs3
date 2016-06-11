5f06ec05855312d2e5ceb1dde30196c7713a872a
raven02 <jacky.kktsui@yahoo.com.hk>
2016-06-11 11:33:02 +0800

    Vulkan: add logic op support

--- rpcs3/Emu/RSX/VK/VKFormats.h
+++ rpcs3/Emu/RSX/VK/VKFormats.h
@@ -13,7 +13,8 @@ namespace vk
 	gpu_formats_support get_optimal_tiling_supported_formats(VkPhysicalDevice physical_device);
 	VkFormat get_compatible_depth_surface_format(const gpu_formats_support &support, rsx::surface_depth_format format);
 	VkStencilOp get_stencil_op(u32 op);
-	
+	VkLogicOp get_logic_op(u32 op);
+
 	std::tuple<VkFilter, VkSamplerMipmapMode> get_min_filter_and_mip(rsx::texture_minify_filter min_filter);
 	VkFilter get_mag_filter(rsx::texture_magnify_filter mag_filter);
 	VkSamplerAddressMode vk_wrap_mode(rsx::texture_wrap_mode gcm_wrap);
