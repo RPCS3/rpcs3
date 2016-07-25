494567dbd3db816202bcdebdfb95f6854a37d1af
raven02 <jacky.kktsui@yahoo.com.hk>
2016-07-25 08:28:09 +0800

    PPU: register undocumented functions in cellSysmodule

--- rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
+++ rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
@@ -344,6 +344,18 @@ s32 cellSysmoduleFetchImage()
 	return CELL_OK;
 }
 
+s32 cellSysmodule_B498BF77()
+{
+	UNIMPLEMENTED_FUNC(cellSysmodule);
+	return CELL_OK;
+}
+
+s32 cellSysmodule_D9B8C0EE()
+{
+	UNIMPLEMENTED_FUNC(cellSysmodule);
+	return CELL_OK;
+}
+
 DECLARE(ppu_module_manager::cellSysmodule)("cellSysmodule", []()
 {
 	REG_FUNC(cellSysmodule, cellSysmoduleInitialize);
@@ -354,4 +366,6 @@ DECLARE(ppu_module_manager::cellSysmodule)("cellSysmodule", []()
 	REG_FUNC(cellSysmodule, cellSysmoduleIsLoaded);
 	REG_FUNC(cellSysmodule, cellSysmoduleGetImagesize);
 	REG_FUNC(cellSysmodule, cellSysmoduleFetchImage);
+	REG_FNID(cellSysmodule, 0xB498BF77, cellSysmodule_B498BF77);
+	REG_FNID(cellSysmodule, 0xD9B8C0EE, cellSysmodule_D9B8C0EE);
 });
