da3843e871427857b34424ccc4c66d23a4b3e419
Zangetsu38 <Dante38@free.fr>
2016-12-19 20:46:08 +0100

    Update Project to Visual Studio 2017 Actually only use for me and for test with VS 2017

--- rpcs3/Emu/Cell/Modules/cellAvconfExt.cpp
+++ rpcs3/Emu/Cell/Modules/cellAvconfExt.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Emu/System.h"
 #include "Emu/Cell/PPUModule.h"
 
@@ -132,11 +132,11 @@ s32 cellVideoOutGetScreenSize(u32 videoOut, vm::ptr<float> screenSize)
 
 DECLARE(ppu_module_manager::cellAvconfExt)("cellSysutilAvconfExt", []()
 {
-	REG_VNID(cellSysutilAvconfExt, 0x00000000, g_gamma, []
+	/*REG_VNID(cellSysutilAvconfExt, 0x00000000, g_gamma, []
 	{
 		// Test
 		*g_gamma = 1.0f;
-	});
+	});*/
 
 	REG_FUNC(cellSysutilAvconfExt, cellAudioOutUnregisterDevice);
 	REG_FUNC(cellSysutilAvconfExt, cellAudioOutGetDeviceInfo2);
