cb1f30e9d2f9b985a708058dc3819c34380f735c
Nekotekina <nekotekina@gmail.com>
2017-02-05 03:06:05 +0300

    Report fixup

--- rpcs3/Emu/System.cpp
+++ rpcs3/Emu/System.cpp
@@ -511,20 +511,16 @@ s32 error_code::error_report(const fmt_type_info* sup, u64 arg)
 			// Filter some annoying reports
 			switch (arg)
 			{
+			case CELL_ESRCH:
 			case CELL_EDEADLK:
 			{
-				if (ppu.m_name == "_cellsurMixerMain" && std::memcmp(ppu.last_function, "sys_mutex_lock", 15) == 0)
+				if (ppu.m_name == "_cellsurMixerMain" || ppu.m_name == "_sys_MixerChStripMain")
 				{
-					level = logs::level::trace;
-				}
-
-				break;
-			}
-			case CELL_ESRCH:
-			{
-				if (ppu.m_name == "_sys_MixerChStripMain" && std::memcmp(ppu.last_function, "sys_lwmutex_lock", 17) == 0)
-				{
-					level = logs::level::trace;
+					if (std::memcmp(ppu.last_function, "sys_mutex_lock", 15) == 0 ||
+						std::memcmp(ppu.last_function, "sys_lwmutex_lock", 17) == 0)
+					{
+						level = logs::level::trace;
+					}
 				}
 
 				break;
