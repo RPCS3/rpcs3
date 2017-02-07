8bd7d5a11f7059e7c34a6a73016104a6da044d35
Yukarin <yukariin@yandex.ru>
2017-02-07 16:06:05 +0300

    Abb LogFrame base layout

--- rpcs3/rpcs3qt/logframe.h
+++ rpcs3/rpcs3qt/logframe.h
@@ -2,13 +2,24 @@
 #define LOGFRAME_H
 
 #include <QDockWidget>
+#include <QTabWidget>
+#include <QTextEdit>
+#include <QTimer>
 
 class LogFrame : public QDockWidget
 {
     Q_OBJECT
 
 public:
-		explicit LogFrame(QWidget *parent = 0);
+	explicit LogFrame(QWidget *parent = 0);
+
+private slots:
+	void Update();
+
+private:
+	QTabWidget *tabWidget;
+	QTextEdit *log;
+	QTextEdit *tty;
 };
 
 #endif // LOGFRAME_H
