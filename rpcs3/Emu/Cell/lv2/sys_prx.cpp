a780d4fbd83e159b1474c942356bcb99b8548c34
Cornee Traas <corneetraas@hotmail.com>
2017-02-08 23:02:09 +0100

    Decode SPRX

--- rpcs3/Emu/Cell/lv2/sys_prx.cpp
+++ rpcs3/Emu/Cell/lv2/sys_prx.cpp
@@ -18,7 +18,10 @@ s32 prx_load_module(std::string path, u64 flags, vm::ptr<sys_prx_load_module_opt
 {
 	sys_prx.warning("prx_load_module(path='%s', flags=0x%llx, pOpt=*0x%x)", path.c_str(), flags, pOpt);
 
-	const ppu_prx_object obj = fs::file(vfs::get(path));
+	fs::file ppu_file;
+	DecryptSelf(ppu_file, fs::file(vfs::get(path)));
+
+	const ppu_prx_object obj = ppu_file;
 
 	if (obj != elf_error::ok)
 	{
