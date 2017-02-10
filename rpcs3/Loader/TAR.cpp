236b0e05c9a6e3e045dc04018c5b64b3d7b41ccb
Cornee Traas <corneetraas@hotmail.com>
2017-02-10 02:10:14 +0100

    Add extract method + offset to TAR loader
    Also adds error checking + operator bool overload

--- rpcs3/Loader/TAR.cpp
+++ rpcs3/Loader/TAR.cpp
@@ -4,9 +4,10 @@
 
 #include <cstdlib>
 
-tar_object::tar_object(const fs::file& file) : m_file(file)
+tar_object::tar_object(const fs::file& file, size_t offset) : m_file(file), initial_offset(offset)
 {
-
+	m_file.seek(initial_offset);
+	largest_offset = initial_offset;
 }
 
 TARHeader tar_object::read_header(u64 offset)
@@ -32,6 +33,8 @@ int octalToDecimal(int octalNumber)
 
 std::vector<std::string> tar_object::get_filenames()
 {
+	if (!isValid) return std::vector<std::string>();
+
 	std::vector<std::string> vec;
 	get_file("");
 	for (auto it = m_map.cbegin(); it != m_map.cend(); ++it)
@@ -43,6 +46,8 @@ std::vector<std::string> tar_object::get_filenames()
 
 fs::file tar_object::get_file(std::string path)
 {
+	if (!isValid || !m_file) return fs::file();
+
 	auto it = m_map.find(path);
 	if (it != m_map.end())
 	{
@@ -50,7 +55,7 @@ fs::file tar_object::get_file(std::string path)
 		int size = octalToDecimal(atoi(header.size));
 		std::vector<u8> buf(size);
 		m_file.read(buf, size);
-		int offset = (m_file.pos() + 512 - 1) & ~(512 - 1);
+		int offset = ((m_file.pos() - initial_offset + 512 - 1) & ~(512 - 1)) + initial_offset; // Always keep the offset aligned to 512 bytes + the initial offset.
 		m_file.seek(offset);
 		return fs::make_stream(std::move(buf));
 	}
@@ -58,12 +63,16 @@ fs::file tar_object::get_file(std::string path)
 	{
 		while (m_file.pos() < m_file.size()) {
 			TARHeader header = read_header(largest_offset);
-			m_map[header.name] = largest_offset;
+			if (!isValid) return fs::file();
+
+			if (std::string(header.magic).find("ustar") != std::string::npos)
+				m_map[header.name] = largest_offset;
+
 			int size = octalToDecimal(atoi(header.size));
 			if (path.compare(header.name) == 0) { //path is equal, read file and advance offset to start of next block
 				std::vector<u8> buf(size);
 				m_file.read(buf, size);
-				int offset = (m_file.pos() + 512 - 1) & ~(512 - 1);
+				int offset = ((m_file.pos() - initial_offset + 512 - 1) & ~(512 - 1)) + initial_offset;
 				m_file.seek(offset);
 				largest_offset = offset;
 
@@ -71,7 +80,7 @@ fs::file tar_object::get_file(std::string path)
 			}
 			else { // just advance offset to next block
 				m_file.seek(size, fs::seek_mode::seek_cur);
-				int offset = (m_file.pos() + 512 - 1) & ~(512 - 1);
+				int offset = ((m_file.pos() - initial_offset + 512 - 1) & ~(512 - 1)) + initial_offset;
 				m_file.seek(offset);
 				largest_offset = offset;
 			}
@@ -79,4 +88,36 @@ fs::file tar_object::get_file(std::string path)
 		
 		return fs::file();
 	}
+}
+
+bool tar_object::extract(std::string path)
+{
+	if (!isValid || !m_file) return false;
+
+	get_file(""); //Make sure we have scanned all files
+	for (auto iter : m_map)
+	{
+		TARHeader header = read_header(iter.second);
+		if (std::string(header.name).empty()) continue;
+
+		switch (header.filetype) {
+		case '0':
+		{
+			fs::file file(header.name, fs::rewrite);
+			file.write(get_file(header.name).to_vector<u8>());
+			break;
+		}
+			
+		case '5':
+		{
+			fs::create_dir(path + header.name);
+			break;
+		}
+
+		default:
+			fmt::raw_error("Tar loader: unknown file type: "+header.filetype);
+			break;
+		}
+	}
+	return true;
 }
--- rpcs3/Loader/TAR.h
+++ rpcs3/Loader/TAR.h
@@ -10,7 +10,8 @@ typedef struct {
 	char chksum[8];
 	char filetype;
 	char linkname[100];
-	char dontcare2[88];
+	char magic[6];
+	char dontcare2[82];
 	char prefix[155];
 	char padding[12];
 } TARHeader;
@@ -19,14 +20,21 @@ class tar_object
 {
 	const fs::file& m_file;
 
-	int largest_offset = 0; //we store the largest offset so we can continue to scan from there.
-	std::map<std::string, u64> m_map; //maps path to offset of header of that file.
+	int initial_offset;
+	int largest_offset; //we store the largest offset so we can continue to scan from there.
+	std::map<std::string, u64> m_map; //maps path to offset of header of that file, so we only need to scan the entire file once.
+	bool isValid = true;
 
 	TARHeader read_header(u64 offset);
+
 public:
-	tar_object(const fs::file& file);
+	tar_object(const fs::file& file, size_t offset = 0);
+
+	operator bool() const { return isValid; };
 
 	std::vector<std::string> get_filenames();
 
 	fs::file get_file(std::string path);
+
+	bool extract(std::string path); // extract all files in archive to path
 };
