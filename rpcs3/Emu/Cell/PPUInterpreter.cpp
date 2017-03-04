67ac8bf0706b4c8498d240fd790324d2acbd2d5a
Wilfried Rabouin <WisterToub@users.noreply.github.com>
2017-03-04 14:41:40 +0100

    PPUInterpreter: Fix undefined behavior of left rotate functions (#2469)
    * PPUInterpreter: Fix undefined behavior of rol8 and rol16 with inline assembly
    
    * PPUInterpreter: Fix undefined behavior of rol32 and rol64
    
    * PPUInterpreter: Change left rotate functions to inline functions and move to types.h

--- rpcs3/Emu/Cell/PPUInterpreter.cpp
+++ rpcs3/Emu/Cell/PPUInterpreter.cpp
@@ -5,11 +5,6 @@
 
 #include <cmath>
 
-// TODO: fix rol8 and rol16 for __GNUG__ (probably with __asm__)
-inline u8 rol8(const u8 x, const u8 n) { return x << n | x >> (8 - n); }
-inline u16 rol16(const u16 x, const u16 n) { return x << n | x >> (16 - n); }
-inline u32 rol32(const u32 x, const u32 n) { return x << n | x >> (32 - n); }
-inline u64 rol64(const u64 x, const u64 n) { return x << n | x >> (64 - n); }
 inline u64 dup32(const u32 x) { return x | static_cast<u64>(x) << 32; }
 
 #if defined(__GNUG__)
