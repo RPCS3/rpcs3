d16ee18b3fcdb3610cf927a19ab98734f2be039e
Ani <ani-leo@outlook.com>
2017-03-24 20:17:56 +0000

    cellSysmodule: Register 0xF044, 0x0054, 0x005C modules
    cellSysmodule: Register 0xF044 module (cellSysutilNpEula)
    Found by debugging Uncharted 2 Demo (NPEA90055)
    Helps in all games that register sys module configuration 'multi-player'
    
    cellSysmodule: Register 0x0054 module (libmedi)
    Found on Motorstorm Apocalypse [NPEA00315] (thanks Zangetsu for the log)
    
    cellSysmodule: Register 0x005C module (cellCrossController)
    Found on LittleBigPlanet 2 [BCES00850] (thanks Zangetsu for the log)

--- rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
+++ rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
@@ -98,11 +98,13 @@ static const char* get_module_name(u16 id)
 	case 0x0050: return "cellSpursJq";
 	case 0x0052: return "cellPngEnc";
 	case 0x0053: return "cellMusicDecodeUtility";
+	case 0x0054: return "libmedi";
 	case 0x0055: return "cellSync2";
 	case 0x0056: return "sceNpUtil";
 	case 0x0057: return "cellRudp";
 	case 0x0059: return "sceNpSns";
 	case 0x005a: return "libgem";
+	case 0x005c: return "cellCrossController";
 	case 0xf00a: return "cellCelpEnc";
 	case 0xf010: return "cellGifDec";
 	case 0xf019: return "cellAdecCelp";
@@ -120,6 +122,7 @@ static const char* get_module_name(u16 id)
 	case 0xf030: return "cellSysutilAvc2";
 	case 0xf034: return "cellSailRec";
 	case 0xf035: return "sceNpTrophy";
+	case 0xf044: return "cellSysutilNpEula";
 	case 0xf053: return "cellAdecAt3multi";
 	case 0xf054: return "cellAtracMulti";
 	}
@@ -213,11 +216,13 @@ static const char* get_module_id(u16 id)
 	case 0x0050: return "CELL_SYSMODULE_SPURS_JQ";
 	case 0x0052: return "CELL_SYSMODULE_PNGENC";
 	case 0x0053: return "CELL_SYSMODULE_SYSUTIL_MUSIC_DECODE2";
+	case 0x0054: return "CELL_SYSMODULE_MEDI";
 	case 0x0055: return "CELL_SYSMODULE_SYNC2";
 	case 0x0056: return "CELL_SYSMODULE_SYSUTIL_NP_UTIL";
 	case 0x0057: return "CELL_SYSMODULE_RUDP";
 	case 0x0059: return "CELL_SYSMODULE_SYSUTIL_NP_SNS";
 	case 0x005a: return "CELL_SYSMODULE_GEM";
+	case 0x005c: return "CELL_SYSMODULE_SYSUTIL_CROSS_CONTROLLER";
 	case 0xf00a: return "CELL_SYSMODULE_CELPENC";
 	case 0xf010: return "CELL_SYSMODULE_GIFDEC";
 	case 0xf019: return "CELL_SYSMODULE_ADEC_CELP";
@@ -235,6 +240,7 @@ static const char* get_module_id(u16 id)
 	case 0xf030: return "CELL_SYSMODULE_SYSUTIL_AVCHAT2";
 	case 0xf034: return "CELL_SYSMODULE_SAIL_REC";
 	case 0xf035: return "CELL_SYSMODULE_SYSUTIL_NP_TROPHY";
+	case 0xf044: return "CELL_SYSMODULE_SYSUTIL_NP_EULA";
 	case 0xf054: return "CELL_SYSMODULE_LIBATRAC3MULTI";
 	case 0xffff: return "CELL_SYSMODULE_INVALID";
 	}
