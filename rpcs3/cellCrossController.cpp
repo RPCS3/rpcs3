e7c245eb5e31735876588c9d31ca17749c1733bd
Ani <ani-leo@outlook.com>
2017-03-25 14:25:24 +0000

    Register HLE libmedi, cellCrossController, cellSysutilNpEula
    Motorstorm Apocalypse calls for cellMediatorGetSignatureLength,
    cellMediatorCreateContext, cellMediatorGetProviderUrl,
    cellMediatorGetStatus
    
    LittleBigPlanet 2 and 3 may call for 0x37E1F502 (unknown name) on
    cellCrossController
    
    Resistance 3 and Uncharted 2 may call for the functions registered
    on cellSysutilNpEula

---
+++ rpcs3/cellCrossController.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Emu/System.h"
+#include "Emu/Cell/PPUModule.h"
+
+logs::channel cellCrossController("cellCrossController", logs::level::notice);
+
+s32 cellCrossController_37E1F502() // LittleBigPlanet 2 and 3
+{
+	cellCrossController.todo("cellCrossController_37E1F502");
+	return CELL_OK;
+}
+
+
+DECLARE(ppu_module_manager::cellCrossController)("cellCrossController", []()
+{
+	REG_FNID(cellCrossController, 0x37E1F502, cellCrossController_37E1F502);
+});
---
+++ rpcs3/cellSysutilNpEula.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "Emu/System.h"
+#include "Emu/Cell/PPUModule.h"
+
+logs::channel cellSysutilNpEula("cellSysutilNpEula", logs::level::notice);
+
+s32 cellSysutilNpEula_59D1629A() // Resistance 3, Uncharted 2
+{
+	cellSysutilNpEula.todo("cellSysutilNpEula_59D1629A");
+	return CELL_OK;
+}
+
+s32 cellSysutilNpEula_5EC05AD8() // Resistance 3
+{
+	cellSysutilNpEula.todo("cellSysutilNpEula_5EC05AD8");
+	return CELL_OK;
+}
+
+s32 cellSysutilNpEula_6599500D() // Resistance 3, Uncharted 2
+{
+	cellSysutilNpEula.todo("cellSysutilNpEula_6599500D");
+	return CELL_OK;
+}
+
+
+DECLARE(ppu_module_manager::cellSysutilNpEula)("cellSysutilNpEula", []()
+{
+	REG_FNID(cellSysutilNpEula, 0x59D1629A, cellSysutilNpEula_59D1629A);
+	REG_FNID(cellSysutilNpEula, 0x5EC05AD8, cellSysutilNpEula_5EC05AD8);
+	REG_FNID(cellSysutilNpEula, 0x6599500D, cellSysutilNpEula_6599500D);
+});
