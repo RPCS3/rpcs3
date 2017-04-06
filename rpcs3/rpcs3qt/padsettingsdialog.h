74e806810dc1b6dd2e2c6ea3386c004e7da5ad2d
Yukariin <Yukariin@users.noreply.github.com>
2017-04-07 02:12:15 +0400

    [WIP] Update Qt interface (#2336)
    * Fix rpcs3qt Linux build
    
    * Files clean up
    
    * Add base MainWindow class
    
    * Add slot stubs
    
    * Update MainWindow::DecryptSPRXLibraries
    
    * Add SettingsDialog base class and tab stubs
    
    * Add CoreTab base layout
    
    * Add compile guards
    
    * Minor fixes
    
    * Add GraphicsTab base layout
    
    * Add OK button signal
    
    * Remove QML stuff
    
    * Fix indentation
    
    * Add AudioTab base layout
    
    * Add InputTab base layout
    
    * Fix layouts
    
    * Add MiscTab base layout
    
    * Fix layouts
    
    * Add NetworkingTab base layout
    
    * Add SystemTab base layout
    
    * Fix button layout in SettingsDialog
    
    * Make SettingsDialog resizable
    
    * Add base dock widget stubs
    
    * Add very base PadSettingsDialog layout
    
    * Add combo box entries
    
    * Abb LogFrame base layout
    
    * Fix indent
    
    * Abb GameListFrame base layout
    
    * Minor fixes
    
    * Add AutoPauseSettingsDialog base layout

---
+++ rpcs3/rpcs3qt/padsettingsdialog.h
@@ -0,0 +1,14 @@
+#ifndef PADSETTINGS_H
+#define PADSETTINGS_H
+
+#include <QDialog>
+
+class PadSettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+        explicit PadSettingsDialog(QWidget *parent = 0);
+};
+
+#endif // PADSETTINGS_H
