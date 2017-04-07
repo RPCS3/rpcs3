a1cc40e7b1142f40197d203cd6d56647b4df1d27
scribam <scribam@users.noreply.github.com>
2017-04-07 14:48:59 +0200

    Add cellLibprof module (#2637)

---
+++ rpcs3/Emu/Cell/Modules/cellLibprof.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Emu/Cell/PPUModule.h"
+
+logs::channel cellLibprof("cellLibprof", logs::level::notice);
+
+s32 cellUserTraceInit()
+{
+	UNIMPLEMENTED_FUNC(cellLibprof);
+	return CELL_OK;
+}
+
+s32 cellUserTraceRegister()
+{
+	UNIMPLEMENTED_FUNC(cellLibprof);
+	return CELL_OK;
+}
+
+s32 cellUserTraceUnregister()
+{
+	UNIMPLEMENTED_FUNC(cellLibprof);
+	return CELL_OK;
+}
+
+s32 cellUserTraceTerminate()
+{
+	UNIMPLEMENTED_FUNC(cellLibprof);
+	return CELL_OK;
+}
+
+DECLARE(ppu_module_manager::cellLibprof)("cellLibprof", []()
+{
+	REG_FUNC(cellLibprof, cellUserTraceInit);
+	REG_FUNC(cellLibprof, cellUserTraceRegister);
+	REG_FUNC(cellLibprof, cellUserTraceUnregister);
+	REG_FUNC(cellLibprof, cellUserTraceTerminate);
+});
