95c6278863e4fa508f6e7b8e416ebeee58115c50
Nekotekina <nekotekina@gmail.com>
2017-04-28 18:20:46 +0300

    Organize cellDaisy

--- rpcs3/Emu/Cell/Modules/cellDaisy.cpp
+++ rpcs3/Emu/Cell/Modules/cellDaisy.cpp
@@ -5,262 +5,122 @@
 
 logs::channel cellDaisy("cellDaisy", logs::level::notice);
 
-s32 _ZN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE()
+using LFQueue2 = struct CellDaisyLFQueue2;
+using Lock = struct CellDaisyLock;
+using ScatterGatherInterlock = struct CellDaisyScatterGatherInterlock;
+using AtomicInterlock = volatile struct CellDaisyAtomicInterlock;
+
+s32 cellDaisyLFQueue2GetPopPointer(vm::ptr<LFQueue2> queue, vm::ptr<s32> pPointer, u32 isBlocking)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij()
+s32 cellDaisyLFQueue2CompletePopPointer(vm::ptr<LFQueue2> queue, s32 pointer, vm::ptr<s32(vm::ptr<void>, u32)> fpSendSignal, u32 isQueueFull)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy4Lock15completeConsumeEj()
+void cellDaisyLFQueue2PushOpen(vm::ptr<LFQueue2> queue)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy9_snprintfEPcjPKcz()
+s32 cellDaisyLFQueue2PushClose(vm::ptr<LFQueue2> queue, vm::ptr<s32(vm::ptr<void>, u32)> fpSendSignal)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy4Lock7popOpenEv()
+void cellDaisyLFQueue2PopOpen(vm::ptr<LFQueue2> queue)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj()
+s32 cellDaisyLFQueue2PopClose(vm::ptr<LFQueue2> queue, vm::ptr<s32(vm::ptr<void>, u32)> fpSendSignal)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy22ScatterGatherInterlock7releaseEv()
+s32 cellDaisyLFQueue2HasUnfinishedConsumer(vm::ptr<LFQueue2> queue, u32 isCancelled)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy4Lock7popOpenEv()
+s32 cellDaisy_snprintf(vm::ptr<char> buffer, u32 count, vm::cptr<char> fmt, ppu_va_args_t fmt_args)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh()
+s32 cellDaisyLock_initialize(vm::ptr<Lock> _this, u32 depth)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh()
+s32 cellDaisyLock_getNextHeadPointer(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy9_snprintfEPcjPKcz()
+s32 cellDaisyLock_getNextTailPointer(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej()
+s32 cellDaisyLock_completeConsume(vm::ptr<Lock> _this, u32 pointer)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy4Lock18getNextHeadPointerEv()
+s32 cellDaisyLock_completeProduce(vm::ptr<Lock> _this, u32 pointer)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy4Lock10initializeEj()
+s32 cellDaisyLock_pushOpen(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE()
+s32 cellDaisyLock_pushClose(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh()
+s32 cellDaisyLock_popOpen(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy4Lock15completeProduceEj()
+s32 cellDaisyLock_popClose(vm::ptr<Lock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E()
+void cellDaisyScatterGatherInterlock_1(vm::ptr<ScatterGatherInterlock> _this, vm::ptr<AtomicInterlock> ea, u32 size, vm::ptr<void> eaSignal, vm::ptr<s32(vm::ptr<void>, u32)> fpSendSignal)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE()
+void cellDaisyScatterGatherInterlock_2(vm::ptr<ScatterGatherInterlock> _this, u32 size, vm::ptr<u32> ids, u32 numSpus, u8 spup)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy4Lock8popCloseEv()
+void cellDaisyScatterGatherInterlock_9tor(vm::ptr<ScatterGatherInterlock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy4Lock8popCloseEv()
+s32 cellDaisyScatterGatherInterlock_probe(vm::ptr<ScatterGatherInterlock> _this, u32 isBlocking)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _ZN4cell5Daisy22ScatterGatherInterlockD2Ev()
+s32 cellDaisyScatterGatherInterlock_release(vm::ptr<ScatterGatherInterlock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
 
-s32 _QN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy4Lock18getNextTailPointerEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy4Lock8pushOpenEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy4Lock9pushCloseEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlockD2Ev()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy22ScatterGatherInterlockD1Ev()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock10initializeEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock15completeProduceEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy4Lock15completeConsumeEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock18getNextHeadPointerEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock9pushCloseEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy22ScatterGatherInterlock5probeEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock8pushOpenEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlock5probeEj()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlockD1Ev()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _ZN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy4Lock18getNextTailPointerEv()
-{
-	fmt::throw_exception("Unimplemented" HERE);
-}
-
-s32 _QN4cell5Daisy22ScatterGatherInterlock7releaseEv()
+void cellDaisyScatterGatherInterlock_proceedSequenceNumber(vm::ptr<ScatterGatherInterlock> _this)
 {
 	fmt::throw_exception("Unimplemented" HERE);
 }
@@ -268,56 +128,58 @@ s32 _QN4cell5Daisy22ScatterGatherInterlock7releaseEv()
 
 DECLARE(ppu_module_manager::cellDaisy)("cellDaisy", []()
 {
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock15completeConsumeEj);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy9_snprintfEPcjPKcz);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock7popOpenEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlock7releaseEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock7popOpenEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy9_snprintfEPcjPKcz);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock18getNextHeadPointerEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock10initializeEj);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock15completeProduceEj);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock8popCloseEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock8popCloseEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockD2Ev);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock18getNextTailPointerEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock8pushOpenEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock9pushCloseEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockD2Ev);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockD1Ev);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock10initializeEj);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock15completeProduceEj);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy4Lock15completeConsumeEj);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock18getNextHeadPointerEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock9pushCloseEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy22ScatterGatherInterlock5probeEj);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock8pushOpenEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlock5probeEj);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlockD1Ev);
-	REG_FUNC(cellDaisy, _ZN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy4Lock18getNextTailPointerEv);
-	REG_FUNC(cellDaisy, _QN4cell5Daisy22ScatterGatherInterlock7releaseEv);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE", cellDaisyLFQueue2PushClose);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy17LFQueue2PushCloseEPNS0_8LFQueue2EPFiPvjE", cellDaisyLFQueue2PushClose);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij", cellDaisyLFQueue2GetPopPointer);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy21LFQueue2GetPopPointerEPNS0_8LFQueue2EPij", cellDaisyLFQueue2GetPopPointer);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj", cellDaisyLFQueue2CompletePopPointer);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy26LFQueue2CompletePopPointerEPNS0_8LFQueue2EiPFiPvjEj", cellDaisyLFQueue2CompletePopPointer);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej", cellDaisyLFQueue2HasUnfinishedConsumer);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy29LFQueue2HasUnfinishedConsumerEPNS0_8LFQueue2Ej", cellDaisyLFQueue2HasUnfinishedConsumer);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E", cellDaisyLFQueue2PushOpen);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy16LFQueue2PushOpenEPNS0_8LFQueue2E", cellDaisyLFQueue2PushOpen);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE", cellDaisyLFQueue2PopClose);	
+	REG_FNID(cellDaisy, "_QN4cell5Daisy16LFQueue2PopCloseEPNS0_8LFQueue2EPFiPvjE", cellDaisyLFQueue2PopClose);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E", cellDaisyLFQueue2PopOpen);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy15LFQueue2PopOpenEPNS0_8LFQueue2E", cellDaisyLFQueue2PopOpen);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy9_snprintfEPcjPKcz", cellDaisy_snprintf);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy9_snprintfEPcjPKcz", cellDaisy_snprintf);
+
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock7popOpenEv", cellDaisyLock_popOpen);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock7popOpenEv", cellDaisyLock_popOpen);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock18getNextHeadPointerEv", cellDaisyLock_getNextHeadPointer);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock18getNextHeadPointerEv", cellDaisyLock_getNextHeadPointer);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock10initializeEj", cellDaisyLock_initialize);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock10initializeEj", cellDaisyLock_initialize);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock15completeProduceEj", cellDaisyLock_completeProduce);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock15completeProduceEj", cellDaisyLock_completeProduce);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock8popCloseEv", cellDaisyLock_popClose);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock8popCloseEv", cellDaisyLock_popClose);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock18getNextTailPointerEv", cellDaisyLock_getNextTailPointer);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock18getNextTailPointerEv", cellDaisyLock_getNextTailPointer);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock8pushOpenEv", cellDaisyLock_pushOpen);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock8pushOpenEv", cellDaisyLock_pushOpen);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock9pushCloseEv", cellDaisyLock_pushClose);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock9pushCloseEv", cellDaisyLock_pushClose);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy4Lock15completeConsumeEj", cellDaisyLock_completeConsume);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy4Lock15completeConsumeEj", cellDaisyLock_completeConsume);
+
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh", cellDaisyScatterGatherInterlock_1);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPjjh", cellDaisyScatterGatherInterlock_1);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh", cellDaisyScatterGatherInterlock_1);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPjjh", cellDaisyScatterGatherInterlock_1);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE", cellDaisyScatterGatherInterlock_2);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockC1EPVNS0_16_AtomicInterlockEjPvPFiS5_jE", cellDaisyScatterGatherInterlock_2);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE", cellDaisyScatterGatherInterlock_2);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockC2EPVNS0_16_AtomicInterlockEjPvPFiS5_jE", cellDaisyScatterGatherInterlock_2);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockD2Ev", cellDaisyScatterGatherInterlock_9tor);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockD2Ev", cellDaisyScatterGatherInterlock_9tor);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlockD1Ev", cellDaisyScatterGatherInterlock_9tor);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlockD1Ev", cellDaisyScatterGatherInterlock_9tor);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlock7releaseEv", cellDaisyScatterGatherInterlock_release);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlock7releaseEv", cellDaisyScatterGatherInterlock_release);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv", cellDaisyScatterGatherInterlock_proceedSequenceNumber);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlock21proceedSequenceNumberEv", cellDaisyScatterGatherInterlock_proceedSequenceNumber);
+	REG_FNID(cellDaisy, "_ZN4cell5Daisy22ScatterGatherInterlock5probeEj", cellDaisyScatterGatherInterlock_probe);
+	REG_FNID(cellDaisy, "_QN4cell5Daisy22ScatterGatherInterlock5probeEj", cellDaisyScatterGatherInterlock_probe);
 });
