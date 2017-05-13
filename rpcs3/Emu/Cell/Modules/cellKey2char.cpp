aca5dc08d095a3b5046931967bf114944b004b6c
scribam <scribam@users.noreply.github.com>
2017-05-14 00:55:07 +0200

    Update cellKey2char

--- rpcs3/Emu/Cell/Modules/cellKey2char.cpp
+++ rpcs3/Emu/Cell/Modules/cellKey2char.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "Emu/Cell/PPUModule.h"
 
+namespace vm { using namespace ps3; }
+
 logs::channel cellKey2char("cellKey2char", logs::level::notice);
 
 // Return Codes
-enum
+enum CellKey2CharError : u32
 {
 	CELL_K2C_OK                        = 0x00000000,
 	CELL_K2C_ERROR_FATAL               = 0x80121301,
@@ -15,34 +17,61 @@ enum
 	CELL_K2C_ERROR_OTHER               = 0x80121306,
 };
 
-s32 cellKey2CharOpen()
+struct CellKey2CharKeyData
 {
-	UNIMPLEMENTED_FUNC(cellKey2char);
-	return CELL_OK;
+	be_t<u32> led;
+	be_t<u32> mkey;
+	be_t<u16> keycode;
+};
+
+template<>
+void fmt_class_string<CellKey2CharError>::format(std::string& out, u64 arg)
+{
+	format_enum(out, arg, [](auto error)
+	{
+		switch (error)
+		{
+			STR_CASE(CELL_K2C_OK);
+			STR_CASE(CELL_K2C_ERROR_FATAL);
+			STR_CASE(CELL_K2C_ERROR_INVALID_HANDLE);
+			STR_CASE(CELL_K2C_ERROR_INVALID_PARAMETER);
+			STR_CASE(CELL_K2C_ERROR_ALREADY_INITIALIZED);
+			STR_CASE(CELL_K2C_ERROR_UNINITIALIZED);
+			STR_CASE(CELL_K2C_ERROR_OTHER);
+		}
+
+		return unknown;
+	});
 }
 
-s32 cellKey2CharClose()
+error_code cellKey2CharOpen(vm::ptr<u8> handle)
 {
-	UNIMPLEMENTED_FUNC(cellKey2char);
-	return CELL_OK;
+	cellKey2char.todo("cellKey2CharOpen(handle=*0x%x)", handle);
+	return CELL_K2C_OK;
 }
 
-s32 cellKey2CharGetChar()
+error_code cellKey2CharClose(vm::ptr<u8> handle)
 {
-	UNIMPLEMENTED_FUNC(cellKey2char);
-	return CELL_OK;
+	cellKey2char.todo("cellKey2CharClose(handle=*0x%x)", handle);
+	return CELL_K2C_OK;
 }
 
-s32 cellKey2CharSetMode()
+error_code cellKey2CharGetChar(vm::ptr<u8> handle, vm::ptr<CellKey2CharKeyData> kdata, vm::pptr<u16> charCode, vm::ptr<u32> charNum, vm::ptr<b8> processed)
 {
-	UNIMPLEMENTED_FUNC(cellKey2char);
-	return CELL_OK;
+	cellKey2char.todo("cellKey2CharGetChar(handle=*0x%x, kdata=*0x%x, charCode=**0x%x, charNum=*0x%x, processed=*0x%x)", handle, kdata, charCode, charNum, processed);
+	return CELL_K2C_OK;
 }
 
-s32 cellKey2CharSetArrangement()
+error_code cellKey2CharSetMode(vm::ptr<u8> handle, s32 mode)
 {
-	UNIMPLEMENTED_FUNC(cellKey2char);
-	return CELL_OK;
+	cellKey2char.todo("cellKey2CharSetMode(handle=*0x%x, mode=0x%x)", handle, mode);
+	return CELL_K2C_OK;
+}
+
+error_code cellKey2CharSetArrangement(vm::ptr<u8> handle, s32 arrange)
+{
+	cellKey2char.todo("cellKey2CharSetArrangement(handle=*0x%x, arrange=0x%x)", handle, arrange);
+	return CELL_K2C_OK;
 }
 
 DECLARE(ppu_module_manager::cellKey2char)("cellKey2char", []()
