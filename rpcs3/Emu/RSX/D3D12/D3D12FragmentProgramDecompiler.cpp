5b76e527c1c08c7a0e48bb1e274a53d31bf0c2ae
shameful <shameful@users.noreply.github.com>
2017-05-13 20:37:11 -0500

    directx fog_mode exponential2 fix
    misplaced parameter

--- rpcs3/Emu/RSX/D3D12/D3D12FragmentProgramDecompiler.cpp
+++ rpcs3/Emu/RSX/D3D12/D3D12FragmentProgramDecompiler.cpp
@@ -164,7 +164,7 @@ namespace
 			OS << "	float4 fogc = float4(11.084 * (fog_param1 * In.fogc.x + fog_param0 - 1.5), exp(11.084 * (fog_param1 * In.fogc.x + fog_param0 - 1.5)), 0., 0.);\n";
 			break;
 		case rsx::fog_mode::exponential2:
-			OS << "	float4 fogc = float4(4.709 * (fog_param1 * In.fogc.x + fog_param0 - 1.5), exp(-pow(4.709 * (fog_param1 * In.fogc.x + fog_param0 - 1.5)), 2.)), 0., 0.);\n";
+			OS << "	float4 fogc = float4(4.709 * (fog_param1 * In.fogc.x + fog_param0 - 1.5), exp(-pow(4.709 * (fog_param1 * In.fogc.x + fog_param0 - 1.5), 2.)), 0., 0.);\n";
 			break;
 		case rsx::fog_mode::linear_abs:
 			OS << "	float4 fogc = float4(fog_param1 * abs(In.fogc.x) + (fog_param0 - 1.), fog_param1 * abs(In.fogc.x) + (fog_param0 - 1.), 0., 0.);\n";
