b856d45e14f5477edd764f103a0f219fc76b7e14
flash-fire <mudroch.robert@gmail.com>
2017-05-28 22:49:43 -0500

    Reverting Mega's suggestion.  If people can use alt-f4 to get around this dialog, they can probably use an emulator too.

--- rpcs3/rpcs3qt/welcome_dialog.h
+++ rpcs3/rpcs3qt/welcome_dialog.h
@@ -11,23 +11,6 @@ class welcome_dialog : public QDialog
 
 public:
 	explicit welcome_dialog(QWidget* parent = nullptr);
-private:
-	void keyPressEvent(QKeyEvent* event)
-	{
-		// this won't work with Alt+F4, the window still closes
-		if (event->key() == Qt::Key_Escape)
-		{
-			event->ignore();
-		}
-	}
-	void closeEvent(QCloseEvent* event)
-	{
-		// spontaneous: don't close on external system level events like Alt+F4
-		if (event->spontaneous())
-		{
-			event->ignore();
-		}
-	}
 };
 
 #endif
