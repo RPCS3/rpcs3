57bab2702f09e69a9d5b7f0d31905bee41e70460
Zion Nimchuk <zionnimchuk@gmail.com>
2017-06-13 17:51:02 -0700

    Fix annoying cmake output.

--- rpcs3/cmake_modules/ConfigureCompiler.cmake
+++ rpcs3/cmake_modules/ConfigureCompiler.cmake
@@ -14,22 +14,13 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 	if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.1)
 		message( FATAL_ERROR "RPCS3 requires at least gcc-5.1." )
 	endif()
-	# FIXME: do we really need this?
-	# GCC 6.1 is insufficient to compile, because of a regression bug
-	#if(GCC_MAJOR EQUAL "6" AND GCC_MINOR EQUAL "1")
-	#	message(FATAL_ERROR
-	#		"GCC ${CMAKE_C_COMPILER_VERSION} is insufficient to build this project! "
-	#		"If you need to compile with GCC, use GCC 5.4 or lower or GCC 6.2 or higher "
-	#		"to build the project, or use CLANG instead. "
-	#		"See this regression bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70869")
-	#endif()
 
 	# Set compiler options here
 
 	# Warnings
 	add_compile_options(-Wno-attributes -Wno-enum-compare -Wno-invalid-offsetof)
 
-elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
 	# Clang 3.4 and lower are too old
 	#if(CLANG_VERSION_MAJOR LESS "3" AND CLANG_VERSION_MINOR LESS "4")
 	if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.4)
