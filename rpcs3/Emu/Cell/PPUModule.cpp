f2ae336474ad2ad67a12a8937fa2e56356b911e4
Jake <jarveson@gmail.com>
2017-06-23 13:40:13 -0500

    add to auto lle

--- rpcs3/Emu/Cell/PPUModule.cpp
+++ rpcs3/Emu/Cell/PPUModule.cpp
@@ -1168,6 +1168,7 @@ void ppu_load_exec(const ppu_exec_object& elf)
 			{ "cellFont", "libfont.sprx" },
 			{ "cellFontFT", "libfontFT.sprx" },
 			{ "cellFontFT", "libfreetype.sprx" },
+            { "cellGcmSys", "libgcm_sys.sprx"},
 			{ "cellGifDec", "libgifdec.sprx" },
 			{ "cellGifDec", "libsre.sprx" },
 			{ "cellJpgDec", "libjpgdec.sprx" },
