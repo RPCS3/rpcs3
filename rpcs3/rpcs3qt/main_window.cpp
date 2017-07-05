aa392a854bac09c1bbf5b4036815e4667fe15d76
scribam <scribam@users.noreply.github.com>
2017-07-05 17:07:32 +0200

    Check if the pth is a valid directory before to look for icons.

--- rpcs3/rpcs3qt/main_window.cpp
+++ rpcs3/rpcs3qt/main_window.cpp
@@ -131,9 +131,11 @@ void main_window::SetAppIconFromPath(const std::string path)
 	// get Icon for the gs_frame from path. this handles presumably all possible use cases
 	QString qpath = qstr(path);
 	std::string icon_list[] = { "/ICON0.PNG", "/PS3_GAME/ICON0.PNG" };
-	std::string path_list[] = { path, sstr(qpath.section("/", 0, -2)) ,sstr(qpath.section("/", 0, -3)) };
+	std::string path_list[] = { path, sstr(qpath.section("/", 0, -2)), sstr(qpath.section("/", 0, -3)) };
 	for (std::string pth : path_list)
 	{
+		if (!fs::is_dir(pth)) continue;
+
 		for (std::string ico : icon_list)
 		{
 			ico = pth + ico;
