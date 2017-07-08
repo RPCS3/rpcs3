b7aed7bbde3e4e59a44a4c9d18b454502432d539
Megamouse <studienricky89@googlemail.com>
2017-07-08 15:56:49 +0200

    Dagimon super mega level
    add stretchToDisplayArea to settingsdialog
    
    fix color picker and toolbar background
    
    settingsdialog adjustments
    
    fix tooltips + .pup
    
    fix adapter box

--- rpcs3/rpcs3qt/gui_settings.h
+++ rpcs3/rpcs3qt/gui_settings.h
@@ -52,6 +52,9 @@ namespace GUI
 	const QString meta        = "Meta";
 	const QString fs          = "FileSystem";
 
+	const QColor mw_tool_bar_color = QColor(227, 227, 227, 255);
+	const QColor gl_icon_color     = QColor(209, 209, 209, 255);
+
 	const GUI_SAVE rg_freeze  = GUI_SAVE(main_window, "recentGamesFrozen", false);
 	const GUI_SAVE rg_entries = GUI_SAVE(main_window, "recentGamesNames", QVariant::fromValue(q_pair_list()));
 
@@ -70,7 +73,7 @@ namespace GUI
 	const GUI_SAVE mw_logger         = GUI_SAVE( main_window, "loggerVisible",   true );
 	const GUI_SAVE mw_gamelist       = GUI_SAVE( main_window, "gamelistVisible", true );
 	const GUI_SAVE mw_toolBarVisible = GUI_SAVE( main_window, "toolBarVisible",  true );
-	const GUI_SAVE mw_toolBarColor   = GUI_SAVE( main_window, "toolBarColor",    QColor(227, 227, 227, 255));
+	const GUI_SAVE mw_toolBarColor   = GUI_SAVE( main_window, "toolBarColor",    mw_tool_bar_color);
 	const GUI_SAVE mw_geometry       = GUI_SAVE( main_window, "geometry",        QByteArray() );
 	const GUI_SAVE mw_windowState    = GUI_SAVE( main_window, "windowState",     QByteArray() );
 	const GUI_SAVE mw_mwState        = GUI_SAVE( main_window, "wwState",         QByteArray() );
@@ -87,7 +90,7 @@ namespace GUI
 	const GUI_SAVE gl_sortCol        = GUI_SAVE( game_list, "sortCol",        1 );
 	const GUI_SAVE gl_state          = GUI_SAVE( game_list, "state",          QByteArray() );
 	const GUI_SAVE gl_iconSize       = GUI_SAVE( game_list, "iconSize",       gl_icon_key_small );
-	const GUI_SAVE gl_iconColor      = GUI_SAVE( game_list, "iconColor",      QColor(209, 209, 209, 255));
+	const GUI_SAVE gl_iconColor      = GUI_SAVE( game_list, "iconColor",      gl_icon_color);
 	const GUI_SAVE gl_listMode       = GUI_SAVE( game_list, "listMode",       true );
 	const GUI_SAVE gl_textFactor     = GUI_SAVE( game_list, "textFactor",     (qreal) 2.0 );
 	const GUI_SAVE gl_marginFactor   = GUI_SAVE( game_list, "marginFactor",   (qreal) 0.09 );
