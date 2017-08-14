7096ca025fa1df92d86e38299a683b38ee05b320
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-08-14 01:28:39 +0100

    Commented out branch as it's still buggy
    The git-version.h file reverts whenever you rebuild and I don't know
    why.

--- rpcs3/rpcs3qt/gs_frame.cpp
+++ rpcs3/rpcs3qt/gs_frame.cpp
@@ -23,11 +23,11 @@ gs_frame::gs_frame(const QString& title, int w, int h, QIcon appIcon, bool disab
 	version = version.substr(version.find_last_of('-') + 1, version.length());
 	version = version.substr(0 , version.find_last_of(' '));
 
-	if (RPCS3_GIT_BRANCH != "master")
+	/*if (RPCS3_GIT_BRANCH != "master")
 	{
 		version += "-";
 		version += RPCS3_GIT_BRANCH;
-	}
+	}*/
 
 	//Add the branch name (Unless it's master)
 
