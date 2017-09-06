92e97e3286dda6b5c1dfb1cb79d09b10770505f2
scribam <scribam@users.noreply.github.com>
2017-09-06 22:06:00 +0200

    [Qt] fix small buttons/missing icons in the menu bar

--- rpcs3/rpcs3qt/main_window.cpp
+++ rpcs3/rpcs3qt/main_window.cpp
@@ -85,7 +85,11 @@ void main_window::Init()
 
 	// for highdpi resize toolbar icons and height dynamically
 	// choose factors to mimic Gui-Design in main_window.ui
-	const int toolBarHeight = menuBar()->sizeHint().height() * 1.5;
+	int toolBarHeight = menuBar()->sizeHint().height() * 1.5;
+	if (toolBarHeight <= 0)
+	{
+		toolBarHeight = menuBar()->height() * 1.5;
+	}
 	ui->toolBar->setIconSize(QSize(toolBarHeight, toolBarHeight));
 	ui->sizeSliderContainer->setFixedWidth(toolBarHeight * 5);
 	ui->sizeSlider->setFixedHeight(toolBarHeight * 0.65f);
