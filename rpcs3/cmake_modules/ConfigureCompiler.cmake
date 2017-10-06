bdffd7679286ded569094e9ea04cfc04e9018dad
scribam <scribam@users.noreply.github.com>
2017-10-06 20:52:08 +0200

    Change compiler requirements in cmake script to match with the values listed in the README.md file

--- rpcs3/cmake_modules/ConfigureCompiler.cmake
+++ rpcs3/cmake_modules/ConfigureCompiler.cmake
@@ -2,17 +2,13 @@ cmake_minimum_required(VERSION 2.8.12)
 # Check and configure compiler options for RPCS3
 
 if(CMAKE_COMPILER_IS_GNUCXX)
-
 	# GCC 4.9 and lower are too old
-	if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9.0)
-		message(FATAL_ERROR
-			"GCC ${CMAKE_CXX_COMPILER_VERSION} is too old.")
+	if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.1)
+		message(FATAL_ERROR "RPCS3 requires at least gcc-5.1.")
 	endif()
-	if (CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 6.1)
-		message( FATAL_ERROR "RPCS3 can't be compiled with gcc-6.1, see #1691." )
-	endif()
-	if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.1)
-		message( FATAL_ERROR "RPCS3 requires at least gcc-5.1." )
+	# GCC 6.1 is blacklisted
+	if(CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 6.1)
+		message(FATAL_ERROR "RPCS3 can't be compiled with gcc-6.1, see #1691.")
 	endif()
 
 	# Set compiler options here
@@ -22,11 +18,10 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 
 elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
 	# Clang 3.4 and lower are too old
-	#if(CLANG_VERSION_MAJOR LESS "3" AND CLANG_VERSION_MINOR LESS "4")
-	if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.4)
-		message(FATAL_ERROR
-			"Clang ${CMAKE_CXX_COMPILER_VERSION} is too old.")
+	if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)
+		message(FATAL_ERROR "RPCS3 requires at least clang-3.5.")
 	endif()
+
 	# Set compiler options here
 
 	add_compile_options(-ftemplate-depth=1024)
