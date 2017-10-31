8616c21a1947fbaebb3ea5c79621f3461a4c6e83
kd-11 <karokidii@gmail.com>
2017-10-31 18:45:54 +0300

    rsx: More fixes - Tag surface store to help determine when contents have been invalidated - Crop framebuffer textures if they are not the requested dimensions!

--- rpcs3/Emu/RSX/GL/GLGSRender.cpp
+++ rpcs3/Emu/RSX/GL/GLGSRender.cpp
@@ -358,6 +358,12 @@ void GLGSRender::end()
 		std::lock_guard<std::mutex> lock(m_sampler_mutex);
 		void* unused = nullptr;
 
+		if (surface_store_tag != m_rtts.cache_tag)
+		{
+			m_samplers_dirty.store(true);
+			surface_store_tag = m_rtts.cache_tag;
+		}
+
 		for (int i = 0; i < rsx::limits::fragment_textures_count; ++i)
 		{
 			if (m_samplers_dirty || m_textures_dirty[i])
