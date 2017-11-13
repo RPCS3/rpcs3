77a833f82b206825e8e96ef9fbca163f408cbb21
Zion Nimchuk <zionnimchuk@gmail.com>
2017-11-13 14:37:23 -0800

    Quick and dirty sys_tty_read implementation

--- rpcs3/Emu/Cell/lv2/sys_tty.cpp
+++ rpcs3/Emu/Cell/lv2/sys_tty.cpp
@@ -9,10 +9,19 @@ extern fs::file g_tty;
 
 error_code sys_tty_read(s32 ch, vm::ptr<char> buf, u32 len, vm::ptr<u32> preadlen)
 {
-	sys_tty.fatal("sys_tty_read(ch=%d, buf=*0x%x, len=%d, preadlen=*0x%x)", ch, buf, len, preadlen);
-
-	// We currently do not support reading from the Console
-	fmt::throw_exception("Unimplemented" HERE);
+	sys_tty.todo("sys_tty_read(ch=%d, buf=*0x%x, len=%d, preadlen=*0x%x)", ch, buf, len, preadlen);
+	std::string input = "insert string here";
+	if (input.length() > len)
+	{
+		*preadlen = len;
+		std::strncpy(buf.get_ptr(), input.c_str(), len);
+	}
+	else
+	{
+		*preadlen = input.length();
+		std::strcpy(buf.get_ptr(), input.c_str());
+	}
+	return CELL_OK;
 }
 
 error_code sys_tty_write(s32 ch, vm::cptr<char> buf, u32 len, vm::ptr<u32> pwritelen)
@@ -23,20 +32,20 @@ error_code sys_tty_write(s32 ch, vm::cptr<char> buf, u32 len, vm::ptr<u32> pwrit
 	{
 		return CELL_EINVAL;
 	}
-	
+
 	const u32 written_len = static_cast<s32>(len) > 0 ? len : 0;
 
 	if (written_len > 0 && g_tty)
 	{
 		g_tty.write(buf.get_ptr(), len);
 	}
-	
+
 	if (!pwritelen)
 	{
 		return CELL_EFAULT;
 	}
 
 	*pwritelen = written_len;
-	
+
 	return CELL_OK;
 }
