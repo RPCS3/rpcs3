c077426e95cd217ffad01cd60d9771f7fda30b94
Unknown <studienricky89@googlemail.com>
2017-12-01 00:06:31 +0100

    Input: Fix filter button for evdev and DS4

--- rpcs3/ds4_pad_handler.h
+++ rpcs3/ds4_pad_handler.h
@@ -154,7 +154,6 @@ private:
 
 private:
 	std::shared_ptr<DS4Device> GetDevice(const std::string& padId);
-	bool CheckDeviceState(std::shared_ptr<DS4Device> device);
 	void TranslateButtonPress(u64 keyCode, bool& pressed, u16& val, bool ignore_threshold = false) override;
 	void ProcessDataToPad(const std::shared_ptr<DS4Device>& ds4Device, const std::shared_ptr<Pad>& pad);
 	// Copies data into padData if status is NewData, otherwise buffer is untouched
