d03ec851c4cff45085c315f3bc148476158b6088
TGEnigma <smartychrisje@hotmail.com>
2018-01-11 09:39:03 +0100

    Add option to disable shader preloading

--- rpcs3/Emu/System.h
+++ rpcs3/Emu/System.h
@@ -342,6 +342,7 @@ struct cfg_root : cfg::node
 		cfg::_bool strict_rendering_mode{this, "Strict Rendering Mode"};
 		cfg::_bool disable_zcull_queries{this, "Disable ZCull Occlusion Queries", false};
 		cfg::_bool disable_vertex_cache{this, "Disable Vertex Cache", false};
+		cfg::_bool disable_on_disk_shader_cache{this, "Disable On-Disk Shader Cache", false};
 		cfg::_bool frame_skip_enabled{this, "Enable Frame Skip", false};
 		cfg::_bool force_cpu_blit_processing{this, "Force CPU Blit", false}; //Debugging option
 		cfg::_int<1, 8> consequtive_frames_to_draw{this, "Consecutive Frames To Draw", 1};
