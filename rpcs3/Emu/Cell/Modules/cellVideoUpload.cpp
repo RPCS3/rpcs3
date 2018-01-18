2821915eae0be7fb5e019f10a25f0fd00d6e68d5
scribam <scribam@users.noreply.github.com>
2018-01-18 20:15:28 +0100

    Add callbacks for cellVideoUpload

--- rpcs3/Emu/Cell/Modules/cellVideoUpload.cpp
+++ rpcs3/Emu/Cell/Modules/cellVideoUpload.cpp
@@ -2,12 +2,23 @@
 #include "Emu/Cell/PPUModule.h"
 
 #include "cellVideoUpload.h"
+#include "cellSysutil.h"
 
 logs::channel cellVideoUpload("cellVideoUpload");
 
-s32 cellVideoUploadInitialize(vm::cptr<CellVideoUploadParam> pParam, vm::ptr<CellVideoUploadCallback> cb, vm::ptr<void> userdata)
+error_code cellVideoUploadInitialize(vm::cptr<CellVideoUploadParam> pParam, vm::ptr<CellVideoUploadCallback> cb, vm::ptr<void> userdata)
 {
 	cellVideoUpload.todo("cellVideoUploadInitialize(pParam=*0x%x, cb=*0x%x, userdata=*0x%x)", pParam, cb, userdata);
+
+	sysutil_register_cb([=](ppu_thread& ppu) -> s32
+	{
+		vm::var<char[]> pResultURL(128);
+
+		cb(ppu, CELL_VIDEO_UPLOAD_STATUS_INITIALIZED, CELL_OK, pResultURL, userdata);
+		cb(ppu, CELL_VIDEO_UPLOAD_STATUS_FINALIZED, CELL_OK, pResultURL, userdata);
+
+		return CELL_OK;
+	});
 
 	return CELL_OK;
 }
