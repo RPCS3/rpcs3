a32e4b1d94e8d911f992593f3338cd10e8178018
Megamouse <studienricky89@googlemail.com>
2018-03-24 15:42:14 +0100

    stub cellVoice

--- rpcs3/Emu/Cell/Modules/cellVoice.h
+++ rpcs3/Emu/Cell/Modules/cellVoice.h
@@ -1,12 +1,10 @@
-#pragma once
-
-
+﻿#pragma once
 
 // libvoice = 0x80310801 - 0x803108ff
 // libvoice version 100
 
 // Error Codes
-enum
+enum CellVoiceError : u32
 {
 	CELL_VOICE_ERROR_ADDRESS_INVALID       = 0x8031080a,
 	CELL_VOICE_ERROR_ARGUMENT_INVALID      = 0x80310805,
@@ -28,30 +26,13 @@ enum
 	CELL_VOICE_ERROR_TOPOLOGY              = 0x80310807,
 };
 
-// Definitions
-enum
+enum CellVoiceAppType
 {
-	CELLVOICE_MAX_IN_VOICE_PORT = 32,
-	CELLVOICE_MAX_OUT_VOICE_PORT = 4,
-	CELLVOICE_GAME_1MB_MAX_IN_VOICE_PORT = 8,
-	CELLVOICE_GAME_1MB_MAX_OUT_VOICE_PORT = 2,
-	CELLVOICE_MAX_PORT = 128,
-	CELLVOICE_INVALID_PORT_ID = 0xff,
+	CELLVOICE_APPTYPE_GAME_1MB = 1 << 29
+};
 
-	CELLVOICE_PORTTYPE_NULL = -1,
-	CELLVOICE_PORTTYPE_IN_MIC = 0,
-	CELLVOICE_PORTTYPE_IN_PCMAUDIO = 1,
-	CELLVOICE_PORTTYPE_IN_VOICE = 2,
-	CELLVOICE_PORTTYPE_OUT_PCMAUDIO = 3,
-	CELLVOICE_PORTTYPE_OUT_VOICE = 4,
-	CELLVOICE_PORTTYPE_OUT_SECONDARY = 5,
-
-	CELLVOICE_PORTSTATE_NULL = -1,
-	CELLVOICE_PORTSTATE_IDLE = 0,
-	CELLVOICE_PORTSTATE_READY = 1,
-	CELLVOICE_PORTSTATE_BUFFERING = 2,
-	CELLVOICE_PORTSTATE_RUNNING = 3,
-
+enum CellVoiceBitRate
+{
 	CELLVOICE_BITRATE_NULL  = -1,
 	CELLVOICE_BITRATE_3850  = 3850,
 	CELLVOICE_BITRATE_4650  = 4650,
@@ -60,32 +41,139 @@ enum
 	CELLVOICE_BITRATE_14400 = 14400,
 	CELLVOICE_BITRATE_16000 = 16000,
 	CELLVOICE_BITRATE_22533 = 22533,
+};
 
-	CELLVOICE_SAMPLINGRATE_NULL = -1,
-	CELLVOICE_SAMPLINGRATE_16000 = 16000,
-
-	CELLVOICE_EVENT_DATA_ERROR = 1 << 0,
-	CELLVOICE_EVENT_PORT_ATTACHED = 1 << 1,
-	CELLVOICE_EVENT_PORT_DETACHED = 1 << 2,
-	CELLVOICE_EVENT_SERVICE_ATTACHED = 1 << 3,
-	CELLVOICE_EVENT_SERVICE_DETACHED = 1 << 4,
+enum CellVoiceEventType
+{
+	CELLVOICE_EVENT_DATA_ERROR         = 1 << 0,
+	CELLVOICE_EVENT_PORT_ATTACHED      = 1 << 1,
+	CELLVOICE_EVENT_PORT_DETACHED      = 1 << 2,
+	CELLVOICE_EVENT_SERVICE_ATTACHED   = 1 << 3,
+	CELLVOICE_EVENT_SERVICE_DETACHED   = 1 << 4,
 	CELLVOICE_EVENT_PORT_WEAK_ATTACHED = 1 << 5,
 	CELLVOICE_EVENT_PORT_WEAK_DETACHED = 1 << 6,
+};
 
-	CELLVOICE_PCM_NULL = -1,
-	CELLVOICE_PCM_FLOAT = 0,
-	CELLVOICE_PCM_FLOAT_LITTLE_ENDIAN = 1,
-	CELLVOICE_PCM_SHORT = 2,
-	CELLVOICE_PCM_SHORT_LITTLE_ENDIAN = 3,
-	CELLVOICE_PCM_INTEGER = 4,
+enum CellVoicePcmDataType
+{
+	CELLVOICE_PCM_NULL                  = -1,
+	CELLVOICE_PCM_FLOAT                 = 0,
+	CELLVOICE_PCM_FLOAT_LITTLE_ENDIAN   = 1,
+	CELLVOICE_PCM_SHORT                 = 2,
+	CELLVOICE_PCM_SHORT_LITTLE_ENDIAN   = 3,
+	CELLVOICE_PCM_INTEGER               = 4,
 	CELLVOICE_PCM_INTEGER_LITTLE_ENDIAN = 5,
+};
 
-	CELLVOICE_ATTR_ENERGY_LEVEL = 1000,
-	CELLVOICE_ATTR_VAD = 1001,
-	CELLVOICE_ATTR_DTX = 1002,
-	CELLVOICE_ATTR_AUTO_RESAMPLE = 1003,
-	CELLVOICE_ATTR_LATENCY = 1004,
+enum CellVoicePortAttr
+{
+	CELLVOICE_ATTR_ENERGY_LEVEL      = 1000,
+	CELLVOICE_ATTR_VAD               = 1001,
+	CELLVOICE_ATTR_DTX               = 1002,
+	CELLVOICE_ATTR_AUTO_RESAMPLE     = 1003,
+	CELLVOICE_ATTR_LATENCY           = 1004,
 	CELLVOICE_ATTR_SILENCE_THRESHOLD = 1005,
+};
 
-	CELLVOICE_APPTYPE_GAME_1MB = 1 << 29
+enum CellVoicePortState
+{
+	CELLVOICE_PORTSTATE_NULL      = -1,
+	CELLVOICE_PORTSTATE_IDLE      = 0,
+	CELLVOICE_PORTSTATE_READY     = 1,
+	CELLVOICE_PORTSTATE_BUFFERING = 2,
+	CELLVOICE_PORTSTATE_RUNNING   = 3,
+};
+
+enum CellVoicePortType
+{
+	CELLVOICE_PORTTYPE_NULL          = -1,
+	CELLVOICE_PORTTYPE_IN_MIC        = 0,
+	CELLVOICE_PORTTYPE_IN_PCMAUDIO   = 1,
+	CELLVOICE_PORTTYPE_IN_VOICE      = 2,
+	CELLVOICE_PORTTYPE_OUT_PCMAUDIO  = 3,
+	CELLVOICE_PORTTYPE_OUT_VOICE     = 4,
+	CELLVOICE_PORTTYPE_OUT_SECONDARY = 5,
+};
+
+enum CellVoiceSamplingRate
+{
+	CELLVOICE_SAMPLINGRATE_NULL  = -1,
+	CELLVOICE_SAMPLINGRATE_16000 = 16000,
+};
+
+enum CellVoiceVersionCheck
+{
+	CELLVOICE_VERSION_100 = 100
+};
+
+// Definitions
+enum
+{
+	CELLVOICE_MAX_IN_VOICE_PORT           = 32,
+	CELLVOICE_MAX_OUT_VOICE_PORT          = 4,
+	CELLVOICE_GAME_1MB_MAX_IN_VOICE_PORT  = 8,
+	CELLVOICE_GAME_1MB_MAX_OUT_VOICE_PORT = 2,
+	CELLVOICE_MAX_PORT                    = 128,
+	CELLVOICE_INVALID_PORT_ID             = 0xff,
+};
+
+struct CellVoiceBasePortInfo // aligned(64)
+{
+	be_t<CellVoicePortType> portType;
+	be_t<CellVoicePortState> state;
+	be_t<u16> numEdge;
+	vm::bptr<u32> pEdge;
+	be_t<u32> numByte;
+	be_t<u32> frameSize;
+};
+
+struct CellVoiceInitParam // aligned(32)
+{
+	be_t<CellVoiceEventType> eventMask;
+	be_t<CellVoiceVersionCheck> version;
+	be_t<s32> appType;
+	u8 reserved[32 - sizeof(s32) * 3];
+};
+
+struct CellVoicePCMFormat
+{
+	u8 numChannels;
+	u8 sampleAlignment;
+	be_t<CellVoicePcmDataType> dataType;
+	be_t<CellVoiceSamplingRate> sampleRate;
+};
+
+struct CellVoicePortParam // aligned(64)
+{
+	be_t<CellVoicePortType> portType;
+	be_t<u16> threshold;
+	be_t<u16> bMute;
+	be_t<f32> volume;
+	union
+	{
+		struct
+		{
+			be_t<CellVoiceBitRate> bitrate;
+		} voice;
+		struct
+		{
+			be_t<u32> bufSize;
+			CellVoicePCMFormat format;
+		} pcmaudio;
+		struct
+		{
+			be_t<u32> playerId;
+		} device;
+	};
+};
+
+struct CellVoiceStartParam // aligned(32)
+{
+	be_t<u32> container;
+	u8 reserved[32 - sizeof(s32) * 1];
+};
+
+struct voice_manager
+{
+	atomic_t<bool> is_init{ false };
 };
