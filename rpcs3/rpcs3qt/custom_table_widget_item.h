401fdd81e8f793a0340b764f4752e836bb1d15af
Megamouse <studienricky89@googlemail.com>
2018-04-07 19:52:42 +0200

    Qt: fix gamelist sort by adding a sort_role to custom_list_widget_item

--- rpcs3/rpcs3qt/custom_table_widget_item.h
+++ rpcs3/rpcs3qt/custom_table_widget_item.h
@@ -4,9 +4,20 @@
 
 class custom_table_widget_item : public QTableWidgetItem
 {
+private:
+	int m_sort_role = Qt::DisplayRole;
+
 public:
 	bool operator <(const QTableWidgetItem &other) const
 	{
-		return data(Qt::UserRole) < other.data(Qt::UserRole);
+		return data(m_sort_role) < other.data(m_sort_role);
+	}
+	void setData(int role, const QVariant &value, bool assign_sort_role = false)
+	{
+		if (assign_sort_role)
+		{
+			m_sort_role = role;
+		}
+		QTableWidgetItem::setData(role, value);
 	}
 };
