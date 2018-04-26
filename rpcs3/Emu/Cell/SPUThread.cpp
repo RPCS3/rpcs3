37d4c2cc7440298b0411c223f81f48b519954752
eladash <elad3356p@gmail.com>
2018-04-26 16:25:11 +0300

    RawSPU: fix interrupt mailbox

--- rpcs3/Emu/Cell/SPUThread.cpp
+++ rpcs3/Emu/Cell/SPUThread.cpp
@@ -332,7 +332,7 @@ const auto spu_putlluc_tx = build_function_asm<bool(*)(u32 raddr, const void* rd
 	c.ret();
 });
 
-void spu_int_ctrl_t::set(u64 ints)
+bool spu_int_ctrl_t::set(u64 ints)
 {
 	// leave only enabled interrupts
 	ints &= mask;
@@ -347,9 +347,12 @@ void spu_int_ctrl_t::set(u64 ints)
 			if (auto handler = tag->handler.lock())
 			{
 				handler->exec();
+				return true;
 			}
 		}
 	}
+
+	return false;
 }
 
 const spu_imm_table_t g_spu_imm;
@@ -1755,17 +1758,21 @@ bool spu_thread::set_ch_value(u32 ch, u32 value)
 	{
 		if (offset >= RAW_SPU_BASE_ADDR)
 		{
-			while (!ch_out_intr_mbox.try_push(value))
-			{
-				if (is_stopped())
-				{
-					return false;
-				}
+			ch_out_intr_mbox.data.store(spu_channel::bit_wait | spu_channel::bit_count | value);
 
-				thread_ctrl::wait();
+			if (int_ctrl[2].set(SPU_INT2_STAT_MAILBOX_INT))
+			{
+				while (ch_out_intr_mbox.get_count())
+				{
+					if (is_stopped())
+					{
+						return false;
+					}
+
+					thread_ctrl::wait();
+				}
 			}
 
-			int_ctrl[2].set(SPU_INT2_STAT_MAILBOX_INT);
 			return true;
 		}
 
