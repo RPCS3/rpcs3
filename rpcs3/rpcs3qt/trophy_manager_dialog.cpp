325048a134d859a26a8b8143a64db268c510af89
Megamouse <studienricky89@googlemail.com>
2018-05-22 11:38:02 +0200

    Qt: apply header states after populating the tables

--- rpcs3/rpcs3qt/trophy_manager_dialog.cpp
+++ rpcs3/rpcs3qt/trophy_manager_dialog.cpp
@@ -250,6 +250,8 @@ trophy_manager_dialog::trophy_manager_dialog(std::shared_ptr<gui_settings> gui_s
 		m_splitter->setSizes({ width_left, width_right });
 	}
 
+	PopulateUI();
+
 	QByteArray game_table_state = m_gui_settings->GetValue(gui::tr_games_state).toByteArray();
 	if (!m_game_table->horizontalHeader()->restoreState(game_table_state) && m_game_table->rowCount())
 	{
@@ -266,7 +268,6 @@ trophy_manager_dialog::trophy_manager_dialog(std::shared_ptr<gui_settings> gui_s
 		m_trophy_table->horizontalHeader()->resizeSections(QHeaderView::ResizeMode::ResizeToContents);
 	}
 
-	PopulateUI();
 	ApplyFilter();
 
 	// Make connects
