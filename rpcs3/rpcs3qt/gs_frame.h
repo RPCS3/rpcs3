b8c54377ba075498e0720322ef71a50a494e8fa2
Megamouse <studienricky89@googlemail.com>
2018-06-08 15:20:18 +0200

    Qt: set up taskbar progress for the gs_frame

--- rpcs3/rpcs3qt/gs_frame.h
+++ rpcs3/rpcs3qt/gs_frame.h
@@ -6,9 +6,30 @@
 #include <QWidget>
 #include <QWindow>
 
+#ifdef _WIN32
+#include <QWinTaskbarProgress>
+#include <QWinTaskbarButton>
+#include <QWinTHumbnailToolbar>
+#include <QWinTHumbnailToolbutton>
+#elif HAVE_QTDBUS
+#include <QtDBus/QDBusMessage>
+#include <QtDBus/QDBusConnection>
+#endif
+
 class gs_frame : public QWindow, public GSFrameBase
 {
 	Q_OBJECT
+
+private:
+	// taskbar progress
+	int m_gauge_max = 100;
+#ifdef _WIN32
+	QWinTaskbarButton* m_tb_button = nullptr;
+	QWinTaskbarProgress* m_tb_progress = nullptr;
+#elif HAVE_QTDBUS
+	int m_progress_value = 0;
+	void UpdateProgress(int progress, bool disable = false);
+#endif
 
 	u64 m_frames = 0;
 	QString m_windowTitle;
@@ -22,12 +43,19 @@ class gs_frame : public QWindow, public GSFrameBase
 
 public:
 	gs_frame(const QString& title, const QRect& geometry, QIcon appIcon, bool disableMouse);
+	~gs_frame();
 
 	draw_context_t make_context() override;
 	void set_current(draw_context_t context) override;
 	void delete_context(draw_context_t context) override;
 
 	wm_event get_default_wm_event() const override;
+
+	// taskbar progress
+	void progress_reset();
+	void progress_increment(int delta);
+	void progress_set_limit(int limit);
+
 protected:
 	virtual void paintEvent(QPaintEvent *event);
 	virtual void showEvent(QShowEvent *event) override;
@@ -51,6 +79,7 @@ protected:
 	bool nativeEvent(const QByteArray &eventType, void *message, long *result) override;
 
 	bool event(QEvent* ev) override;
+
 private Q_SLOTS:
 	void HandleCursor(QWindow::Visibility visibility);
 };
