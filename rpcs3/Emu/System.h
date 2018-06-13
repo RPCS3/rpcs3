af71cf09220d0783e78ce1692ea02a1c6edbec15
Megamouse <studienricky89@googlemail.com>
2018-06-13 13:54:16 +0200

    implement "Enable TSX" combobox

--- rpcs3/Emu/System.h
+++ rpcs3/Emu/System.h
@@ -155,6 +155,13 @@ enum class detail_level
 	high,
 };
 
+enum class tsx_usage
+{
+	disabled,
+	enabled,
+	forced,
+};
+
 enum CellNetCtlState : s32;
 enum CellSysutilLang : s32;
 
@@ -326,6 +333,7 @@ struct cfg_root : cfg::node
 		cfg::_bool spu_accurate_putlluc{this, "Accurate PUTLLUC", false};
 		cfg::_bool spu_verification{this, "SPU Verification", true}; // Should be enabled
 		cfg::_bool spu_cache{this, "SPU Cache", true};
+		cfg::_enum<tsx_usage> enable_TSX{this, "Enable TSX", tsx_usage::enabled}; // Enable TSX. Forcing this on Haswell/Broadwell CPUs should be used carefully
 
 		cfg::_enum<lib_loading_type> lib_loading{this, "Lib Loader", lib_loading_type::liblv2only};
 		cfg::_bool hook_functions{this, "Hook static functions"};
--- rpcs3/rpcs3qt/settings_dialog.cpp
+++ rpcs3/rpcs3qt/settings_dialog.cpp
@@ -20,6 +20,7 @@
 #include "stdafx.h"
 #include "Emu/System.h"
 #include "Crypto/unself.h"
+#include "Utilities/sysinfo.h"
 
 #include <unordered_set>
 #include <thread>
@@ -1079,6 +1080,40 @@ settings_dialog::settings_dialog(std::shared_ptr<gui_settings> guiSettings, std:
 
 	xemu_settings->EnhanceCheckBox(ui->spuDebug, emu_settings::SPUDebug);
 	SubscribeTooltip(ui->spuDebug, json_debug["spuDebug"].toString());
+		
+	if (utils::has_rtm())
+	{
+		xemu_settings->EnhanceComboBox(ui->enableTSX, emu_settings::EnableTSX);
+		SubscribeTooltip(ui->enableTSX, json_debug["enableTSX"].toString());
+
+		static const QString tsx_forced  = qstr(fmt::format("%s", tsx_usage::forced));
+		static const QString tsx_default = qstr(xemu_settings->GetSettingDefault(emu_settings::EnableTSX));
+
+		// connect the toogled signal so that the stateChanged signal in EnhanceCheckBox can be prevented
+		connect(ui->enableTSX, &QComboBox::currentTextChanged, [this](const QString& text)
+		{
+			if (text == tsx_forced && !utils::has_mpx() && QMessageBox::No == QMessageBox::critical(this, tr("Haswell/Broadwell TSX Warning"), tr(
+				R"(
+					<p style="white-space: nowrap;">
+						RPCS3 has detected you are using TSX functions on a Haswell or Broadwell CPU.<br>
+						Intel has deactivated these functions in newer Microcode revisions, since they can lead to unpredicted behaviour.<br>
+						That means using TSX may break games or even <font color="red"><b>damage</b></font> your data.<br>
+						We recommend to disable this feature and update your computer BIOS.<br><br>
+						Do you wish to use TSX anyway?
+					</p>
+				)"
+			), QMessageBox::Yes, QMessageBox::No))
+			{
+				// Reset if the messagebox was answered with no. This prevents the currentIndexChanged signal in EnhanceComboBox
+				ui->enableTSX->setCurrentText(tsx_default);
+			}
+		});
+	}
+	else
+	{
+		ui->label_enableTSX->setHidden(true);
+		ui->enableTSX->setHidden(true);
+	}
 
 	//
 	// Layout fix for High Dpi
