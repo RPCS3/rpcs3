2bc4cb4e5809229ebb3738f195df83a6df5d1ebf
Ofek <ofeklavie@gmail.com>
2018-06-16 15:11:38 +0300

    Fix sys_spu_thread_connect event

--- rpcs3/Emu/Cell/lv2/sys_spu.cpp
+++ rpcs3/Emu/Cell/lv2/sys_spu.cpp
@@ -1024,7 +1024,7 @@ error_code sys_spu_thread_connect_event(u32 id, u32 eq, u32 et, u8 spup)
 		return CELL_ESRCH;
 	}
 
-	if (et != SYS_SPU_THREAD_EVENT_USER || spup > 63 || queue->type != SYS_PPU_QUEUE)
+	if (et != SYS_SPU_THREAD_EVENT_USER || spup > 63)
 	{
 		sys_spu.error("sys_spu_thread_connect_event(): invalid arguments (et=%d, spup=%d, queue->type=%d)", et, spup, queue->type);
 		return CELL_EINVAL;
