eef900ef418dc95f1ffd4a38217ee04062ff2b8d
Megamouse <studienricky89@googlemail.com>
2018-06-20 02:11:50 +0200

    user-manager: megamouse fixes
    (cherry picked from commit 949807c1cc689e12e5f0cea367564306ea36a759)

--- rpcs3/rpcs3qt/user_manager_dialog.h
+++ rpcs3/rpcs3qt/user_manager_dialog.h
@@ -24,7 +24,7 @@ class user_manager_dialog : public QDialog
 {
 	Q_OBJECT
 public:
-	explicit user_manager_dialog(std::shared_ptr<gui_settings> gui_settings, std::shared_ptr<emu_settings> emu_settings, const std::string& dir = "", QWidget* parent = nullptr);
+	explicit user_manager_dialog(std::shared_ptr<gui_settings> gui_settings, std::shared_ptr<emu_settings> emu_settings, QWidget* parent = nullptr);
 Q_SIGNALS:
 	void OnUserLoginSuccess();
 private Q_SLOTS:
@@ -34,24 +34,21 @@ private Q_SLOTS:
 	void OnUserRename();
 	void OnSort(int logicalIndex);
 private:
-	void Init(const std::string& dir);
+	void Init();
 	void UpdateTable();
-	void GenerateUser(const std::string& username);
-	bool ValidateUsername(const QString& textToValidate);
+	void GenerateUser(const std::string& user_id, const std::string& username);
+	bool ValidateUsername(const QString& text_to_validate);
 
 	void ShowContextMenu(const QPoint &pos);
 
 	void closeEvent(QCloseEvent* event) override;
 
 	QTableWidget* m_table;
-	std::string m_dir;
-	std::string m_selected_user;
+	std::string m_active_user;
 	std::vector<UserAccount*> m_user_list;
 
 	std::shared_ptr<gui_settings> m_gui_settings;
 	std::shared_ptr<emu_settings> m_emu_settings;
-
-	QMenu* m_sort_options;
 
 	int m_sort_column;
 	bool m_sort_ascending;
