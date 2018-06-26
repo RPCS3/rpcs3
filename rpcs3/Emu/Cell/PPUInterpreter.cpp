f1e6532ec02ad7671103ce29bc842092f8a35e77
scribam <scribam@users.noreply.github.com>
2018-06-26 22:11:08 +0200

    ppu: improve fnmsub instruction

--- rpcs3/Emu/Cell/PPUInterpreter.cpp
+++ rpcs3/Emu/Cell/PPUInterpreter.cpp
@@ -4813,7 +4813,7 @@ bool ppu_interpreter::FMADD(ppu_thread& ppu, ppu_opcode_t op)
 
 bool ppu_interpreter::FNMSUB(ppu_thread& ppu, ppu_opcode_t op)
 {
-	ppu.fpr[op.frd] = -(ppu.fpr[op.fra] * ppu.fpr[op.frc]) + ppu.fpr[op.frb];
+	ppu.fpr[op.frd] = -(ppu.fpr[op.fra] * ppu.fpr[op.frc] - ppu.fpr[op.frb]);
 	if (UNLIKELY(op.rc)) fmt::throw_exception("%s: op.rc", __func__); //ppu_cr_set(ppu, 1, ppu.fpscr.fg, ppu.fpscr.fl, ppu.fpscr.fe, ppu.fpscr.fu);
 	return true;
 }
