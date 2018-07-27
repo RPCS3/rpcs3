0f36e87010b672b6c0fe33ec11cef94452410cde
kd-11 <karokidii@gmail.com>
2018-07-27 21:33:10 +0300

    zcull: Improve the delay algorithm to be more consistent - Use proper time checking; depending on what is being done one 'tick' can   be almost a millisecond long or several nanoseconds - Avoid spamming the system timer unless necessary

--- rpcs3/Emu/RSX/RSXThread.h
+++ rpcs3/Emu/RSX/RSXThread.h
@@ -258,9 +258,9 @@ namespace rsx
 
 		struct ZCULL_control
 		{
-			// Delay in 'cycles' before a report update operation is forced to retire
-			const u32 max_zcull_cycles_delay = 128;
-			const u32 min_zcull_cycles_delay = 16;
+			// Delay before a report update operation is forced to retire
+			const u32 max_zcull_delay_us = 500;
+			const u32 min_zcull_delay_us = 50;
 
 			// Number of occlusion query slots available. Real hardware actually has far fewer units before choking
 			const u32 occlusion_query_count = 128;
@@ -273,7 +273,7 @@ namespace rsx
 			occlusion_query_info* m_current_task = nullptr;
 			u32 m_statistics_tag_id = 0;
 			u64 m_tsc = 0;
-			u32 m_cycles_delay = max_zcull_cycles_delay;
+			u32 m_cycles_delay = max_zcull_delay_us;
 
 			std::vector<queued_report_write> m_pending_writes;
 			std::unordered_map<u32, u32> m_statistics_map;
