cf53a5f668467d0b1f502ae15a41ffde8a47c7db
Lassi Hämäläinen <lassi.m.hamalainen@gmail.com>
2018-08-25 12:42:37 +0300

    Add cstring include to Log.cpp

--- Utilities/Log.cpp
+++ Utilities/Log.cpp
@@ -1,4 +1,4 @@
-﻿#include "Log.h"
+#include "Log.h"
 #include "File.h"
 #include "StrFmt.h"
 #include "sema.h"
@@ -11,6 +11,7 @@
 #include <unordered_map>
 #include <thread>
 #include <chrono>
+#include <cstring>
 
 using namespace std::literals::chrono_literals;
 
