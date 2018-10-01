2d5c2d28899f99d1d2630129448f6cfaa820b4af
eladash <elad3356p@gmail.com>
2018-10-01 07:57:12 +0300

    sys_rsx: fix log spam

--- rpcs3/Emu/Cell/lv2/sys_rsx.cpp
+++ rpcs3/Emu/Cell/lv2/sys_rsx.cpp
@@ -232,8 +232,8 @@ s32 sys_rsx_context_iounmap(u32 context_id, u32 io, u32 size)
  */
 s32 sys_rsx_context_attribute(s32 context_id, u32 package_id, u64 a3, u64 a4, u64 a5, u64 a6)
 {
-	// Flip/queue/user command/vblank as trace to help with log spam
-	if (package_id == 0x102 || package_id == 0x103 || package_id == 0xFED || package_id == 0xFEF)
+	// Flip/queue/reset flip/flip event/user command/vblank as trace to help with log spam
+	if (package_id == 0x102 || package_id == 0x103 || package_id == 0x10a || package_id == 0xFEC || package_id == 0xFED || package_id == 0xFEF)
 		sys_rsx.trace("sys_rsx_context_attribute(context_id=0x%x, package_id=0x%x, a3=0x%llx, a4=0x%llx, a5=0x%llx, a6=0x%llx)", context_id, package_id, a3, a4, a5, a6);
 	else
 		sys_rsx.warning("sys_rsx_context_attribute(context_id=0x%x, package_id=0x%x, a3=0x%llx, a4=0x%llx, a5=0x%llx, a6=0x%llx)", context_id, package_id, a3, a4, a5, a6);
