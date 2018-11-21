38f3e954e37611a939277f9359db92d19909e71a
Rui Pinheiro <rui@ruipinheiro.eu>
2018-11-21 00:08:27 +0000

    msmw2 timeout hack

--- rpcs3/Emu/Cell/lv2/sys_event.cpp
+++ rpcs3/Emu/Cell/lv2/sys_event.cpp
@@ -268,6 +268,11 @@ error_code sys_event_queue_receive(ppu_thread& ppu, u32 equeue_id, vm::ptr<sys_e
 
 	ppu.gpr[3] = CELL_OK;
 
+	// "/dev_flash/vsh/module/msmw2.sprx" seems to rely on some cryptic shared memory behaviour that we don't emulate correctly
+	// This is a hack to avoid waiting for 1m40s every time we boot vsh
+	if (timeout == 0x5f5e100)
+		timeout = 1;
+
 	const auto queue = idm::get<lv2_obj, lv2_event_queue>(equeue_id, [&](lv2_event_queue& queue) -> CellError
 	{
 		if (queue.type != SYS_PPU_QUEUE)
