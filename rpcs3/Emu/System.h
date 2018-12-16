d7be0a96f3423a5af1775205185e7838cd7b9e90
Nekotekina <nekotekina@gmail.com>
2018-12-17 02:05:26 +0300

    SPU LLVM: approximate xfloat option
    Adapt previous SPU ASMJIT changes made by @kd-11
    FM, FMA, FNMS, FMS are approximated.
    FCGT, FCMGT are accurate.

--- rpcs3/Emu/System.h
+++ rpcs3/Emu/System.h
@@ -378,6 +378,7 @@ struct cfg_root : cfg::node
 		cfg::_bool spu_cache{this, "SPU Cache", true};
 		cfg::_enum<tsx_usage> enable_TSX{this, "Enable TSX", tsx_usage::enabled}; // Enable TSX. Forcing this on Haswell/Broadwell CPUs should be used carefully
 		cfg::_bool spu_accurate_xfloat{this, "Accurate xfloat", false};
+		cfg::_bool spu_approx_xfloat{this, "Approximate xfloat", true};
 
 		cfg::_enum<lib_loading_type> lib_loading{this, "Lib Loader", lib_loading_type::liblv2only};
 		cfg::_bool hook_functions{this, "Hook static functions"};
