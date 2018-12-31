1085cd909b96f0820f464168eb801ecdcad37476
Zion Nimchuk <zionnimchuk@gmail.com>
2018-12-30 16:41:02 -0800

    Deploy linux binaries to RPCS3/rpcs3-binaries-linux via GitHub Releases

--- .travis/build-linux.bash
+++ .travis/build-linux.bash
@@ -32,4 +32,4 @@ ninja
 
 cd ..
 # If it compiled succesfully let's deploy
-if [ $? -eq 0 ] && [ -n "$UPLOAD_URL" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then /bin/bash -ex .travis/deploy-linux.bash ; fi
+if [ $? -eq 0 ] && [ -n "$GITHUB_TOKEN" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then /bin/bash -ex .travis/deploy-linux.bash ; fi
