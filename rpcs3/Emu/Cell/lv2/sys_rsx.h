a50edb4f56ce02e75e8e9e3d0feb224ce531cdf4
eladash <elad3356p@gmail.com>
2019-03-08 10:08:30 +0200

    Use error_code in sys_rsx

--- rpcs3/Emu/Cell/lv2/sys_rsx.h
+++ rpcs3/Emu/Cell/lv2/sys_rsx.h
@@ -107,15 +107,15 @@ struct SysRsxConfig
 };
 
 // SysCalls
-s32 sys_rsx_device_open();
-s32 sys_rsx_device_close();
-s32 sys_rsx_memory_allocate(vm::ptr<u32> mem_handle, vm::ptr<u64> mem_addr, u32 size, u64 flags, u64 a5, u64 a6, u64 a7);
-s32 sys_rsx_memory_free(u32 mem_handle);
-s32 sys_rsx_context_allocate(vm::ptr<u32> context_id, vm::ptr<u64> lpar_dma_control, vm::ptr<u64> lpar_driver_info, vm::ptr<u64> lpar_reports, u64 mem_ctx, u64 system_mode);
-s32 sys_rsx_context_free(u32 context_id);
-s32 sys_rsx_context_iomap(u32 context_id, u32 io, u32 ea, u32 size, u64 flags);
-s32 sys_rsx_context_iounmap(u32 context_id, u32 io, u32 size);
-s32 sys_rsx_context_attribute(s32 context_id, u32 package_id, u64 a3, u64 a4, u64 a5, u64 a6);
-s32 sys_rsx_device_map(vm::ptr<u64> dev_addr, vm::ptr<u64> a2, u32 dev_id);
-s32 sys_rsx_device_unmap(u32 dev_id);
-s32 sys_rsx_attribute(u32 a1, u32 a2, u32 a3, u32 a4, u32 a5);
+error_code sys_rsx_device_open();
+error_code sys_rsx_device_close();
+error_code sys_rsx_memory_allocate(vm::ptr<u32> mem_handle, vm::ptr<u64> mem_addr, u32 size, u64 flags, u64 a5, u64 a6, u64 a7);
+error_code sys_rsx_memory_free(u32 mem_handle);
+error_code sys_rsx_context_allocate(vm::ptr<u32> context_id, vm::ptr<u64> lpar_dma_control, vm::ptr<u64> lpar_driver_info, vm::ptr<u64> lpar_reports, u64 mem_ctx, u64 system_mode);
+error_code sys_rsx_context_free(u32 context_id);
+error_code sys_rsx_context_iomap(u32 context_id, u32 io, u32 ea, u32 size, u64 flags);
+error_code sys_rsx_context_iounmap(u32 context_id, u32 io, u32 size);
+error_code sys_rsx_context_attribute(s32 context_id, u32 package_id, u64 a3, u64 a4, u64 a5, u64 a6);
+error_code sys_rsx_device_map(vm::ptr<u64> dev_addr, vm::ptr<u64> a2, u32 dev_id);
+error_code sys_rsx_device_unmap(u32 dev_id);
+error_code sys_rsx_attribute(u32 a1, u32 a2, u32 a3, u32 a4, u32 a5);
