1ed4d76d91a5d0cc454bec21a65054c5938baf76
scribam <scribam@users.noreply.github.com>
2019-03-30 00:28:24 +0100

    hle: Add cellSysmoduleUnloadModuleInternal and cellSysmoduleLoadModuleInternal functions

--- rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
+++ rpcs3/Emu/Cell/Modules/cellSysmodule.cpp
@@ -361,13 +361,13 @@ s32 cellSysmoduleFetchImage()
 	return CELL_OK;
 }
 
-s32 cellSysmodule_B498BF77()
+s32 cellSysmoduleUnloadModuleInternal()
 {
 	UNIMPLEMENTED_FUNC(cellSysmodule);
 	return CELL_OK;
 }
 
-s32 cellSysmodule_D9B8C0EE()
+s32 cellSysmoduleLoadModuleInternal()
 {
 	UNIMPLEMENTED_FUNC(cellSysmodule);
 	return CELL_OK;
@@ -389,7 +389,7 @@ DECLARE(ppu_module_manager::cellSysmodule)("cellSysmodule", []()
 	REG_FUNC(cellSysmodule, cellSysmoduleIsLoaded);
 	REG_FUNC(cellSysmodule, cellSysmoduleGetImagesize);
 	REG_FUNC(cellSysmodule, cellSysmoduleFetchImage);
-	REG_FNID(cellSysmodule, 0xB498BF77, cellSysmodule_B498BF77);
-	REG_FNID(cellSysmodule, 0xD9B8C0EE, cellSysmodule_D9B8C0EE);
+	REG_FUNC(cellSysmodule, cellSysmoduleUnloadModuleInternal);
+	REG_FUNC(cellSysmodule, cellSysmoduleLoadModuleInternal);
 	REG_FNID(cellSysmodule, 0x1A267F98, cellSysmodule_1A267F98);
 });
