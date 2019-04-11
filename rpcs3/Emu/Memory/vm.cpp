d6194dabeeb740da719901fee19feed56acfb8c9
eladash <elad3356p@gmail.com>
2019-04-11 18:16:20 +0300

    vm: Fix allocations bounderies check

--- rpcs3/Emu/Memory/vm.cpp
+++ rpcs3/Emu/Memory/vm.cpp
@@ -715,7 +715,7 @@ namespace vm
 			shm = std::make_shared<utils::shm>(size);
 
 		// Search for an appropriate place (unoptimized)
-		for (u32 addr = ::align(this->addr, align); addr < this->addr + this->size - 1; addr += align)
+		for (u32 addr = ::align(this->addr, align), max = this->addr + this->size - size; addr <= max; addr += align)
 		{
 			if (try_alloc(addr, pflags, size, std::move(shm)))
 			{
