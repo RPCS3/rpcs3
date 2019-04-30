7508ae6e6503c075b3d52f0989188581940e28ac
eladash <elad3356p@gmail.com>
2019-04-30 09:53:46 +0300

    Add EAGAIN check for sys_memory_contianer_create

--- rpcs3/Emu/Cell/lv2/sys_memory.h
+++ rpcs3/Emu/Cell/lv2/sys_memory.h
@@ -46,7 +46,7 @@ struct sys_page_attr_t
 
 struct lv2_memory_container
 {
-	static const u32 id_base = 0x1; // Wrong?
+	static const u32 id_base = 0x3F000000;
 	static const u32 id_step = 0x1;
 	static const u32 id_count = 16;
 
