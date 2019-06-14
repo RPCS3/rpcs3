8dafc8e542b5e22ee9602dff1141fe797edadc12
kd-11 <karokidii@gmail.com>
2019-06-14 16:27:23 +0300

    cfg: Default MSAA level to auto

--- rpcs3/Emu/System.h
+++ rpcs3/Emu/System.h
@@ -436,7 +436,7 @@ struct cfg_root : cfg::node
 		cfg::_enum<video_resolution> resolution{this, "Resolution", video_resolution::_720};
 		cfg::_enum<video_aspect> aspect_ratio{this, "Aspect ratio", video_aspect::_16_9};
 		cfg::_enum<frame_limit_type> frame_limit{this, "Frame limit", frame_limit_type::none};
-		cfg::_enum<msaa_level> antialiasing_level{this, "MSAA", msaa_level::none};
+		cfg::_enum<msaa_level> antialiasing_level{this, "MSAA", msaa_level::_auto};
 
 		cfg::_bool write_color_buffers{this, "Write Color Buffers"};
 		cfg::_bool write_depth_buffer{this, "Write Depth Buffer"};
