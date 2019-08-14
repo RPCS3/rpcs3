80827aa31a2aa8e35fa168397de4b52c47e0db13
Eladash <elad3356p@gmail.com>
2019-08-14 18:18:06 +0300

    Stub sys_process_get_number_of_object(object == SYS_TRACE_OBJECT)

--- rpcs3/Emu/Cell/lv2/sys_trace.cpp
+++ rpcs3/Emu/Cell/lv2/sys_trace.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "sys_trace.h"
 
 #include "Emu/System.h"
@@ -9,62 +9,64 @@
 
 LOG_CHANNEL(sys_trace);
 
+// TODO: DEX/DECR mode support?
+
 s32 sys_trace_create()
 {
 	sys_trace.todo("sys_trace_create()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_start()
 {
 	sys_trace.todo("sys_trace_start()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_stop()
 {
 	sys_trace.todo("sys_trace_stop()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_update_top_index()
 {
 	sys_trace.todo("sys_trace_update_top_index()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_destroy()
 {
 	sys_trace.todo("sys_trace_destroy()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_drain()
 {
 	sys_trace.todo("sys_trace_drain()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_attach_process()
 {
 	sys_trace.todo("sys_trace_attach_process()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_allocate_buffer()
 {
 	sys_trace.todo("sys_trace_allocate_buffer()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_free_buffer()
 {
 	sys_trace.todo("sys_trace_free_buffer()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
 
 s32 sys_trace_create2()
 {
 	sys_trace.todo("sys_trace_create2()");
-	return CELL_OK;
+	return CELL_ENOSYS;
 }
