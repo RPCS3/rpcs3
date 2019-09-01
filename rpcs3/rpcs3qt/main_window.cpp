0dfe85dc14a7448aab401dd0062e5ffcde69b23c
JoãoPaulo <53271430+Joaozin-tech@users.noreply.github.com>
2019-09-01 13:19:11 -0300

    Update FW 4.85 Latest Version (#6437)
    * Update FW Latest Version
    
    Updates the latest FW version according to: https://www.playstation.com/en-us/support/system-updates/ps3/
    
    Co-Authored-By: MsDarkLow <msdarklow@gmail.com>

--- rpcs3/rpcs3qt/main_window.cpp
+++ rpcs3/rpcs3qt/main_window.cpp
@@ -528,7 +528,7 @@ void main_window::InstallPup(const QString& dropPath)
 		version_string.erase(version_pos);
 	}
 
-	const std::string cur_version = "4.84";
+	const std::string cur_version = "4.85";
 
 	if (version_string < cur_version &&
 		QMessageBox::question(this, tr("RPCS3 Firmware Installer"), tr("Old firmware detected.\nThe newest firmware version is %1 and you are trying to install version %2\nContinue installation?").arg(qstr(cur_version), qstr(version_string)),
