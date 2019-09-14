0d9c9c4358e933f64580cff6123651357a51cd8a
msuih <msuih@users.noreply.github.com>
2019-09-14 06:05:00 +0300

    Fix debug build
    Seems to be a compiler bug

--- rpcs3/Emu/IdManager.h
+++ rpcs3/Emu/IdManager.h
@@ -30,7 +30,7 @@ namespace id_manager
 		static const u32 base    = T::id_base;
 		static const u32 step    = T::id_step;
 		static const u32 count   = T::id_count;
-		static const u32 invalid = base > 0 ? 0 : -1;
+		static const u32 invalid = -+!base;
 
 		// Note: full 32 bits range cannot be used at current implementation
 		static_assert(count > 0 && step > 0 && u64{step} * count + base < u64{UINT32_MAX} + (base != 0 ? 1 : 0), "ID traits: invalid object range");
