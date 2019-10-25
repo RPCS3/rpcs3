7b16d444b81b398bcc1bd0c5c6b30c13b996cef7
Wojtek Arabczyk <arabek@users.noreply.github.com>
2019-10-25 09:16:30 +0200

    Don't lower the overall volume when downmixing
    This restores the previous behavior.
    
    The PR still contains a fix for proper channel relative volume when
    downmixing multiple sources to stereo.

--- rpcs3/Emu/Cell/Modules/cellAudio.cpp
+++ rpcs3/Emu/Cell/Modules/cellAudio.cpp
@@ -754,9 +754,9 @@ void cell_audio_thread::mix(float *out_buffer, s32 offset)
 		if (port.state != audio_port_state::started) continue;
 
 		auto buf = port.get_vm_ptr(offset);
-		static const float k = 0.3694f; /* avoids clipping loud audio when downmixing multiple channels to stereo */
+		static const float k = 1.f;
 		static const float minus_3db = 0.707f; /* value taken from
-                                              https://www.dolby.com/us/en/technologies/a-guide-to-dolby-metadata.pdf */
+							  https://www.dolby.com/us/en/technologies/a-guide-to-dolby-metadata.pdf */
 		float& m = port.level;
 
 		// part of cellAudioSetPortLevel functionality
@@ -838,7 +838,7 @@ void cell_audio_thread::mix(float *out_buffer, s32 offset)
 					if constexpr (DownmixToStereo)
 					{
 						const float mid = center * minus_3db; /* don't mix in the lfe as per
-                                                     dolby specification */
+											 dolby specification */
 						out_buffer[out + 0] = (left + rear_left + (side_left * minus_3db) + mid) * k;
 						out_buffer[out + 1] = (right + rear_right + (side_right * minus_3db) + mid) * k;
 					}
