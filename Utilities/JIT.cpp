dbece4d00fc8b6d815f33e596f0bd81bf35e4937
Nekotekina <nekotekina@gmail.com>
2019-12-13 02:11:21 +0300

    LLVM: add workaround for cascadelake CPU
    Need to investigate it later.

--- Utilities/JIT.cpp
+++ Utilities/JIT.cpp
@@ -982,6 +982,10 @@ std::string jit_compiler::cpu(const std::string& _cpu)
 			{
 				m_cpu = "skylake";
 			}
+			else
+			{
+				m_cpu = "skylake-avx512";
+			}
 		}
 
 		if (m_cpu == "znver1" && utils::has_clwb())
