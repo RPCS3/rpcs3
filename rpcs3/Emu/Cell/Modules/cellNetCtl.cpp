bcf193e7d5951656509885c105b7d1ee0ae17158
Marcin Mitura <mitura07@wp.pl>
2020-01-11 20:53:59 +0100

    Update cellNetCtl.cpp

--- rpcs3/Emu/Cell/Modules/cellNetCtl.cpp
+++ rpcs3/Emu/Cell/Modules/cellNetCtl.cpp
@@ -208,15 +208,7 @@ error_code cellNetCtlGetInfo(s32 code, vm::ptr<CellNetCtlInfo> info)
 			strcpy_trunc(info->ip_address, g_cfg.net.ip_address);
 		}
 	}
-	else if (code == CELL_NET_CTL_INFO_NETMASK)
-	{
-		strcpy_trunc(info->netmask, "255.255.255.255");
-	}
-	else if (code == CELL_NET_CTL_INFO_HTTP_PROXY_CONFIG)
-	{
-		info->http_proxy_config = 0;
-	}
-
+	
 	return CELL_OK;
 }
 
@@ -266,7 +258,7 @@ error_code cellNetCtlNetStartDialogAbortAsync()
 
 error_code cellNetCtlNetStartDialogUnloadAsync(vm::ptr<CellNetCtlNetStartDialogResult> result)
 {
-	cellNetCtl.warning("cellNetCtlNetStartDialogUnloadAsync(result=*0x%x)", result);
+	cellNetCtl.error("cellNetCtlNetStartDialogUnloadAsync(result=*0x%x)", result);
 
 	if (!g_fxo->get<cell_net_ctl_manager>()->is_initialized)
 	{
@@ -290,7 +282,8 @@ error_code cellNetCtlNetStartDialogUnloadAsync(vm::ptr<CellNetCtlNetStartDialogR
 	else
 	{
 		// Hack
-		result->result = CELL_NET_CTL_ERROR_DIALOG_CANCELED;
+		//	result->result = CELL_NET_CTL_ERROR_DIALOG_CANCELED;
+	result->result = 0;
 	}
 
 	sysutil_send_system_cmd(CELL_SYSUTIL_NET_CTL_NETSTART_UNLOADED, 0);
