1b8e230ed879dd68be33e874f7fd7eab6c254811
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2020-01-27 18:05:49 +0000

    Initial GHLtar emulation

---
+++ rpcs3/Emu/Io/GHLtar.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Emu/Io/usb_device.h"
+#include <queue>
+#include <mutex>
+
+class usb_device_ghltar : public usb_device_emulated
+{
+public:
+	usb_device_ghltar();
+	~usb_device_ghltar();
+
+	void control_transfer(u8 bmRequestType, u8 bRequest, u16 wValue, u16 wIndex, u16 wLength, u32 buf_size, u8* buf, UsbTransfer* transfer) override;
+	void interrupt_transfer(u32 buf_size, u8* buf, u32 endpoint, UsbTransfer* transfer) override;
+
+protected:
+	u8 interrupt_counter = 0;
+	bool shouldSendOne = false;
+};
