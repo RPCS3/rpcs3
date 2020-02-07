7b11397b9d25a02382aed14289afe183e5a12da1
kd-11 <karokidii@gmail.com>
2020-02-07 19:02:01 +0300

    overlays: Fixup
    - Avoid calling join on self thread.
    - Avoid use-after-free.

--- rpcs3/Emu/RSX/Overlays/overlays.cpp
+++ rpcs3/Emu/RSX/Overlays/overlays.cpp
@@ -317,10 +317,26 @@ namespace rsx
 				reader_lock lock(m_threadpool_mutex);
 				for (auto& worker : m_workers)
 				{
-					worker.join();
+					if (std::this_thread::get_id() != worker.get_id() && worker.joinable())
+					{
+						worker.join();
+					}
+					else
+					{
+						worker.detach();
+					}
 				}
 			}
 
+			pad::SetIntercepted(false);
+
+			if (on_close && use_callback)
+			{
+				g_last_user_response = return_code;
+				on_close(return_code);
+			}
+
+			// NOTE: Object removal should be the last step
 			if (auto manager = g_fxo->get<display_manager>())
 			{
 				if (auto dlg = manager->get<rsx::overlays::message_dialog>())
@@ -330,14 +346,6 @@ namespace rsx
 				}
 
 				manager->remove(uid);
-			}
-
-			pad::SetIntercepted(false);
-
-			if (on_close && use_callback)
-			{
-				g_last_user_response = return_code;
-				on_close(return_code);
 			}
 		}
 
