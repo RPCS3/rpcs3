0c757222cb0208abac05628d79d4876f5f8075e3
Eladash <elad3356p@gmail.com>
2020-02-20 22:10:49 +0200

    Minor improvements to sys_ss_random_number_generator
    * Replace CELL_EABORT with exception.
    * Improve error codes a bit.
    * pkg_id == 1 should not return an error if root permission is present.
    * Avoid passing vm pointers to native API, use temp buffer instead.

--- rpcs3/Emu/Cell/lv2/sys_ss.h
+++ rpcs3/Emu/Cell/lv2/sys_ss.h
@@ -3,13 +3,22 @@
 #include "Emu/Memory/vm_ptr.h"
 #include "Emu/Cell/ErrorCodes.h"
 
+// Unofficial error code names
+enum sys_ss_rng_error : u32
+{
+	SYS_SS_RNG_ERROR_INVALID_PKG = 0x80010500,
+	SYS_SS_RNG_ERROR_ENOMEM = 0x80010501,
+	SYS_SS_RNG_ERROR_EAGAIN = 0x80010503,
+	SYS_SS_RNG_ERROR_EFAULT = 0x80010509,
+};
+
 struct CellSsOpenPSID
 {
 	be_t<u64> high;
 	be_t<u64> low;
 };
 
-error_code sys_ss_random_number_generator(u32 arg1, vm::ptr<void> buf, u64 size);
+error_code sys_ss_random_number_generator(u64 pkg_id, vm::ptr<void> buf, u64 size);
 error_code sys_ss_access_control_engine(u64 pkg_id, u64 a2, u64 a3);
 s32 sys_ss_get_console_id(vm::ptr<u8> buf);
 s32 sys_ss_get_open_psid(vm::ptr<CellSsOpenPSID> ptr);
