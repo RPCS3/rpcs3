cc71d2c4bf37cab492ed1e3bcb6e941d33cd31fd
JohnHolmesII <jedistephen@hotmail.com>
2020-02-22 18:03:58 -0800

    memory_string_searcher: Change to std strings  - Compiler warnings indicated that the call to toStdString() did not    create an object that lived long enough be used. Simply use std    string ahead of time.

--- rpcs3/rpcs3qt/memory_string_searcher.cpp
+++ rpcs3/rpcs3qt/memory_string_searcher.cpp
@@ -33,8 +33,8 @@ memory_string_searcher::memory_string_searcher(QWidget* parent)
 
 void memory_string_searcher::OnSearch()
 {
-	const QString wstr = m_addr_line->text();
-	const char *str = wstr.toStdString().c_str();
+	const std::string wstr = m_addr_line->text().toStdString();
+	const char *str = wstr.c_str();
 	const u32 len = wstr.length();
 
 	gui_log.notice("Searching for string %s", str);
