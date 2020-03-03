9ded69438f288acf6c0f2f250d737e27d8a8ce70
Jeremy Bristol <jeremybristol@aol.com>
2020-03-03 16:53:57 -0500

    Update gcm_enums.cpp

--- rpcs3/Emu/RSX/gcm_enums.cpp
+++ rpcs3/Emu/RSX/gcm_enums.cpp
@@ -720,7 +720,7 @@ rsx::blend_equation rsx::to_blend_equation(u16 in)
 	case CELL_GCM_FUNC_ADD_SIGNED: return rsx::blend_equation::add_signed;
 	case CELL_GCM_FUNC_REVERSE_ADD_SIGNED: return rsx::blend_equation::reverse_add_signed;
 	}
-	fmt::throw_exception("Unknown blend eq 0x%x" HERE, in);
+	// Disabled because of issue #3357 - fmt::throw_exception("Unknown blend eq 0x%x" HERE, in);
 }
 
 enum
