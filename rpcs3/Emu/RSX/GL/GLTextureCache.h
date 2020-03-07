d64f0ffbbd72b26831cd8f5a55d92e9b8dc31247
kd-11 <karokidii@gmail.com>
2020-03-07 16:35:12 +0300

    gl: Restore commented out line
    - Byte order step was disabled for debugging and not restored

--- rpcs3/Emu/RSX/GL/GLTextureCache.h
+++ rpcs3/Emu/RSX/GL/GLTextureCache.h
@@ -170,7 +170,7 @@ namespace gl
 
 			pixel_pack_settings pack_settings;
 			pack_settings.alignment(1);
-			//pack_settings.swap_bytes(pack_unpack_swap_bytes);
+			pack_settings.swap_bytes(pack_unpack_swap_bytes);
 
 			src->copy_to(nullptr, format, type, pack_settings);
 			real_pitch = src->pitch();
