713df966dd52761fdc9697d3a7c33d8538ddacb0
Jan Beich <jbeich@FreeBSD.org>
2020-03-27 18:14:07 +0000

    Qt: unbreak build with 5.15.0-beta2
    rpcs3/rpcs3qt/pad_led_settings_dialog.cpp:82:15: error: variable has incomplete type 'QPainterPath'
            QPainterPath path;
                         ^
    /usr/local/include/qt5/QtGui/qmatrix.h:54:7: note: forward declaration of 'QPainterPath'
    class QPainterPath;
          ^

--- rpcs3/rpcs3qt/pad_led_settings_dialog.cpp
+++ rpcs3/rpcs3qt/pad_led_settings_dialog.cpp
@@ -2,6 +2,7 @@
 
 #include <QColorDialog>
 #include <QPainter>
+#include <QPainterPath>
 #include <QPixmap>
 
 pad_led_settings_dialog::pad_led_settings_dialog(const int& colorR, const int& colorG, const int& colorB, const bool& led_low_battery_blink, const bool& led_battery_indicator, const int& led_battery_indicator_brightness, QDialog * parent)
