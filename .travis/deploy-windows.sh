f204f5b06cc744aebf06e05c125c989126d0c0d5
JohnHolmesII <jedistephen@hotmail.com>
2020-03-27 17:19:44 -0700

    CI: Add workaround for exporting variables in Azure
    Using '-x' to echo commands in the shell causes
    the Azure process commands to be processed twice

--- .travis/deploy-windows.sh
+++ .travis/deploy-windows.sh
@@ -1,32 +1,12 @@
 #!/bin/sh -ex
 
-# From pure sh bible, strips rogue quotes
-# Does single and double quotes
-trim_quotes()
-{
-    set -f
-
-    old_ifs=$IFS
-    IFS=\"\'
-
-    set -- $1
-    IFS=
-    printf '%s\n' "$*"
-    IFS=$old_ifs
-
-    set +f
-}
-
 # BUILD_blablabla is Azure specific, so we wrap it for portability
-# The BUILD var is passed from a previous stage, so it is cleaned
-# due to a bug in Azure
 ARTIFACT_DIR="$BUILD_ARTIFACTSTAGINGDIRECTORY"
-BUILD=$(trim_quotes "$BUILD")
 
 # Remove unecessary files
 rm -f ./bin/rpcs3.exp ./bin/rpcs3.lib ./bin/rpcs3.pdb
 
-# Prepare compatibility database for packaging, as well as 
+# Prepare compatibility database for packaging, as well as
 # certificate for ssl (auto-updater)
 curl -sL 'https://rpcs3.net/compatibility?api=v1&export' | iconv -t UTF-8 > ./bin/GuiConfigs/compat_database.dat
 curl -sL 'https://curl.haxx.se/ca/cacert.pem' > ./bin/cacert.pem
