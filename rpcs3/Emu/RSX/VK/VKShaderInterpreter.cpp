781125e5c451c58fc388ad00594d67bde9eb5847
kd-11 <karokidii@gmail.com>
2020-05-02 00:25:42 +0300

    glsl: Improve VGPR usage
    - VGPR usage lowered from 159 -> 127 for texturing. Occupancy doubled from 1 to 2
    - Eliminate most temporary registers

--- rpcs3/Emu/RSX/VK/VKShaderInterpreter.cpp
+++ rpcs3/Emu/RSX/VK/VKShaderInterpreter.cpp
@@ -185,8 +185,6 @@ namespace vk
 		"	uvec4 fp_instructions[];\n"
 		"};\n\n";
 
-		::program_common::insert_fog_declaration(builder, "vec4", "fogc", true);
-
 		builder << program_common::interpreter::get_fragment_interpreter();
 		const std::string s = builder.str();
 
