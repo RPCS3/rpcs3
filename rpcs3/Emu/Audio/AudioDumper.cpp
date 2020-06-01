c4a558e9abaa82772dbd898812bd26ff9c832b57
illusion <37698908+illusion0001@users.noreply.github.com>
2020-06-01 16:58:14 +1100

    Audio dumper: append filename with titleid and date-time
    prevents overwrite of old file
    
    Co-authored-by: Megamouse <studienricky89@googlemail.com>

--- rpcs3/Emu/Audio/AudioDumper.cpp
+++ rpcs3/Emu/Audio/AudioDumper.cpp
@@ -1,13 +1,21 @@
 ﻿#include "stdafx.h"
 #include "AudioDumper.h"
 
+#include "Utilities/date_time.h"
+#include "Emu/System.h"
 
 AudioDumper::AudioDumper(u16 ch)
 	: m_header(ch)
 {
 	if (GetCh())
 	{
-		m_output.open(fs::get_cache_dir() + "audio.wav", fs::rewrite);
+		std::string path = fs::get_cache_dir() + "audio_";
+		if (const std::string id = Emu.GetTitleID(); !id.empty())
+		{
+			path += id + "_";
+		};
+		path += date_time::current_time_narrow<'_'>() + ".wav";
+		m_output.open(path, fs::rewrite);
 		m_output.write(m_header); // write initial file header
 	}
 }
