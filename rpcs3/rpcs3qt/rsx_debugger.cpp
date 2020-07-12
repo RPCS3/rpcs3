b4186ca5cb1f38a4531fb1f99a32fc6a95945447
Eladash <elad3356p@gmail.com>
2020-07-12 06:49:10 +0300

    RSX debugger: fix command count on non-method commands

--- rpcs3/rpcs3qt/rsx_debugger.cpp
+++ rpcs3/rpcs3qt/rsx_debugger.cpp
@@ -613,14 +613,16 @@ void rsx_debugger::GetMemory()
 		{
 			u32 cmd = *vm::get_super_ptr<u32>(ea);
 			u32 count = (cmd >> 18) & 0x7ff;
+
+			if (cmd & RSX_METHOD_NON_METHOD_CMD_MASK)
+			{
+				count = 0;
+			}
+
 			m_list_commands->setItem(i, 1, new QTableWidgetItem(qstr(fmt::format("%08x", cmd))));
 			m_list_commands->setItem(i, 2, new QTableWidgetItem(DisAsmCommand(cmd, count, addr)));
 			m_list_commands->setItem(i, 3, new QTableWidgetItem(QString::number(count)));
-
-			if(!(cmd & RSX_METHOD_NON_METHOD_CMD_MASK))
-			{
-				addr += 4 * count;
-			}
+			addr += 4 * count;
 		}
 		else
 		{
