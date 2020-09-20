e0ccc0dacd63b70c094fb533fa2d2ebc61e3eb8a
Eladash <elad3356p@gmail.com>
2020-09-20 09:27:08 +0300

    sceNpTrophyRegisterContext: Atomically create trophy data

--- rpcs3/Emu/VFS.h
+++ rpcs3/Emu/VFS.h
@@ -21,6 +21,8 @@ namespace vfs
 	// Functions in this namespace operate on host filepaths, similar to fs::
 	namespace host
 	{
+		std::string hash_path(const std::string& path, const std::string& dev_root);
+
 		// Call fs::rename with retry on access error
 		bool rename(const std::string& from, const std::string& to, bool overwrite);
 
