72284b4530fc0b75c77f866b573c973f1e6fd9c2
Nekotekina <nekotekina@gmail.com>
2020-12-10 18:44:17 +0300

    Use atomic_t<> in VKMemAlloc

--- rpcs3/Emu/RSX/VK/VKMemAlloc.cpp
+++ rpcs3/Emu/RSX/VK/VKMemAlloc.cpp
@@ -1,5 +1,12 @@
 #define VMA_IMPLEMENTATION
 
+#include "util/atomic.hpp"
+
+#define VMA_ATOMIC_UINT32 atomic_t<u32>
+#define VMA_ATOMIC_UINT64 atomic_t<u64>
+#define compare_exchange_strong compare_exchange
+#define compare_exchange_weak compare_exchange
+
 #ifdef _MSC_VER
 #pragma warning(push, 0)
 #else
