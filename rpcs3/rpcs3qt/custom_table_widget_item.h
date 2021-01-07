72e1d03860162b53110b8d2f2e9bfb0b6517b543
Megamouse <studienricky89@googlemail.com>
2021-01-07 20:00:48 +0100

    Qt: ugly fix for ugly deprecation warning
    Apparently Qt wants us to implement each comparison on our own, and there is no simple replacement for this.

--- rpcs3/rpcs3qt/custom_table_widget_item.h
+++ rpcs3/rpcs3qt/custom_table_widget_item.h
@@ -11,41 +11,10 @@ public:
 	using QTableWidgetItem::setData;
 
 	custom_table_widget_item(){}
-	custom_table_widget_item(const std::string& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0)
-	: QTableWidgetItem(QString::fromStdString(text).simplified()) // simplified() forces single line text
-	{
-		if (sort_role != Qt::DisplayRole)
-		{
-			setData(sort_role, sort_value, true);
-		}
-	}
-	custom_table_widget_item(const QString& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0)
-	: QTableWidgetItem(text.simplified()) // simplified() forces single line text
-	{
-		if (sort_role != Qt::DisplayRole)
-		{
-			setData(sort_role, sort_value, true);
-		}
-	}
+	custom_table_widget_item(const std::string& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0);
+	custom_table_widget_item(const QString& text, int sort_role = Qt::DisplayRole, const QVariant& sort_value = 0);
 
-	bool operator <(const QTableWidgetItem& other) const
-	{
-#ifndef _MSC_VER
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-		return data(m_sort_role) < other.data(m_sort_role);
-#ifndef _MSC_VER
-#pragma GCC diagnostic pop
-#endif
-	}
+	bool operator<(const QTableWidgetItem& other) const;
 
-	void setData(int role, const QVariant& value, bool assign_sort_role)
-	{
-		if (assign_sort_role)
-		{
-			m_sort_role = role;
-		}
-		QTableWidgetItem::setData(role, value);
-	}
+	void setData(int role, const QVariant& value, bool assign_sort_role);
 };
