631d7d0ce7a7978e13414b68eb267ce0db2264e9
Nekotekina <nekotekina@gmail.com>
2021-01-10 19:06:27 +0300

    Enable -Wunknown-pragmas

--- rpcs3/Emu/RSX/VK/VulkanAPI.h
+++ rpcs3/Emu/RSX/VK/VulkanAPI.h
@@ -8,12 +8,16 @@
 #define VK_USE_PLATFORM_XLIB_KHR
 #endif
 
+#ifdef _MSC_VER
 #pragma warning( push )
 #pragma warning( disable : 4005 )
+#endif
 
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_sdk_platform.h>
 
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 
 #include <util/types.hpp>
--- rpcs3/cmake_modules/ConfigureCompiler.cmake
+++ rpcs3/cmake_modules/ConfigureCompiler.cmake
@@ -34,7 +34,6 @@ else()
 
 	#TODO Clean the code so these are removed
 	add_compile_options(-Wno-unused-variable)
-	add_compile_options(-Wno-unknown-pragmas)
 	add_compile_options(-Wno-unused-function)
 	add_compile_options(-Wno-attributes)
 
