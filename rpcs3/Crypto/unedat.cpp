6cf73fad13bc82254b692dcb5496c7172a4c99fe
Nekotekina <nekotekina@gmail.com>
2021-01-13 18:16:43 +0300

    Crypto: workaround -Wunused-variable
    Ignore for key vault for now.

--- rpcs3/Crypto/unedat.cpp
+++ rpcs3/Crypto/unedat.cpp
@@ -23,7 +23,7 @@ void generate_key(int crypto_mode, int version, unsigned char *key_final, unsign
 	case 0x10000000:
 		// Encrypted ERK.
 		// Decrypt the key with EDAT_KEY + EDAT_IV and copy the original IV.
-		aescbc128_decrypt(const_cast<u8*>(version ? EDAT_KEY_1 : EDAT_KEY_0), const_cast<u8*>(EDAT_IV), key, key_final, 0x10);
+		aescbc128_decrypt(version ? EDAT_KEY_1 : EDAT_KEY_0, EDAT_IV, key, key_final, 0x10);
 		memcpy(iv_final, iv, 0x10);
 		break;
 	case 0x20000000:
@@ -49,7 +49,7 @@ void generate_hash(int hash_mode, int version, unsigned char *hash_final, unsign
 	case 0x10000000:
 		// Encrypted HASH.
 		// Decrypt the hash with EDAT_KEY + EDAT_IV.
-		aescbc128_decrypt(const_cast<u8*>(version ? EDAT_KEY_1 : EDAT_KEY_0), const_cast<u8*>(EDAT_IV), hash, hash_final, 0x10);
+		aescbc128_decrypt(version ? EDAT_KEY_1 : EDAT_KEY_0, EDAT_IV, hash, hash_final, 0x10);
 		break;
 	case 0x20000000:
 		// Default HASH.
