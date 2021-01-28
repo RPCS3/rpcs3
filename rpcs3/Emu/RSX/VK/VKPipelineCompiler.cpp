49431ba9c2ec1ac00762c5326a6a10e3f62b4448
Eladash <elad3356p@gmail.com>
2021-01-28 20:33:50 +0200

    Fix minor issue with usage of STL thread::hardware_concurrency()

--- rpcs3/Emu/RSX/VK/VKPipelineCompiler.cpp
+++ rpcs3/Emu/RSX/VK/VKPipelineCompiler.cpp
@@ -6,6 +6,8 @@
 
 #include <thread>
 
+#include "util/sysinfo.hpp"
+
 namespace vk
 {
 	// Global list of worker threads
@@ -186,7 +188,7 @@ namespace vk
 		if (num_worker_threads == 0)
 		{
 			// Select optimal number of compiler threads
-			const auto hw_threads = std::thread::hardware_concurrency();
+			const auto hw_threads = utils::get_thread_count();
 			if (hw_threads > 12)
 			{
 				num_worker_threads = 6;
