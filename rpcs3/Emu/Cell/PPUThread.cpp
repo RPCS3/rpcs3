a41765fb05f56a9fa2e147c16f140bf66d80383c
Eladash <elad3356p@gmail.com>
2021-02-16 20:56:00 +0200

    Fix double-pause on PPU breakpoints stepping

--- rpcs3/Emu/Cell/PPUThread.cpp
+++ rpcs3/Emu/Cell/PPUThread.cpp
@@ -455,7 +455,7 @@ extern void ppu_register_function_at(u32 addr, u32 size, ppu_function_t ptr)
 static bool ppu_break(ppu_thread& ppu, ppu_opcode_t op)
 {
 	// Pause
-	ppu.state += cpu_flag::dbg_pause;
+	ppu.state.atomic_op([](bs_t<cpu_flag>& state) { if (!(state & cpu_flag::dbg_step)) state += cpu_flag::dbg_pause; });
 	
 	if (ppu.check_state())
 	{
