b6c3a258d09a64cbe23f756d2cf12f38624ce9d3
Eladash <elad3356p@gmail.com>
2021-03-09 12:52:30 +0200

    Update render_creator.cpp

--- rpcs3/rpcs3qt/render_creator.cpp
+++ rpcs3/rpcs3qt/render_creator.cpp
@@ -36,7 +36,7 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 
 	static QStringList compatible_gpus;
 
-	auto enum_thread = new named_thread("Vulkan Device Enumeration Thread"sv, [&]()
+	auto enum_thread_v = new named_thread("Vulkan Device Enumeration Thread"sv, [&]()
 	{
 		thread_ctrl::scoped_priority low_prio(-1);
 
@@ -61,6 +61,7 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 		cond.notify_all();
 	});
 
+	std::unique_ptr<decltype(enum_thread_v)> enum_thread(enum_thread_v);
 	{
 		std::unique_lock lck(mtx);
 		cond.wait_for(lck, std::chrono::seconds(10), [&] { return !thread_running; });
@@ -68,6 +69,8 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 
 	if (thread_running)
 	{
+		enum_thread.release(); // Detach thread (destructor is not called)
+
 		cfg_log.error("Vulkan device enumeration timed out");
 		const auto button = QMessageBox::critical(nullptr, tr("Vulkan Check Timeout"),
 			tr("Querying for Vulkan-compatible devices is taking too long. This is usually caused by malfunctioning "
@@ -87,7 +90,7 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 	{
 		supports_vulkan = device_found;
 		vulkan_adapters = std::move(compatible_gpus);
-		delete enum_thread; // Join thread
+		enum_thread.reset(); // Join thread
 	}
 #endif
 
