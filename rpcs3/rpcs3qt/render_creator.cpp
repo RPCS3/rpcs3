39229b43a6f68ded5c40036c2b937230749f98de
Eladash <elad3356p@gmail.com>
2021-03-10 10:30:32 +0200

    Update render_creator.cpp

--- rpcs3/rpcs3qt/render_creator.cpp
+++ rpcs3/rpcs3qt/render_creator.cpp
@@ -25,22 +25,22 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 	// plugged in. This whole contraption is for showing an error message in case that happens, so that user has
 	// some idea about why the emulator window isn't showing up.
 
-	struct thread_data_t
-	{
-		std::mutex mtx;
-		std::condition_variable cond;
-		bool work_done = false;
-	};
 
-	const auto data = std::make_shared<thread_data_t>();
+	static atomic_t<bool> was_called = false;
+	if (was_called.exchange(true))
+		fmt::throw_exception("Render_Creator cannot be created more than once");
 
-	auto enum_thread_v = new named_thread("Vulkan Device Enumeration Thread"sv, [adapters = &vulkan_adapters, data]()
+	static std::mutex mtx;
+	static std::condition_variable cond;
+	static bool work_done = false;
+
+	auto enum_thread_v = new named_thread("Vulkan Device Enumeration Thread"sv, [adapters = &vulkan_adapters, &]()
 	{
 		thread_ctrl::scoped_priority low_prio(-1);
 
 		vk::instance device_enum_context;
 
-		std::unique_lock lock(data->mtx, std::defer_lock);
+		std::unique_lock lock(mtx, std::defer_lock);
 
 		if (device_enum_context.create("RPCS3", true))
 		{
@@ -49,7 +49,7 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 
 			lock.lock();
 
-			if (!data->work_done) // The spawning thread gave up, do not attempt to modify vulkan_adapters
+			if (!work_done) // The spawning thread gave up, do not attempt to modify vulkan_adapters
 			{
 				for (auto& gpu : gpus)
 				{
@@ -62,18 +62,18 @@ render_creator::render_creator(QObject *parent) : QObject(parent)
 			lock.lock();
 		}
 
-		data->work_done = true;
+		work_done = true;
 		lock.unlock();
-		data->cond.notify_all();
+		cond.notify_all();
 	});
 
 	std::unique_ptr<std::remove_pointer_t<decltype(enum_thread_v)>> enum_thread(enum_thread_v);
 	{
-		std::unique_lock lck(data->mtx);
-		cond.wait_for(lck, std::chrono::seconds(10), [&] { return data->work_done; });
+		std::unique_lock lck(mtx);
+		cond.wait_for(lck, std::chrono::seconds(10), [&] { return work_done; });
 	}
 
-	if (std::scoped_lock{data->mtx}, !std::exchange(data->work_done, true)) // If thread hasn't done its job yet, it won't anymore
+	if (std::scoped_lock{mtx}, !std::exchange(work_done, true)) // If thread hasn't done its job yet, it won't anymore
 	{
 		enum_thread.release(); // Detach thread (destructor is not called)
 
