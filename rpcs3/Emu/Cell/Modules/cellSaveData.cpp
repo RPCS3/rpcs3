9e32fba57235f8f2f7506eb79621184bcef9ef2c
Eladash <elad3356p@gmail.com>
2021-03-17 08:52:07 +0200

    Update cellSaveData.cpp

--- rpcs3/Emu/Cell/Modules/cellSaveData.cpp
+++ rpcs3/Emu/Cell/Modules/cellSaveData.cpp
@@ -2284,7 +2284,7 @@ error_code cellSaveDataUserFixedDelete(ppu_thread& ppu, u32 userId, PSetList set
 
 void cellSaveDataEnableOverlay(s32 enable)
 {
-	cellSaveData.warning("cellSaveDataEnableOverlay(enable=%d)", enable);
+	cellSaveData.todo("cellSaveDataEnableOverlay(enable=%d)", enable);
 }
 
 
