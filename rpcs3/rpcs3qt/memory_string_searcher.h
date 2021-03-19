d598f14509ef732bb56753fa6f5d1cac61c69ff2
Eladash <elad3356p@gmail.com>
2021-03-19 17:25:32 +0200

    Memory string searcher improvements
    * Search the entire PS3 4GB address space instead of a small area of memory which is meant mostly for PRX/SELF memory aka vm::main segment. This limitation rendered string searcher nearly useless.
    * Implement hexadecimal byte-string, hexadecimal integer, float and double value search formats.
    * Multi-thread searching.
    * Optimize searching algorithm vastly.
    * Fix crash on empty string, report an error.
    * Implement case-insensitive string search.

--- rpcs3/rpcs3qt/memory_string_searcher.h
+++ rpcs3/rpcs3qt/memory_string_searcher.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <QDialog>
-#include <QLineEdit>
+
+class QLineEdit;
+class QCheckBox;
+class QComboBox;
 
 class memory_string_searcher : public QDialog
 {
 	Q_OBJECT
 
 	QLineEdit* m_addr_line;
+	QCheckBox* m_chkbox_case_insensitive = nullptr;
+	QComboBox* m_cbox_input_mode = nullptr;
 
 public:
 	memory_string_searcher(QWidget* parent);
