d282ad6aa5fd5e53f452f2320505b681e7833039
Eladash <elad3356p@gmail.com>
2021-03-23 10:30:43 +0200

    Update Thread.cpp

--- Utilities/Thread.cpp
+++ Utilities/Thread.cpp
@@ -1409,7 +1409,7 @@ bool handle_access_violation(u32 addr, bool is_writing, x64_context* context) no
 			return false;
 		}
 
-		if (vm::reader_lock rlock; area->flags & 0x120 || vm::check_addr(addr, 0))
+		if (vm::reader_lock rlock; vm::check_addr(addr, 0))
 		{
 			// For allocated memory with protection lower than required (such as protection::no or read-only while writing to it)
 			utils::memory_protect(vm::base(addr & -0x1000), 0x1000, utils::protection::rw);
