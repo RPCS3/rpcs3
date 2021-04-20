17f88c1029b62144a9b2198580b2527db564cf4c
Megamouse <studienricky89@googlemail.com>
2021-04-20 20:12:49 +0200

    Update rpcs3/Input/ds4_pad_handler.cpp
    Co-authored-by: Florin9doi <2995486+Florin9doi@users.noreply.github.com>

--- rpcs3/Input/ds4_pad_handler.cpp
+++ rpcs3/Input/ds4_pad_handler.cpp
@@ -5,7 +5,7 @@
 LOG_CHANNEL(ds4_log, "DS4");
 
 constexpr id_pair SONY_DS4_ID_0 = {0x054C, 0xBA0};
-constexpr id_pair SONY_DS4_ID_1 = {0x054C, 0x5C4};
+constexpr id_pair SONY_DS4_ID_1 = {0x054C, 0x05C4};
 constexpr id_pair SONY_DS4_ID_2 = {0x054C, 0x09CC};
 
 constexpr id_pair ZEROPLUS_ID_0 = {0x0C12, 0x0E20};
