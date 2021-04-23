3bd33db3c34151701e56ab0b217f6d77ddfb5e6a
kd-11 <karokidii@gmail.com>
2021-04-23 23:43:05 +0300

    vk: Fix broken FBO swizzle for depth textures. WDB was basically broken for these.

--- rpcs3/Emu/RSX/VK/VKGSRender.cpp
+++ rpcs3/Emu/RSX/VK/VKGSRender.cpp
@@ -2166,7 +2166,7 @@ void VKGSRender::prepare_rtts(rsx::framebuffer_creation_context context)
 		const utils::address_range surface_range = m_depth_surface_info.get_memory_range();
 		if (g_cfg.video.write_depth_buffer)
 		{
-			const u32 gcm_format = (m_depth_surface_info.depth_format != rsx::surface_depth_format::z16) ? CELL_GCM_TEXTURE_DEPTH16 : CELL_GCM_TEXTURE_DEPTH24_D8;
+			const u32 gcm_format = (m_depth_surface_info.depth_format == rsx::surface_depth_format::z16) ? CELL_GCM_TEXTURE_DEPTH16 : CELL_GCM_TEXTURE_DEPTH24_D8;
 			m_texture_cache.lock_memory_region(
 				*m_current_command_buffer, m_rtts.m_bound_depth_stencil.second, surface_range, true,
 				m_depth_surface_info.width, m_depth_surface_info.height, m_framebuffer_layout.actual_zeta_pitch, gcm_format, true);
