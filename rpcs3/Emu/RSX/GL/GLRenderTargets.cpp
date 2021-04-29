3afa027b57d7f0d7093d3817c77d4fe93cdad110
Megamouse <studienricky89@googlemail.com>
2021-04-29 20:18:47 +0200

    GL: Remove unused copy functions

--- rpcs3/Emu/RSX/GL/GLRenderTargets.cpp
+++ rpcs3/Emu/RSX/GL/GLRenderTargets.cpp
@@ -394,16 +394,6 @@ void GLGSRender::init_buffers(rsx::framebuffer_creation_context context, bool /*
 	}
 }
 
-std::array<std::vector<std::byte>, 4> GLGSRender::copy_render_targets_to_memory()
-{
-	return {};
-}
-
-std::array<std::vector<std::byte>, 2> GLGSRender::copy_depth_stencil_buffer_to_memory()
-{
-	return {};
-}
-
 // Render target helpers
 void gl::render_target::clear_memory(gl::command_context& cmd)
 {
