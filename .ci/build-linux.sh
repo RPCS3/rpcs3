cffd7af9afdc1e944a50991c4bfb1925ef79c392
Zion Nimchuk <zionnimchuk@gmail.com>
2021-05-02 16:08:30 -0700

    Remove libopengl.so.0 hack, and use LEGACY CMake OpenGL option for AppImage builds

--- .ci/build-linux.sh
+++ .ci/build-linux.sh
@@ -51,6 +51,7 @@ cmake ..                                               \
     -DCMAKE_AR="$AR"                                   \
     -DCMAKE_RANLIB="$RANLIB"                           \
     -DUSE_SYSTEM_CURL=ON                               \
+    -DOpenGL_GL_PREFERENCE=LEGACY                      \
     -G Ninja
 
 ninja; build_status=$?;
