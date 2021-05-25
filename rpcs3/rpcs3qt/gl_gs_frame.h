83d3658ef01f61204ac5fa1dc8b8fd54c5ca481e
Megamouse <studienricky89@googlemail.com>
2021-05-26 00:16:24 +0200

    scale debug overlay based on current dpi settings
    Depending on the dpi settings, the debug overlay was almost unreadable.
    I also took the liberty to refactor some redundant client size calls and to add some margin to the left of the debug text.

--- rpcs3/rpcs3qt/gl_gs_frame.h
+++ rpcs3/rpcs3qt/gl_gs_frame.h
@@ -23,5 +23,5 @@ public:
 	draw_context_t make_context() override;
 	void set_current(draw_context_t ctx) override;
 	void delete_context(draw_context_t ctx) override;
-	void flip(draw_context_t context, bool skip_frame=false) override;
+	void flip(draw_context_t context, bool skip_frame = false) override;
 };
--- rpcs3/rpcs3qt/gs_frame.h
+++ rpcs3/rpcs3qt/gs_frame.h
@@ -90,6 +90,7 @@ protected:
 	void flip(draw_context_t context, bool skip_frame = false) override;
 	int client_width() override;
 	int client_height() override;
+	double client_device_pixel_ratio() const override;
 
 	bool event(QEvent* ev) override;
 
