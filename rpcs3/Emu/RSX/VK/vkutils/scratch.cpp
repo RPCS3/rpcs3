99bbcd15e19bb495ef95d60b50d793500c180b12
kd-11 <karokidii@gmail.com>
2021-07-26 22:31:56 +0300

    vk: Fix undefined format class for typeless helpers

--- rpcs3/Emu/RSX/VK/vkutils/scratch.cpp
+++ rpcs3/Emu/RSX/VK/vkutils/scratch.cpp
@@ -129,7 +129,8 @@ namespace vk
 
 			return new vk::image(*g_render_device, g_render_device->get_memory_mapping().device_local, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 				VK_IMAGE_TYPE_2D, format, new_width, new_height, 1, 1, 1, VK_SAMPLE_COUNT_1_BIT, VK_IMAGE_LAYOUT_UNDEFINED,
-				VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT, 0, VMM_ALLOCATION_POOL_SCRATCH);
+				VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT, 0, VMM_ALLOCATION_POOL_SCRATCH,
+				format_class);
 		};
 
 		const u32 key = (format_class << 24u) | format;
