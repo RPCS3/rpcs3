fd6cbb99865941512ea4011a270b4f58480d960a
Eladash <elad3356p@gmail.com>
2021-07-29 12:04:27 +0300

    Update vm_ptr.h

--- rpcs3/Emu/Memory/vm_ptr.h
+++ rpcs3/Emu/Memory/vm_ptr.h
@@ -214,6 +214,12 @@ namespace vm
 			return { ok, std::bit_cast<decltype(try_read().second)>(buf) };
 		}
 
+		template <bool = false> requires (!std::is_void_v<T>)
+		bool try_read(std::conditional_t<std::is_void_v<T>, char, std::remove_const_t<T>>& out) const
+		{
+			return vm::try_access(vm::cast(m_addr), &out, sizeof(T), false);
+		}
+
 		template <bool = false> requires (!std::is_void_v<T> && !std::is_const_v<T>)
 		bool try_write(const std::conditional_t<std::is_void_v<T>, char, T>& _in) const
 		{
