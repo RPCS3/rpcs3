c5d2c4636db776ace9ca61a981303f695223ee06
Justin Lewis <cpluspluspython@gmail.com>
2021-08-14 14:18:30 -0500

    Fixed header file issue on Linux

--- rpcs3/Emu/NP/signaling_handler.cpp
+++ rpcs3/Emu/NP/signaling_handler.cpp
@@ -4,10 +4,10 @@
 #include "Emu/IdManager.h"
 #include "Emu/Cell/Modules/cellSysutil.h"
 #include "np_handler.h"
-#include <ws2tcpip.h>
 
 #ifdef _WIN32
 #include <winsock2.h>
+#include <ws2tcpip.h>
 #else
 #include <netinet/in.h>
 #include <arpa/inet.h>
