c861a5c938275cb650a4a943e2d849f53012330b
kd-11 <karokidii@gmail.com>
2021-10-26 23:43:27 +0300

    rsx: Do not reuse/destroy sections that have references held
    - Avoids a situation where blit-dst and blit-src have overlapping ranges. Uploading blit-dst destroys blit-src and vice-versa.
      This is not the end of the world, but blit-src should be kept around until the operation is completed to avoid stale references!

--- rpcs3/Emu/RSX/Common/texture_cache_utils.h
+++ rpcs3/Emu/RSX/Common/texture_cache_utils.h
@@ -1042,7 +1042,7 @@ namespace rsx
 	 * Cached Texture Section
 	 */
 	template <typename derived_type, typename traits>
-	class cached_texture_section : public rsx::buffered_section
+	class cached_texture_section : public rsx::buffered_section, public rsx::ref_counted
 	{
 	public:
 		using ranged_storage_type       = ranged_storage<traits>;
@@ -1312,6 +1312,11 @@ namespace rsx
 
 		bool can_be_reused() const
 		{
+			if (has_refs()) [[unlikely]]
+			{
+				return false;
+			}
+
 			return !exists() || (is_dirty() && !is_locked());
 		}
 
