dbd41487bfbc37c88c7f49a6c44490ea83fec5c5
Eladash <elad3356p@gmail.com>
2022-02-23 22:00:25 +0200

    Update main.cpp

--- rpcs3/main.cpp
+++ rpcs3/main.cpp
@@ -371,7 +371,7 @@ void fmt_class_string<std::chrono::sys_time<typename std::chrono::system_clock::
 	std::ostringstream ss;
 	const std::time_t dateTime = std::chrono::system_clock::to_time_t(get_object(arg));
  	const std::tm tm = *std::localtime(&dateTime);
-	ss << std::put_time(&tm, "%Y-%m-%e-%H-%0M");
+	ss << std::put_time(&tm, "%Y-%m-%eT%H:%M:%S");
  	out += ss.str();
 }
 
