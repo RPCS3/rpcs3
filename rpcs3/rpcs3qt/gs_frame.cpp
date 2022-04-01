42a7b310c3899e1e55e7caa7cff1e4bb2425fb21
Megamouse <studienricky89@googlemail.com>
2022-04-01 19:48:59 +0200

    Revert "gs_frame: remove unnecessary CallFromMainThread"
    This reverts commit 3002e592c367c9414fafc3df6cefddcaab32cf71.

--- rpcs3/rpcs3qt/gs_frame.cpp
+++ rpcs3/rpcs3qt/gs_frame.cpp
@@ -830,9 +830,19 @@ bool gs_frame::event(QEvent* ev)
 			}
 
 			int result = QMessageBox::Yes;
-			m_gui_settings->ShowConfirmationBox(tr("Exit Game?"),
-				tr("Do you really want to exit the game?<br><br>Any unsaved progress will be lost!<br>"),
-				gui::ib_confirm_exit, &result, nullptr);
+			atomic_t<bool> called = false;
+
+			Emu.CallFromMainThread([this, &result, &called]()
+			{
+				m_gui_settings->ShowConfirmationBox(tr("Exit Game?"),
+					tr("Do you really want to exit the game?<br><br>Any unsaved progress will be lost!<br>"),
+					gui::ib_confirm_exit, &result, nullptr);
+
+				called = true;
+				called.notify_one();
+			});
+
+			called.wait(false);
 
 			if (result != QMessageBox::Yes)
 			{
