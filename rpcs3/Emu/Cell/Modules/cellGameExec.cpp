c656ad6568e87c3a54cf1322ac0533807eb140e8
clienthax <clienthax@gmail.com>
2022-04-10 14:14:17 +0100

    Add stubs.

--- rpcs3/Emu/Cell/Modules/cellGameExec.cpp
+++ rpcs3/Emu/Cell/Modules/cellGameExec.cpp
@@ -76,13 +76,13 @@ error_code cellGameGetHomeLaunchOptionPath(vm::ptr<char> commonPath, vm::ptr<cha
 	return CELL_GAME_ERROR_NOAPP;
 }
 
-error_code cellGameExecGame()
+error_code cellGameExecGame(u32 type, vm::ptr<char> dirName, u32 options, u32 memContainer, u32 execData, u32 userData)
 {
 	UNIMPLEMENTED_FUNC(cellGameExec);
 	return CELL_OK;
 }
 
-error_code cellGameDeleteGame()
+error_code cellGameDeleteGame(vm::ptr<char> dirName, u32 memContainer)
 {
 	UNIMPLEMENTED_FUNC(cellGameExec);
 	return CELL_OK;
@@ -121,6 +121,18 @@ error_code cellGameGetBootGameInfo(vm::ptr<u32> type, vm::ptr<char> dirName, vm:
 	return CELL_OK;
 }
 
+error_code cellGameGetExitGameInfo(vm::ptr<u32> status, vm::ptr<u32> type, vm::ptr<char> dirName, vm::ptr<u32> execData, vm::ptr<u32> userData)
+{
+	UNIMPLEMENTED_FUNC(cellGameExec);
+	return CELL_OK;
+}
+
+error_code cellGameGetList(u32 listBufNum, u32 unk, vm::ptr<u32> listNum, vm::ptr<u32> getListNum, u32 memContainer)
+{
+	UNIMPLEMENTED_FUNC(cellGameExec);
+	return CELL_OK;
+}
+
 DECLARE(ppu_module_manager::cellGameExec)("cellGameExec", []()
 {
 	REG_FUNC(cellGameExec, cellGameSetExitParam);
@@ -131,4 +143,6 @@ DECLARE(ppu_module_manager::cellGameExec)("cellGameExec", []()
 	REG_FUNC(cellGameExec, cellGameExecGame);
 	REG_FUNC(cellGameExec, cellGameDeleteGame);
 	REG_FUNC(cellGameExec, cellGameGetBootGameInfo);
+	REG_FUNC(cellGameExec, cellGameGetExitGameInfo);
+	REG_FUNC(cellGameExec, cellGameGetList);
 });
