5e1babdf4d7dd11dcd8d46df0988522f500f62a1
nastys <nastys@users.noreply.github.com>
2022-04-14 03:22:16 +0200

    MoltenVK as a submodule

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 brew update
-brew install llvm@13 molten-vk vulkan-headers sdl2 nasm qt@5 ninja cmake glew git p7zip create-dmg ccache
+brew install llvm@13 sdl2 nasm qt@5 ninja cmake glew git p7zip create-dmg ccache
 
 export MACOSX_DEPLOYMENT_TARGET=11.6
 export CXX=clang++
@@ -10,15 +10,31 @@ export Qt5_DIR="/usr/local/opt/qt@5/lib/cmake/Qt5"
 export PATH="/usr/local/opt/llvm/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Apple/usr/bin"
 export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
 export CPPFLAGS="-I/usr/local/opt/llvm/include -msse -msse2 -mcx16 -no-pie"
-export CPLUS_INCLUDE_PATH="/usr/local/opt/molten-vk/include"
-export VULKAN_SDK="/usr/local/opt/molten-vk"
-export VK_ICD_FILENAMES="$VULKAN_SDK/share/vulkan/icd.d/MoltenVK_icd.json"
+export VULKAN_SDK="$CIRRUS_WORKING_DIR/3rdparty/MoltenVK/MoltenVK/MoltenVK"
+export VK_ICD_FILENAMES="$VULKAN_SDK/dylib/macOS/MoltenVK_icd.json"
+export CPLUS_INCLUDE_PATH="$VULKAN_SDK/include"
+export Vulkan_INCLUDE_DIR="$VULKAN_SDK/include"
+export Vulkan_LIBRARY="$VULKAN_SDK/lib/libvulkan.dylib"
 
 git submodule update --init --recursive --depth 1
 
 # 3rdparty fixes
-ln -s "$VULKAN_SDK/lib/libMoltenVK.dylib" "$VULKAN_SDK/lib/libvulkan.dylib"
 sed -i '' "s/extern const double NSAppKitVersionNumber;/const double NSAppKitVersionNumber = 1343;/g" 3rdparty/hidapi/hidapi/mac/hid.c
+
+# build MoltenVK
+cd 3rdparty/MoltenVK/MoltenVK
+./fetchDependencies --macos
+xcodebuild build -quiet -project MoltenVKPackaging.xcodeproj -scheme "MoltenVK Package (macOS only)" -configuration "Release" -arch "x86_64"
+cd ../../../
+mkdir "$VULKAN_SDK/lib"
+ln "$VULKAN_SDK/dylib/macOS/libMoltenVK.dylib" "$VULKAN_SDK/lib/libMoltenVK.dylib"
+ln "$VULKAN_SDK/dylib/macOS/libMoltenVK.dylib" "$VULKAN_SDK/lib/libvulkan.dylib"
+ln "$VULKAN_SDK/MoltenVK.xcframework/macos-x86_64/libMoltenVK.a" "$VULKAN_SDK/lib/libvulkan.a"
+mkdir "$VULKAN_SDK/bin"
+ln -s "$VULKAN_SDK/../Package/Release/MoltenVKShaderConverter/Tools/MoltenVKShaderConverter" "$VULKAN_SDK/bin/MoltenVKShaderConverter"
+ln -s "$VULKAN_SDK/../Package/Release/MoltenVKShaderConverter/include/MoltenVKShaderConverter" "$VULKAN_SDK/include/MoltenVKShaderConverter"
+mkdir -p "$VULKAN_SDK/share/vulkan/icd.d"
+ln "$VULKAN_SDK/icd/MoltenVK_icd.json" "$VULKAN_SDK/share/vulkan/icd.d/MoltenVK_icd.json"
 
 mkdir build && cd build || exit 1
 
