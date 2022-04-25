c8700dd24684d04fc253a04265caa5a41a4ef394
Megamouse <studienricky89@googlemail.com>
2022-04-26 00:15:27 +0200

    Input: log interception and its source function

--- rpcs3/Emu/Io/interception.cpp
+++ rpcs3/Emu/Io/interception.cpp
@@ -5,14 +5,18 @@
 #include "Input/pad_thread.h"
 #include "Emu/IdManager.h"
 
+LOG_CHANNEL(input_log, "Input");
+
 namespace input
 {
 	atomic_t<bool> g_pads_intercepted{false};
 	atomic_t<bool> g_keyboards_intercepted{false};
 	atomic_t<bool> g_mice_intercepted{false};
 
-	void SetIntercepted(bool pads_intercepted, bool keyboards_intercepted, bool mice_intercepted)
+	void SetIntercepted(bool pads_intercepted, bool keyboards_intercepted, bool mice_intercepted, const char* func)
 	{
+		input_log.warning("SetIntercepted: pads=%d, keyboards=%d, mice=%d, src=%s)", pads_intercepted, keyboards_intercepted, mice_intercepted, func);
+
 		g_pads_intercepted = pads_intercepted;
 		g_keyboards_intercepted = keyboards_intercepted;
 		g_mice_intercepted = mice_intercepted;
@@ -30,8 +34,8 @@ namespace input
 		}
 	}
 
-	void SetIntercepted(bool all_intercepted)
+	void SetIntercepted(bool all_intercepted, const char* func)
 	{
-		SetIntercepted(all_intercepted, all_intercepted, all_intercepted);
+		SetIntercepted(all_intercepted, all_intercepted, all_intercepted, func);
 	}
 }
--- rpcs3/Emu/Io/interception.h
+++ rpcs3/Emu/Io/interception.h
@@ -8,6 +8,6 @@ namespace input
 	extern atomic_t<bool> g_keyboards_intercepted;
 	extern atomic_t<bool> g_mice_intercepted;
 
-	void SetIntercepted(bool pads_intercepted, bool keyboards_intercepted, bool mice_intercepted);
-	void SetIntercepted(bool all_intercepted);
+	void SetIntercepted(bool pads_intercepted, bool keyboards_intercepted, bool mice_intercepted, const char* func = __builtin_FUNCTION());
+	void SetIntercepted(bool all_intercepted, const char* func = __builtin_FUNCTION());
 }
