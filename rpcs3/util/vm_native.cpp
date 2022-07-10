9e57efe82c4044dfdae1317a1e582bc2ec83f3cc
sguo35 <sguo35@gmail.com>
2022-07-09 23:38:48 -0700

    spu: implement assembly functions for arm64

--- rpcs3/util/vm_native.cpp
+++ rpcs3/util/vm_native.cpp
@@ -260,7 +260,7 @@ namespace utils
 
 #ifdef __APPLE__
 #ifdef ARCH_ARM64
-        auto ptr = ::mmap(use_addr, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE | MAP_JIT | c_map_noreserve, -1, 0);
+		auto ptr = ::mmap(use_addr, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE | MAP_JIT | c_map_noreserve, -1, 0);
 #else
 		auto ptr = ::mmap(use_addr, size, PROT_NONE, MAP_ANON | MAP_PRIVATE | MAP_JIT | c_map_noreserve, -1, 0);
 #endif
