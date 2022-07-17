69e1226821e8a5e89dd3142d675de4daae5c6dc0
kd-11 <karokidii@gmail.com>
2022-07-17 13:59:14 +0300

    gl: Fix compute invocation counts for format handling code

--- rpcs3/Emu/RSX/GL/GLTexture.cpp
+++ rpcs3/Emu/RSX/GL/GLTexture.cpp
@@ -856,7 +856,7 @@ namespace gl
 				if (driver_caps.ARB_compute_shader_supported)
 				{
 					// 0. Preconf
-					mem_layout.alignment = caps.alignment;
+					mem_layout.alignment = static_cast<u8>(caps.alignment);
 					mem_layout.swap_bytes = op.require_swap;
 					mem_layout.format = gl_format;
 					mem_layout.type = gl_type;
