d1ff443f77e8c0ebfb224f690ca5b40c8ef3822f
Eladash <elad3356p@gmail.com>
2022-07-25 23:24:55 +0300

    Savestates/MSVC: Replace bugged std::unique (compiler bug)

--- rpcs3/Emu/Memory/vm.cpp
+++ rpcs3/Emu/Memory/vm.cpp
@@ -2008,9 +2008,26 @@ namespace vm
 			if (loc) loc->get_shared_memory(shared);
 		}
 
-		shared.erase(std::unique(shared.begin(), shared.end(), [](auto& a, auto& b) { return a.first == b.first; }), shared.end());
-
 		std::map<utils::shm*, usz> shared_map;
+
+#ifndef _MSC_VER
+		shared.erase(std::unique(shared.begin(), shared.end(), [](auto& a, auto& b) { return a.first == b.first; }), shared.end());
+#else
+		// Workaround for bugged std::unique
+		for (auto it = shared.begin(); it != shared.end();)
+		{
+			if (shared_map.count(it->first))
+			{
+				it = shared.erase(it);
+				continue;
+			}
+
+			shared_map.emplace(it->first, 0);
+			it++;
+		}
+
+		shared_map.clear();
+#endif
 
 		for (auto& p : shared)
 		{
