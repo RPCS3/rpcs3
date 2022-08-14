edb39039ddabc6321c81c655eaefb128ea8195fc
kd-11 <karokidii@gmail.com>
2022-08-14 20:25:52 +0300

    Fix linux build

--- Utilities/stack_trace.h
+++ Utilities/stack_trace.h
@@ -4,13 +4,13 @@
 
 namespace utils
 {
-	std::vector<void*> backtrace(int max_depth = 255);
-	std::vector<std::string> backtrace_symbols(const std::vector<void*>& stack);
+	std::vector<void*> get_backtrace(int max_depth = 255);
+	std::vector<std::string> get_backtrace_symbols(const std::vector<void*>& stack);
 
 	FORCE_INLINE void print_trace(logs::channel& logger, int max_depth = 255)
 	{
-		const auto trace = backtrace(max_depth);
-		const auto lines = backtrace_symbols(trace);
+		const auto trace = get_backtrace(max_depth);
+		const auto lines = get_backtrace_symbols(trace);
 
 		for (const auto& line : lines)
 		{
