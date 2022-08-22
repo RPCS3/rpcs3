cd1ae29cb4a411e1a6a712d1381250cba6cd7277
Eladash <elad3356p@gmail.com>
2022-08-22 17:48:45 +0300

    Bugfix after #12523

--- rpcs3/Emu/Cell/SPUThread.cpp
+++ rpcs3/Emu/Cell/SPUThread.cpp
@@ -2821,8 +2821,13 @@ bool spu_thread::do_putllc(const spu_mfc_cmd& args)
 		if (cmp_rdata(to_write, rdata))
 		{
 			// Writeback of unchanged data. Only check memory change
-			raddr = 0; // Disable notification
-			return cmp_rdata(rdata, vm::_ref<spu_rdata_t>(addr)) && res.compare_and_swap_test(rtime, rtime + 128);
+			if (cmp_rdata(rdata, vm::_ref<spu_rdata_t>(addr)) && res.compare_and_swap_test(rtime, rtime + 128))
+			{
+				raddr = 0; // Disable notification
+				return true;
+			}
+
+			return false;
 		}
 
 		auto [_oldd, _ok] = res.fetch_op([&](u64& r)
