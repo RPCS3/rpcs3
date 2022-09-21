b0c797b099cb0ccbf2f90dc36195ea4814e4a10b
Elad Ashkenazi <elad3356p@gmail.com>
2022-09-21 20:49:25 +0300

    Hotfix after #12678

--- rpcs3/Emu/Cell/lv2/sys_event.cpp
+++ rpcs3/Emu/Cell/lv2/sys_event.cpp
@@ -405,7 +405,7 @@ error_code sys_event_queue_tryreceive(ppu_thread& ppu, u32 equeue_id, vm::ptr<sy
 	lock.unlock();
 	ppu.check_state();
 
-	std::copy_n(event_array.get_ptr(), count, events.begin());
+	std::copy_n(events.begin(), event_array.get_ptr());
 	*number = count;
 
 	return CELL_OK;
