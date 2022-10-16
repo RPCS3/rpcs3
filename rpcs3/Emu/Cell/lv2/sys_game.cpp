4505e684027ea711caf05f65a1799cf6b61b7124
brian218 <brian218@users.noreply.github.com>
2022-10-16 13:23:48 +0800

    Implemented sys_game_watchdog syscalls

--- rpcs3/Emu/Cell/lv2/sys_game.cpp
+++ rpcs3/Emu/Cell/lv2/sys_game.cpp
@@ -2,11 +2,81 @@
 #include "util/sysinfo.hpp"
 #include "Emu/Memory/vm_ptr.h"
 #include "Emu/Cell/ErrorCodes.h"
+#include "Emu/System.h"
 #include "Emu/system_config.h"
+
+#include <thread>
 
 #include "sys_game.h"
 
 LOG_CHANNEL(sys_game);
+
+atomic_t<bool> watchdog_stopped = true;
+atomic_t<u64> watchdog_last_clear;
+u64 get_timestamp()
+{
+	return (get_system_time() - Emu.GetPauseTime());
+}
+
+error_code _sys_game_watchdog_start(u32 timeout)
+{
+	sys_game.trace("sys_game_watchdog_start(timeout=%d)", timeout);
+
+	if (!watchdog_stopped)
+	{
+		return CELL_EABORT;
+	}
+
+	auto watchdog = [=]()
+	{
+		while (!watchdog_stopped)
+		{
+			if (Emu.IsStopped() || get_timestamp() - watchdog_last_clear > timeout * 1000000)
+			{
+				watchdog_stopped = true;
+				if (!Emu.IsStopped())
+				{
+					sys_game.warning("Watchdog timeout! Restarting the game...");
+					Emu.CallFromMainThread([]()
+						{
+							Emu.Restart();
+						});
+				}
+				break;
+			}
+			std::this_thread::sleep_for(1s);
+		}
+	};
+
+	watchdog_stopped = false;
+	watchdog_last_clear = get_timestamp();
+	std::thread(watchdog).detach();
+
+	return CELL_OK;
+}
+
+error_code _sys_game_watchdog_stop()
+{
+	sys_game.trace("sys_game_watchdog_stop()");
+
+	watchdog_stopped = true;
+
+	return CELL_OK;
+}
+
+error_code _sys_game_watchdog_clear()
+{
+	sys_game.trace("sys_game_watchdog_clear()");
+
+	watchdog_last_clear = get_timestamp();
+
+	return CELL_OK;
+}
+
+u64 _sys_game_get_system_sw_version()
+{
+	return stof(utils::get_firmware_version()) * 10000;
+}
 
 error_code _sys_game_board_storage_read(vm::ptr<u8> buffer1, vm::ptr<u8> buffer2)
 {
@@ -34,9 +104,4 @@ error_code _sys_game_board_storage_read(vm::ptr<u8> buffer1, vm::ptr<u8> buffer2
 	*buffer2 = 0x00;
 
 	return CELL_OK;
-}
-
-u64 _sys_game_get_system_sw_version()
-{
-	return stof(utils::get_firmware_version()) * 10000;
 }
