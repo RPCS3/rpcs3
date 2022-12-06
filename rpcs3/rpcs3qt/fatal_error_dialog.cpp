0aaa40cd53d8f5ed9d03850eb6f16ba7f28ce40c
kd-11 <karokidii@gmail.com>
2022-12-06 18:26:38 +0300

    ux: Update vcredist links and optionally disable help text from error dialog

--- rpcs3/rpcs3qt/fatal_error_dialog.cpp
+++ rpcs3/rpcs3qt/fatal_error_dialog.cpp
@@ -4,9 +4,44 @@
 #include <QTextDocument>
 #include <QIcon>
 
-fatal_error_dialog::fatal_error_dialog(std::string_view text, bool is_html) : QMessageBox()
+const QString document_with_help_text = R"(
+<style>
+	p {white-space: nowrap;}
+</style>
+<p>
+	%1<br>
+	%2<br>
+	<a href='https://github.com/RPCS3/rpcs3/wiki/How-to-ask-for-Support'>https://github.com/RPCS3/rpcs3/wiki/How-to-ask-for-Support</a><br>
+	%3<br>
+</p>
+)";
+
+const QString document_without_help_text = R"(
+<style>
+	p {white-space: nowrap;}
+</style>
+<p>
+	%1<br>
+</p>
+)";
+
+fatal_error_dialog::fatal_error_dialog(std::string_view text, bool is_html, bool include_help_text) : QMessageBox()
 {
-	const QString msg = QString::fromUtf8(text.data(), text.size());
+	const QString qstr = QString::fromUtf8(text.data(), text.size());
+	const QString msg = is_html ? qstr : Qt::convertFromPlainText(qstr);
+
+	QString document_body;
+	if (include_help_text) [[likely]]
+	{
+		document_body = document_with_help_text
+			.arg(msg)
+			.arg(tr("HOW TO REPORT ERRORS:"))
+			.arg(tr("Please, don't send incorrect reports. Thanks for understanding."));
+	}
+	else
+	{
+		document_body = document_without_help_text.arg(msg);
+	}
 
 #ifndef __APPLE__
 	setWindowIcon(QIcon(":/rpcs3.ico"));
@@ -14,19 +49,6 @@ fatal_error_dialog::fatal_error_dialog(std::string_view text, bool is_html) : QM
 	setWindowTitle(tr("RPCS3: Fatal Error"));
 	setIcon(QMessageBox::Icon::Critical);
 	setTextFormat(Qt::TextFormat::RichText);
-	setText(QString(R"(
-			<style>
-				p {white-space: nowrap;}
-			</style>
-			<p>
-				%1<br>
-				%2<br>
-				<a href='https://github.com/RPCS3/rpcs3/wiki/How-to-ask-for-Support'>https://github.com/RPCS3/rpcs3/wiki/How-to-ask-for-Support</a><br>
-				%3<br>
-			</p>
-			)")
-		.arg(is_html ? msg : Qt::convertFromPlainText(msg))
-		.arg(tr("HOW TO REPORT ERRORS:"))
-		.arg(tr("Please, don't send incorrect reports. Thanks for understanding.")));
+	setText(document_body);
 	layout()->setSizeConstraint(QLayout::SetFixedSize);
 }
