65ae85061fdbbe50fed0a0f9b7c7fda320fdfeca
brian218 <17809637+brian218@users.noreply.github.com>
2022-12-13 14:33:17 +0800

    Misc and minor improvements of syscalls used by VSH (#13067)

--- rpcs3/Emu/Cell/lv2/sys_ss.h
+++ rpcs3/Emu/Cell/lv2/sys_ss.h
@@ -20,8 +20,8 @@ struct CellSsOpenPSID
 
 error_code sys_ss_random_number_generator(u64 pkg_id, vm::ptr<void> buf, u64 size);
 error_code sys_ss_access_control_engine(u64 pkg_id, u64 a2, u64 a3);
-s32 sys_ss_get_console_id(vm::ptr<u8> buf);
-s32 sys_ss_get_open_psid(vm::ptr<CellSsOpenPSID> psid);
+error_code sys_ss_get_console_id(vm::ptr<u8> buf);
+error_code sys_ss_get_open_psid(vm::ptr<CellSsOpenPSID> psid);
 error_code sys_ss_appliance_info_manager(u32 code, vm::ptr<u8> buffer);
 error_code sys_ss_get_cache_of_product_mode(vm::ptr<u8> ptr);
 error_code sys_ss_secure_rtc(u64 cmd, u64 a2, u64 a3, u64 a4);
