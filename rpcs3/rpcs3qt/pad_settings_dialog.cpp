52b67b46fcaab6c0838a44761900065983098f1c
Anton Kourganov <37295096+DungeonMaster47@users.noreply.github.com>
2023-01-24 00:00:08 +0300

    Allow unbinding pad buttons by right clicking (#13283)

--- rpcs3/rpcs3qt/pad_settings_dialog.cpp
+++ rpcs3/rpcs3qt/pad_settings_dialog.cpp
@@ -998,6 +998,23 @@ bool pad_settings_dialog::eventFilter(QObject* object, QEvent* event)
 	{
 	case QEvent::MouseButtonRelease:
 	{
+		// On right click clear binding if we are not remapping pad button
+		if (m_button_id == button_ids::id_pad_begin)
+		{
+			QMouseEvent* mouse_event = static_cast<QMouseEvent*>(event);
+			if (const auto button = qobject_cast<QPushButton*>(object); button && mouse_event->button() == Qt::RightButton)
+			{
+				if (const int button_id = m_pad_buttons->id(button); m_cfg_entries.contains(button_id))
+				{
+					m_cfg_entries[button_id].key.clear();
+					m_cfg_entries[button_id].text.clear();
+					UpdateLabels();
+
+					return true;
+				}
+			}
+		}
+
 		// Disabled buttons should not absorb mouseclicks
 		event->ignore();
 		break;
