49455965c0e2aa9385d78811e29dbe0de1c974e0
brian218 <brian218@users.noreply.github.com>
2023-01-23 11:26:43 +0800

    sys_game: Implemented sys_game_get_rtc_status() & Updated sys_game_board_storage_read()

--- rpcs3/Emu/Cell/lv2/sys_game.h
+++ rpcs3/Emu/Cell/lv2/sys_game.h
@@ -7,3 +7,4 @@ error_code _sys_game_watchdog_stop();
 error_code _sys_game_watchdog_clear();
 u64 _sys_game_get_system_sw_version();
 error_code _sys_game_board_storage_read(vm::ptr<u8> buffer1, vm::ptr<u8> buffer2);
+error_code _sys_game_get_rtc_status(vm::ptr<s32> status);
