0885884839c72936f7d47263ca56697fa3146766
kd-11 <karokidii@gmail.com>
2023-02-01 23:04:08 +0300

    rsx: Implement notification queue

--- rpcs3/Emu/RSX/Overlays/overlay_message.h
+++ rpcs3/Emu/RSX/Overlays/overlay_message.h
@@ -7,21 +7,29 @@ namespace rsx
 {
 	namespace overlays
 	{
-		class message_item
+		enum message_pin_location
+		{
+			top,
+			bottom
+		};
+
+		class message_item : public overlay_element
 		{
 		public:
 			template <typename T>
-			message_item(T msg_id, u64 expiration, std::shared_ptr<atomic_t<u32>> refs);
-			void update(usz index, u64 time);
+			message_item(T msg_id, u64 expiration, std::shared_ptr<atomic_t<u32>> refs, std::unique_ptr<overlay_element> icon = {});
+			void update(usz index, u64 time, u16 origin, int grow_direction);
 
 			u64 get_expiration() const;
-			compiled_resource get_compiled();
+			compiled_resource& get_compiled();
 
 		private:
 			label m_text{};
+			std::unique_ptr<overlay_element> m_icon{};
 			animation_color_interpolate m_fade_animation;
 
 			u64 m_expiration_time = 0;
+			u64 m_visible_duration = 0;
 			std::shared_ptr<atomic_t<u32>> m_refs;
 			bool m_processed      = false;
 			usz m_cur_pos         = umax;
@@ -34,32 +42,56 @@ namespace rsx
 			compiled_resource get_compiled() override;
 
 			template <typename T>
-			void queue_message(T msg_id, u64 expiration, std::shared_ptr<atomic_t<u32>> refs)
+			void queue_message(
+				T msg_id,
+				u64 expiration,
+				std::shared_ptr<atomic_t<u32>> refs,
+				message_pin_location location = message_pin_location::top,
+				std::unique_ptr<overlay_element> icon = {})
 			{
 				std::lock_guard lock(m_mutex_queue);
+
+				auto& queue = location == message_pin_location::top
+					? m_ready_queue_top
+					: m_ready_queue_bottom;
 
 				if constexpr (std::is_same_v<T, std::initializer_list<localized_string_id>>)
 				{
 					for (auto id : msg_id)
 					{
-						m_queue.emplace_back(id, expiration, refs);
+						queue.emplace_back(id, expiration, refs, icon);
 					}
 				}
 				else
 				{
-					m_queue.emplace_back(msg_id, expiration, std::move(refs));
+					queue.emplace_back(msg_id, expiration, std::move(refs), std::move(icon));
 				}
 
 				visible = true;
 			}
 
 		private:
+			const u32 max_visible_items = 3;
 			shared_mutex m_mutex_queue;
-			std::deque<message_item> m_queue;
+
+			// Top and bottom enqueued sets
+			std::deque<message_item> m_ready_queue_top;
+			std::deque<message_item> m_ready_queue_bottom;
+
+			// Top and bottom visible sets
+			std::deque<message_item> m_vis_items_top;
+			std::deque<message_item> m_vis_items_bottom;
+
+			void update_queue(std::deque<message_item>& vis_set, std::deque<message_item>& ready_set, u16 origin, int grow_direction);
 		};
 
 		template <typename T>
-		void queue_message(T msg_id, u64 expiration = 5'000'000, std::shared_ptr<atomic_t<u32>> refs = {})
+		void queue_message(
+			T msg_id,
+			u64 expiration = 5'000'000,
+			std::shared_ptr<atomic_t<u32>> refs = {},
+			message_pin_location location = message_pin_location::top,
+			std::unique_ptr<overlay_element> icon = {})
 		{
 			if (auto manager = g_fxo->try_get<rsx::overlays::display_manager>())
 			{
@@ -69,7 +101,7 @@ namespace rsx
 					msg_overlay = std::make_shared<rsx::overlays::message>();
 					msg_overlay = manager->add(msg_overlay);
 				}
-				msg_overlay->queue_message(msg_id, expiration, std::move(refs));
+				msg_overlay->queue_message(msg_id, expiration, std::move(refs), location, std::move(icon));
 			}
 		}
 
