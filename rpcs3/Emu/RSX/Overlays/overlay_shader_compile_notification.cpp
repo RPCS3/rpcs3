262ecda2bfc826b11ceb36c561ac3404d2bfecf9
kd-11 <karokidii@gmail.com>
2023-02-03 04:40:11 +0300

    rsx/overlays: Cache the loading animated icon
    Smooths out some frametime spikes when showing the notification message

--- rpcs3/Emu/RSX/Overlays/overlay_shader_compile_notification.cpp
+++ rpcs3/Emu/RSX/Overlays/overlay_shader_compile_notification.cpp
@@ -7,14 +7,22 @@ namespace rsx
 {
 	namespace overlays
 	{
+		static std::shared_ptr<loading_icon24> s_loading_icon24;
+
 		void show_shader_compile_notification()
 		{
+			if (!s_loading_icon24)
+			{
+				// Creating the icon requires FS read, so it is important to cache it
+				s_loading_icon24 = std::make_shared<loading_icon24>();
+			}
+
 			queue_message(
 				localized_string_id::RSX_OVERLAYS_COMPILING_SHADERS,
 				5'000'000,
 				{},
 				message_pin_location::bottom,
-				std::make_shared<loading_icon24>());
+				s_loading_icon24);
 		}
 	}
 }
