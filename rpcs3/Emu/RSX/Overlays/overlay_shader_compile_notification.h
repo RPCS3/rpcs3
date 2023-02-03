2503525016704f56d027c110e652eab9a6771c0a
kd-11 <karokidii@gmail.com>
2023-02-03 03:24:05 +0300

    rsx/overlays: Move shader compiling notification to a separate cpp file

--- rpcs3/Emu/RSX/Overlays/overlay_shader_compile_notification.h
+++ rpcs3/Emu/RSX/Overlays/overlay_shader_compile_notification.h
@@ -1,21 +1,9 @@
 #pragma once
 
-#include "overlays.h"
-#include "overlay_message.h"
-#include "overlay_loading_icon.hpp"
-
 namespace rsx
 {
 	namespace overlays
 	{
-		static inline void show_shader_compile_notification()
-		{
-			queue_message(
-				localized_string_id::RSX_OVERLAYS_COMPILING_SHADERS,
-				5'000'000,
-				{},
-				message_pin_location::bottom,
-				std::make_unique<loading_icon24>());
-		}
+		void show_shader_compile_notification();
 	}
 }
