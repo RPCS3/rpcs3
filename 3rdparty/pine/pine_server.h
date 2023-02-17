a6e23d2aa3c5f454a56441a88a527c0088db2d98
Elad Ashkenazi <elad3356p@gmail.com>
2023-02-17 14:28:46 +0200

    PINE: Fix crash on exit

--- 3rdparty/pine/pine_server.h
+++ 3rdparty/pine/pine_server.h
@@ -354,6 +354,28 @@ namespace pine
 		 */
 		bool StartSocket()
 		{
+			::pollfd poll_fd{};
+
+			for (int pending_connection = 0; pending_connection != 1;)
+			{
+				if (thread_ctrl::state() == thread_state::aborting)
+				{
+					return false;
+				}
+
+				std::memset(&poll_fd, 0, sizeof(poll_fd));
+				poll_fd.events  = POLLIN;
+				poll_fd.revents = 0;
+				poll_fd.fd      = m_sock;
+
+#ifdef _WIN32
+				// Try to wait for an incoming connection
+				pending_connection = ::WSAPoll(&poll_fd, 1, 10);
+#else
+				pending_connection = ::poll(&poll_fd, 1, 10);
+#endif
+			}
+
 			m_msgsock = accept(m_sock, 0, 0);
 
 			if (m_msgsock == -1)
