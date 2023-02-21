6cdafd4ef518165ac9d06b1a21c21ef3d26ac496
brian218 <brian218@users.noreply.github.com>
2023-02-21 20:16:49 +0800

    sys_game: LV2 board storage minor fixups

--- rpcs3/Emu/Cell/lv2/sys_game.h
+++ rpcs3/Emu/Cell/lv2/sys_game.h
@@ -6,6 +6,6 @@ error_code _sys_game_watchdog_start(u32 timeout);
 error_code _sys_game_watchdog_stop();
 error_code _sys_game_watchdog_clear();
 u64 _sys_game_get_system_sw_version();
-error_code _sys_game_board_storage_read(vm::ptr<u8> buffer1, vm::ptr<u8> buffer2);
-error_code _sys_game_board_storage_write(vm::ptr<u8> buffer1, vm::ptr<u8> buffer2);
+error_code _sys_game_board_storage_read(vm::ptr<u8> buffer, vm::ptr<u8> status);
+error_code _sys_game_board_storage_write(vm::ptr<u8> buffer, vm::ptr<u8> status);
 error_code _sys_game_get_rtc_status(vm::ptr<s32> status);
