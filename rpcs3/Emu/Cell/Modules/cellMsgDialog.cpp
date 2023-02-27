b14303b0f727c2f1dbb0ba1e14c900102ef8f71f
Elad Ashkenazi <elad3356p@gmail.com>
2023-02-27 14:07:35 +0200

    cellMsgDialog: fix use-after-free

--- rpcs3/Emu/Cell/Modules/cellMsgDialog.cpp
+++ rpcs3/Emu/Cell/Modules/cellMsgDialog.cpp
@@ -166,7 +166,7 @@ error_code open_msg_dialog(bool is_blocking, u32 type, vm::cptr<char> msgString,
 
 		const auto notify = std::make_shared<atomic_t<bool>>(false);
 
-		const auto res = manager->create<rsx::overlays::message_dialog>()->show(is_blocking, msgString.get_ptr(), _type, [callback, userData, &return_code, is_blocking, &notify](s32 status)
+		const auto res = manager->create<rsx::overlays::message_dialog>()->show(is_blocking, msgString.get_ptr(), _type, [callback, userData, &return_code, is_blocking, notify](s32 status)
 		{
 			if (is_blocking && return_code)
 			{
