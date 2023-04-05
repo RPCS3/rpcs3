c6ab1aa2270453f6ce9e281f544221668f17e173
Elad Ashkenazi <elad3356p@gmail.com>
2023-04-05 22:53:01 +0300

    Fix emulator crash when stopping emulation after being paused (#13530)

--- rpcs3/Emu/System.cpp
+++ rpcs3/Emu/System.cpp
@@ -2498,7 +2498,13 @@ void Emulator::GracefulShutdown(bool allow_autoexit, bool async_op, bool savesta
 		for (u32 i = 100; i < 140; i++)
 		{
 			std::this_thread::sleep_for(50ms);
-			Resume(); // TODO: Prevent pausing by other threads while in this loop
+
+			// TODO: Prevent pausing by other threads while in this loop
+			CallFromMainThread([this]()
+			{
+				Resume();
+			}, nullptr, true, read_counter);
+
 			process_qt_events(); // Is nullified when performed on non-main thread
 
 			if (!read_sysutil_signal && read_counter != get_sysutil_cb_manager_read_count())
