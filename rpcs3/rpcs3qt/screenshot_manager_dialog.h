957713015a9f5873511fca33fa875f1d9ea83fad
Megamouse <studienricky89@googlemail.com>
2023-04-25 10:04:17 +0200

    screenshot manager: use flow layout
    This whole screenshot thing was really janky, as I added it in half a day or so. But this commit should make everything smooth.
    Sadly there is no real lazy loading yet (icons are loaded async, but indiscriminately).

--- rpcs3/rpcs3qt/screenshot_manager_dialog.h
+++ rpcs3/rpcs3qt/screenshot_manager_dialog.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#include "flow_widget.h";
+
 #include <QDialog>
 #include <QFutureWatcher>
-#include <QIcon>
+#include <QPixmap>
 #include <QSize>
-
-class QListWidget;
-class QListWidgetItem;
+#include <QLabel>
+#include <QEvent>
+#include <QThread>
+#include <array>
 
 class screenshot_manager_dialog : public QDialog
 {
@@ -16,38 +19,49 @@ public:
 	screenshot_manager_dialog(QWidget* parent = nullptr);
 	~screenshot_manager_dialog();
 
-protected:
-	void resizeEvent(QResizeEvent* event) override;
-
-private Q_SLOTS:
-	void update_icon(int index) const;
+	bool eventFilter(QObject* watched, QEvent* event) override;
 
 Q_SIGNALS:
-	void signal_icon_change(int index, const QString& path);
+	void signal_entry_parsed(const QString& path);
+	void signal_icon_preview(const QString& path);
+
+public Q_SLOTS:
+	void update_icon(const QPixmap& pixmap);
+
+private Q_SLOTS:
+	void add_entry(const QString& path);
+	void show_preview(const QString& path);
+
+protected:
+	void showEvent(QShowEvent* event) override;
 
 private:
-	static void show_preview(QListWidgetItem* item);
-	void update_icons(int value);
+	void reload();
 
-	enum item_role
-	{
-		source = Qt::UserRole,
-		loaded = Qt::UserRole + 1,
-	};
-
-	struct thumbnail
-	{
-		QIcon icon;
-		QString path;
-		int index = 0;
-	};
-
-	QListWidget* m_grid = nullptr;
-
-	QFutureWatcher<thumbnail> m_icon_loader;
+	bool m_abort_parsing = false;
+	const std::array<int, 1> m_parsing_threads{0};
+	QFutureWatcher<void> m_parsing_watcher;
+	flow_widget* m_flow_widget = nullptr;
 
 	QSize m_icon_size;
-	QIcon m_placeholder;
+	QPixmap m_placeholder;
+};
 
-	int m_scrollbar_value = 0;
+class screenshot_item : public flow_widget_item
+{
+	Q_OBJECT
+
+public:
+	screenshot_item(QWidget* parent);
+	virtual ~screenshot_item();
+
+	QString icon_path;
+	QSize icon_size;
+	QLabel* label{};
+
+private:
+	std::unique_ptr<QThread> m_thread;
+
+Q_SIGNALS:
+	void signal_icon_update(const QPixmap& pixmap);
 };
