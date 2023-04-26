db6bb33f79b9a96e94ed078821b5a44e43bdd1ee
Elad Ashkenazi <elad3356p@gmail.com>
2023-04-26 17:53:10 +0300

    savestates:/sys_spu: Fix join saving

--- rpcs3/Emu/Cell/lv2/sys_spu.cpp
+++ rpcs3/Emu/Cell/lv2/sys_spu.cpp
@@ -1484,7 +1484,7 @@ error_code sys_spu_thread_group_join(ppu_thread& ppu, u32 id, vm::ptr<u32> cause
 			{
 				std::lock_guard lock(group->mutex);
 
-				if (!group->waiter)
+				if (group->waiter != &ppu)
 				{
 					break;
 				}
