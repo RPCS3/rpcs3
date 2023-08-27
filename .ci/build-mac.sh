ff7252102ff7e851f65fecb2eb84309bc1fb5712
nastys <7950891+nastys@users.noreply.github.com>
2023-08-27 13:46:35 +0200

    Update build-mac.sh

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -7,7 +7,7 @@ brew install -f --overwrite nasm ninja git p7zip create-dmg ccache pipenv
 arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 arch -x86_64 /usr/local/bin/brew update
 arch -x86_64 /usr/local/bin/brew install --build-from-source ffmpeg webp gettext
-arch -x86_64 /usr/local/bin/brew install -f --overwrite llvm@16 glew cmake sdl2 vulkan-headers
+arch -x86_64 /usr/local/bin/brew install -f --overwrite llvm@16 glew cmake sdl2 vulkan-headers coreutils
 arch -x86_64 /usr/local/bin/brew link -f llvm@16
 
 # moltenvk based on commit for 1.2.5 release
