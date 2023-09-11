7630a65c3b7822efa66d3887cba654fe833f8672
Eladash <elad3356p@gmail.com>
2023-09-11 12:52:10 +0300

    Rewrite Utilitis/sema.cpp

--- Utilities/sema.cpp
+++ Utilities/sema.cpp
@@ -8,72 +8,84 @@ void semaphore_base::imp_wait()
 	{
 		busy_wait();
 
-		const s32 value = m_value.load();
+		const u32 value = m_value.load();
 
-		if (value > 0 && m_value.compare_and_swap_test(value, value - 1))
+		if (value & c_value_mask && m_value.compare_and_swap_test(value, value - c_value))
 		{
 			return;
 		}
 	}
+
+	bool waits = false;
 
 	while (true)
 	{
 		// Try hard way
-		const s32 value = m_value.atomic_op([](s32& value)
+		const u32 value = m_value.fetch_op([&](u32& value)
 		{
-			// Use sign bit to acknowledge waiter presence
-			if (value && value > smin)
-			{
-				value--;
+			ensure(value != c_waiter_mask); // "semaphore_base: overflow"
 
-				if (value < 0)
+			if (value & c_value_mask)
+			{
+				// Obtain signal
+				value -= c_value;
+
+				if (waits)
 				{
-					// Remove sign bit
-					value -= s32{smin};
+					// Remove waiter
+					value -= c_waiter;
 				}
 			}
-			else
+			else if (!waits)
 			{
-				// Set sign bit
-				value = smin;
+				// Add waiter
+				value += c_waiter;
 			}
-
-			return value;
 		});
 
-		if (value >= 0)
+		if (value & c_value_mask)
 		{
-			// Signal other waiter to wake up or to restore sign bit
-			m_value.notify_one();
-			return;
+			break;
 		}
 
-		m_value.wait(value);
+		m_value.wait(value + (waits ? 0 : c_waiter));
+		waits = true;
 	}
 }
 
-void semaphore_base::imp_post(s32 _old)
+void semaphore_base::imp_post(u32 _old)
 {
-	ensure(_old < 0); // "semaphore_base: overflow"
+	ensure(~_old & c_value_mask); // "semaphore_base: overflow"
 
-	m_value.notify_one();
+	if ((_old & c_waiter_mask) / c_waiter > (_old & c_value_mask) / c_value)
+	{
+		m_value.notify_one();
+	}
 }
 
-bool semaphore_base::try_post(s32 _max)
+bool semaphore_base::try_post(u32 _max)
 {
 	// Conditional increment
-	const s32 value = m_value.fetch_op([&](s32& value)
+	const auto [value, ok] = m_value.fetch_op([&](u32& value)
 	{
-		if (value < _max)
+		if ((value & c_value_mask) <= _max)
 		{
-			value += 1;
+			value += c_value;
+			return true;
 		}
+
+		return false;
 	});
 
-	if (value < 0)
+	if (!ok)
+	{
+		return false;
+	}
+
+	if (value & c_waiter_mask)
 	{
 		imp_post(value);
 	}
 
-	return value < _max;
+	return true;
 }
