dad36c6989192905c45ca86f37fb377e7bab1929
nastys <7950891+nastys@users.noreply.github.com>
2023-10-10 00:16:48 +0200

    macOS CI: force install Python 3.11

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -18,6 +18,7 @@ if [ "$INSTALL_DEPS" = "true" ] ; then
 
   arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   arch -x86_64 /usr/local/bin/brew update
+  arch -x86_64 /usr/local/bin/brew install -f --overwrite python@3.11
   arch -x86_64 /usr/local/bin/brew reinstall -f --build-from-source ffmpeg gnutls freetype jpeg-xl
   arch -x86_64 /usr/local/bin/brew install llvm@16 glew cmake sdl2 vulkan-headers coreutils
   arch -x86_64 /usr/local/bin/brew link -f llvm@16
