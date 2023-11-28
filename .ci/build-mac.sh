56676729052801f190c2ae59ee167c240e57c3c8
nastys <7950891+nastys@users.noreply.github.com>
2023-11-28 18:56:39 +0100

    macOS CI: link ffmpeg after building it from source (#14843)

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -7,6 +7,7 @@ else
   /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 fi
 
+<<<<<<< HEAD
 if [ -z "$INSTALL_DEPS" ]; then
   export INSTALL_DEPS="true"
 fi
@@ -18,7 +19,9 @@ if [ "$INSTALL_DEPS" = "true" ] ; then
 
   arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   arch -x86_64 /usr/local/bin/brew update
-  arch -x86_64 /usr/local/bin/brew install -f --overwrite python@3.11
+  arch -x86_64 /usr/local/bin/brew uninstall -f --ignore-dependencies ffmpeg
+  arch -x86_64 /usr/local/bin/brew install -f --build-from-source ffmpeg
+  arch -x86_64 /usr/local/bin/brew install -f --overwrite python@3.11 || arch -x86_64 /usr/local/bin/brew link --overwrite python@3.11
   arch -x86_64 /usr/local/bin/brew reinstall -f --build-from-source ffmpeg gnutls freetype jpeg-xl
   arch -x86_64 /usr/local/bin/brew install llvm@16 glew cmake sdl2 vulkan-headers coreutils
   arch -x86_64 /usr/local/bin/brew link -f llvm@16
