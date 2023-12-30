15bca3e7c1b8f3f8d8ef23e0bd807bd7e278e43f
Elad Ashkenazi <elad3356p@gmail.com>
2023-12-30 14:35:51 +0200

    Update types.hpp

--- rpcs3/util/types.hpp
+++ rpcs3/util/types.hpp
@@ -59,7 +59,7 @@ using namespace std::literals;
 #if defined(_DEBUG) || defined(_AUDIT)
 #define AUDIT(...) (static_cast<void>(ensure(__VA_ARGS__)))
 #else
-#define AUDIT(...) (static_cast<void>((__VA_ARGS__))
+#define AUDIT(...) (static_cast<void>((__VA_ARGS__)))
 #endif
 
 namespace utils
