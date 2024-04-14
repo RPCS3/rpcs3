682a5710989e90a3c1967a3743217b9c1ecbb170
Florin9doi <Florin9doi@users.noreply.github.com>
2024-04-14 22:46:43 +0300

    Set a unique System Name

--- rpcs3/Emu/system_config.cpp
+++ rpcs3/Emu/system_config.cpp
@@ -10,3 +10,9 @@ bool cfg_root::node_core::enable_tsx_by_default()
 {
 	return utils::has_rtm() && utils::has_mpx() && !utils::has_tsx_force_abort();
 }
+
+std::string cfg_root::node_sys::get_random_system_name()
+{
+	std::srand(std::time(nullptr));
+	return "RPCS3-" + std::to_string(100 + std::rand() % 899);
+}
