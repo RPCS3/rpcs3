0a47b449f8c8b1e5de4e352198b2917c67ceb706
Elad Ashkenazi <18193363+elad335@users.noreply.github.com>
2024-06-12 08:42:03 +0300

    LV2: Optimization for timeout

--- rpcs3/Emu/Cell/lv2/sys_event.cpp
+++ rpcs3/Emu/Cell/lv2/sys_event.cpp
@@ -521,6 +521,12 @@ error_code sys_event_queue_receive(ppu_thread& ppu, u32 equeue_id, vm::ptr<sys_e
 					continue;
 				}
 
+				if (!atomic_storage<ppu_thread*>::load(queue->pq))
+				{
+					// Waiters queue is empty, so the thread must have been signaled
+					break;
+				}
+
 				std::lock_guard lock(queue->mutex);
 
 				if (!queue->unqueue(queue->pq, &ppu))
