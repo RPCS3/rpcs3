ab7ba544421dddbb324c70e64232d3641f2f0858
Elad Ashkenazi <18193363+elad335@users.noreply.github.com>
2024-08-13 09:07:17 +0300

    Fix vm::reservation_update

--- rpcs3/Emu/Memory/vm.cpp
+++ rpcs3/Emu/Memory/vm.cpp
@@ -101,6 +101,13 @@ namespace vm
 		u64 old = -1;
 		const auto cpu = get_current_cpu_thread();
 
+		const bool had_wait = cpu && cpu->state & cpu_flag::wait;
+
+		if (cpu && !had_wait)
+		{
+			cpu->state += cpu_flag::wait;
+		}
+
 		while (true)
 		{
 			const auto [ok, rtime] = try_reservation_update(addr);
@@ -112,15 +119,15 @@ namespace vm
 					reservation_notifier(addr).notify_all();
 				}
 
+				if (cpu && !had_wait && cpu->test_stopped())
+				{
+					//
+				}
+
 				return;
 			}
 
 			old = rtime;
-
-			if (cpu && cpu->test_stopped())
-			{
-				return;
-			}
 		}
 	}
 
