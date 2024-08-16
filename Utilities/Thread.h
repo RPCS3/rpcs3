1bd45651865670051bd8a65eaa9bdec833508cca
Elad Ashkenazi <18193363+elad335@users.noreply.github.com>
2024-08-16 20:06:20 +0300

    Implement thread_ctrl::wait_until()

--- Utilities/Thread.h
+++ Utilities/Thread.h
@@ -276,6 +276,9 @@ public:
 	// Wait once with timeout. Infinite value is -1.
 	static void wait_for(u64 usec, bool alert = true);
 
+	// Wait once with time point, add_time is added to the time point.
+	static void wait_until(u64* wait_time, u64 add_time = 0, u64 min_wait = 0, bool update_to_current_time = true);
+
 	// Waiting with accurate timeout
 	static void wait_for_accurate(u64 usec);
 
