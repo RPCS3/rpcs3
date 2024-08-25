dcd4ffa342546881ebd480925ac1d28d6ac71ebf
RipleyTom <RipleyTom@users.noreply.github.com>
2024-08-25 11:15:49 +0200

    Handle WSAENETRESET error

--- rpcs3/Emu/Cell/lv2/sys_net/sys_net_helpers.cpp
+++ rpcs3/Emu/Cell/lv2/sys_net/sys_net_helpers.cpp
@@ -83,6 +83,13 @@ sys_net_error convert_error(bool is_blocking, int native_error, [[maybe_unused]]
 		ERROR_CASE(ECONNREFUSED);
 		ERROR_CASE(EHOSTDOWN);
 		ERROR_CASE(EHOSTUNREACH);
+#ifdef _WIN32
+		// Windows likes to be special with unique errors
+		case WSAENETRESET:
+			result = SYS_NET_ECONNRESET;
+			name = "WSAENETRESET";
+			break;
+#endif
 	default:
 		fmt::throw_exception("sys_net get_last_error(is_blocking=%d, native_error=%d): Unknown/illegal socket error", is_blocking, native_error);
 	}
