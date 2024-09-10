79c1d844fb5761fce7d0bfd31a598fc3662cfec6
nastys <7950891+nastys@users.noreply.github.com>
2024-09-10 08:49:52 +0200

    Update build-mac-arm64.sh

--- .ci/build-mac-arm64.sh
+++ .ci/build-mac-arm64.sh
@@ -17,18 +17,18 @@ brew_arm64_install_packages() {
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
-export BREW_ARM64_PATH="/opt/homebrew1"
-
-sudo mkdir -p "$BREW_ARM64_PATH"
-sudo chmod 777 "$BREW_ARM64_PATH"
-curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$BREW_ARM64_PATH"
-
-brew_arm64_install_packages 0mq aom aribb24 ca-certificates cjson curl dav1d ffmpeg@5 fontconfig freetype freetype2 gettext glew gmp gnutls lame libbluray libidn2 libnettle libogg libpng librist libsodium libsoxr libtasn libtasn1 libunistring libvmaf libvorbis libvpx libx11 libxau libxcb libxdmcp llvm@18 mbedtls molten-vk nettle opencore-amr openjpeg openssl opus p11-kit pkg-config pkgconfig pzstd rav1e sdl2 snappy speex srt svt-av1 theora vulkan-headers webp x264 x265 xz z3 zeromq zmq zstd
 
 /usr/local/bin/brew update
 /usr/local/bin/brew install -f --overwrite nasm ninja p7zip ccache pipenv #create-dmg
 /usr/local/bin/brew install llvm@18 cmake
 /usr/local/bin/brew link -f llvm@18
+
+export BREW_ARM64_PATH="/opt/homebrew1"
+sudo mkdir -p "$BREW_ARM64_PATH"
+sudo chmod 777 "$BREW_ARM64_PATH"
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$BREW_ARM64_PATH"
+
+brew_arm64_install_packages 0mq aom aribb24 ca-certificates cjson curl dav1d ffmpeg@5 fontconfig freetype freetype2 gettext glew gmp gnutls lame libbluray libidn2 libnettle libogg libpng librist libsodium libsoxr libtasn libtasn1 libunistring libvmaf libvorbis libvpx libx11 libxau libxcb libxdmcp llvm@18 mbedtls molten-vk nettle opencore-amr openjpeg openssl opus p11-kit pkg-config pkgconfig pzstd rav1e sdl2 snappy speex srt svt-av1 theora vulkan-headers webp x264 x265 xz z3 zeromq zmq zstd
 
 # moltenvk based on commit for 1.2.10 release
 wget https://raw.githubusercontent.com/Homebrew/homebrew-core/0d9f25fbd1658e975d00bd0e8cccd20a0c2cb74b/Formula/m/molten-vk.rb
