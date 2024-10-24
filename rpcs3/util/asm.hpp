36da27770a7ea6002c16efa07b231994202568d4
elad335 <18193363+elad335@users.noreply.github.com>
2024-10-24 10:12:50 +0300

    asm: Fix utils::rational_mul optimization

--- rpcs3/util/asm.hpp
+++ rpcs3/util/asm.hpp
@@ -410,10 +410,12 @@ namespace utils
 			return static_cast<T>(value * u64{numerator} / u64{denominator});
 		}
 
+#if is_u128_emulated
 		if constexpr (sizeof(T) <= sizeof(u128) / 2)
 		{
-			return static_cast<T>(value * u128{numerator} / u64{denominator});
+			return static_cast<T>(u128_from_mul(value, numerator) / u64{denominator});
 		}
+#endif
 
 		return static_cast<T>(value / denominator * numerator + (value % denominator) * numerator / denominator);
 	}
