5b61569e81010cb84f1bc0595ad6badfb83f8797
Darkhost1999 <60384196+Darkhost1999@users.noreply.github.com>
2024-11-22 17:23:46 -0600

    New line and update ...savestate.h

--- rpcs3/Emu/RSX/Overlays/HomeMenu/overlay_home_menu_savestate.h
+++ rpcs3/Emu/RSX/Overlays/HomeMenu/overlay_home_menu_savestate.h
@@ -8,11 +8,7 @@ namespace rsx
 	{
 		struct home_menu_savestate : public home_menu_page
 		{
-		public:
 			home_menu_savestate(s16 x, s16 y, u16 width, u16 height, bool use_separators, home_menu_page* parent);
-
-		private:
-			std::vector<std::shared_ptr<home_menu_page>> m_savestate_pages;
 		};
 	}
 }
