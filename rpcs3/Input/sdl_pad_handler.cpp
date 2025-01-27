4953bff1d57b063a1ed5e61a0afaf0f434076e6e
Megamouse <studienricky89@googlemail.com>
2025-01-27 20:32:29 +0100

    Add SDL camera handler

--- rpcs3/Input/sdl_pad_handler.cpp
+++ rpcs3/Input/sdl_pad_handler.cpp
@@ -2,6 +2,7 @@
 
 #include "stdafx.h"
 #include "sdl_pad_handler.h"
+#include "sdl_instance.h"
 #include "Emu/system_utils.hpp"
 #include "Emu/system_config.h"
 #include "Emu/System.h"
@@ -9,117 +10,6 @@
 #include <mutex>
 
 LOG_CHANNEL(sdl_log, "SDL");
-
-struct sdl_instance
-{
-public:
-	sdl_instance() = default;
-	~sdl_instance()
-	{
-		// Only quit SDL once on exit. SDL uses a global state internally...
-		if (m_initialized)
-		{
-			sdl_log.notice("Quitting SDL ...");
-			SDL_Quit();
-		}
-	}
-
-	static sdl_instance& get_instance()
-	{
-		static sdl_instance instance {};
-		return instance;
-	}
-
-	bool initialize()
-	{
-		// Only init SDL once. SDL uses a global state internally...
-		if (m_initialized)
-		{
-			return true;
-		}
-
-		sdl_log.notice("Initializing SDL ...");
-
-		// Set non-dynamic hints before SDL_Init
-		if (!SDL_SetHint(SDL_HINT_JOYSTICK_THREAD, "1"))
-		{
-			sdl_log.error("Could not set SDL_HINT_JOYSTICK_THREAD: %s", SDL_GetError());
-		}
-
-		if (!SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_GAMEPAD))
-		{
-			sdl_log.error("Could not initialize! SDL Error: %s", SDL_GetError());
-			return false;
-		}
-
-		SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
-		SDL_SetLogOutputFunction([](void*, int category, SDL_LogPriority priority, const char* message)
-		{
-			std::string category_name;
-			switch (category)
-			{
-			case SDL_LOG_CATEGORY_APPLICATION:
-				category_name = "app";
-				break;
-			case SDL_LOG_CATEGORY_ERROR:
-				category_name = "error";
-				break;
-			case SDL_LOG_CATEGORY_ASSERT:
-				category_name = "assert";
-				break;
-			case SDL_LOG_CATEGORY_SYSTEM:
-				category_name = "system";
-				break;
-			case SDL_LOG_CATEGORY_AUDIO:
-				category_name = "audio";
-				break;
-			case SDL_LOG_CATEGORY_VIDEO:
-				category_name = "video";
-				break;
-			case SDL_LOG_CATEGORY_RENDER:
-				category_name = "render";
-				break;
-			case SDL_LOG_CATEGORY_INPUT:
-				category_name = "input";
-				break;
-			case SDL_LOG_CATEGORY_TEST:
-				category_name = "test";
-				break;
-			default:
-				category_name = fmt::format("unknown(%d)", category);
-				break;
-			}
-
-			switch (priority)
-			{
-			case SDL_LOG_PRIORITY_VERBOSE:
-			case SDL_LOG_PRIORITY_DEBUG:
-				sdl_log.trace("%s: %s", category_name, message);
-				break;
-			case SDL_LOG_PRIORITY_INFO:
-				sdl_log.notice("%s: %s", category_name, message);
-				break;
-			case SDL_LOG_PRIORITY_WARN:
-				sdl_log.warning("%s: %s", category_name, message);
-				break;
-			case SDL_LOG_PRIORITY_ERROR:
-				sdl_log.error("%s: %s", category_name, message);
-				break;
-			case SDL_LOG_PRIORITY_CRITICAL:
-				sdl_log.error("%s: %s", category_name, message);
-				break;
-			default:
-				break;
-			}
-		}, nullptr);
-
-		m_initialized = true;
-		return true;
-	}
-
-private:
-	bool m_initialized = false;
-};
 
 sdl_pad_handler::sdl_pad_handler() : PadHandlerBase(pad_handler::sdl)
 {
@@ -266,14 +156,7 @@ bool sdl_pad_handler::Init()
 	if (m_is_init)
 		return true;
 
-	bool instance_success;
-
-	Emu.BlockingCallFromMainThread([&instance_success]()
-	{
-		instance_success = sdl_instance::get_instance().initialize();
-	});
-
-	if (!instance_success)
+	if (!sdl_instance::get_instance().initialize())
 		return false;
 
 	if (g_cfg.io.load_sdl_mappings)
