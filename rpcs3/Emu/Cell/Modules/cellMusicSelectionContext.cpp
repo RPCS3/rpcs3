a5d07af89a2b71863879c4023813884724869bb3
Megamouse <studienricky89@googlemail.com>
2025-02-04 01:21:03 +0100

    cellMusic: always select the whole playlist instead of a single track

--- rpcs3/Emu/Cell/Modules/cellMusicSelectionContext.cpp
+++ rpcs3/Emu/Cell/Modules/cellMusicSelectionContext.cpp
@@ -248,6 +248,29 @@ bool music_selection_context::load_playlist()
 	return true;
 }
 
+void music_selection_context::set_track(std::string_view track)
+{
+	if (track.empty()) return;
+
+	if (playlist.empty())
+	{
+		cellMusicSelectionContext.error("No tracks to play... (requested path='%s')", track);
+		return;
+	}
+
+	for (usz i = 0; i < playlist.size(); i++)
+	{
+		cellMusicSelectionContext.error("Comparing track '%s' vs '%s'", track, playlist[i]);
+		if (track.ends_with(playlist[i]))
+		{
+			first_track = current_track = static_cast<u32>(i);
+			return;
+		}
+	}
+
+	cellMusicSelectionContext.error("Track '%s' not found...", track);
+}
+
 u32 music_selection_context::step_track(bool next)
 {
 	if (playlist.empty())
