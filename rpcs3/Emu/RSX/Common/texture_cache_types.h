c4676f041f8ecc320b5614646c4343d5d66a4514
Megamouse <studienricky89@googlemail.com>
2025-02-24 21:56:29 +0100

    move texture_cache_types implementation to cpp

--- rpcs3/Emu/RSX/Common/texture_cache_types.h
+++ rpcs3/Emu/RSX/Common/texture_cache_types.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "Emu/system_config.h"
-
 namespace rsx
 {
 	/**
@@ -131,35 +129,6 @@ namespace rsx
 	private:
 		u32 m_flag_bits = 0;
 
-		void flag_bits_from_cause(enum_type cause)
-		{
-			constexpr std::array s_lookup_table
-			{
-				std::make_pair<enum_type, u32>(enum_type::read, flags::cause_is_read),
-				std::make_pair<enum_type, u32>(enum_type::deferred_read, flags::cause_is_read | flags::cause_is_deferred),
-				std::make_pair<enum_type, u32>(enum_type::write, flags::cause_is_write),
-				std::make_pair<enum_type, u32>(enum_type::deferred_write, flags::cause_is_write | flags::cause_is_deferred),
-				std::make_pair<enum_type, u32>(enum_type::unmap, flags::cause_keeps_fault_range_protection | flags::cause_skips_flush),
-				std::make_pair<enum_type, u32>(enum_type::reprotect, flags::cause_keeps_fault_range_protection),
-				std::make_pair<enum_type, u32>(enum_type::superseded_by_fbo, flags::cause_keeps_fault_range_protection | flags::cause_skips_fbos | flags::cause_skips_flush),
-				std::make_pair<enum_type, u32>(enum_type::committed_as_fbo, flags::cause_skips_fbos),
-			};
-
-			m_flag_bits = 0;
-			for (const auto& entry : s_lookup_table)
-			{
-				if (entry.first == cause)
-				{
-					m_flag_bits = entry.second | flags::cause_is_valid;
-					break;
-				}
-			}
-
-			if (cause == enum_type::superseded_by_fbo &&
-				g_cfg.video.strict_texture_flushing) [[ unlikely ]]
-			{
-				m_flag_bits &= ~flags::cause_skips_flush;
-			}
-		}
+		void flag_bits_from_cause(enum_type cause);
 	};
 }
