cac068dad913d2a54a4c3b299ec1d60ed56212be
DH <dh.rpcs3@gmail.com>
2025-02-28 21:22:32 +0300

    rsx: implement android swapchain

--- rpcs3/Emu/RSX/VK/VulkanAPI.h
+++ rpcs3/Emu/RSX/VK/VulkanAPI.h
@@ -4,6 +4,8 @@
 #define VK_USE_PLATFORM_WIN32_KHR
 #elif defined(__APPLE__)
 #define VK_USE_PLATFORM_MACOS_MVK
+#elif defined(ANDROID)
+#define VK_USE_PLATFORM_ANDROID_KHR
 #elif HAVE_X11
 #define VK_USE_PLATFORM_XLIB_KHR
 #endif
