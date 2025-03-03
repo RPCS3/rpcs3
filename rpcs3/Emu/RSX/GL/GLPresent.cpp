c525568e8df45da7334269166923100a815cba48
Megamouse <studienricky89@googlemail.com>
2025-03-03 18:41:42 +0100

    Use move semantics for present_frame

--- rpcs3/Emu/RSX/GL/GLPresent.cpp
+++ rpcs3/Emu/RSX/GL/GLPresent.cpp
@@ -296,7 +296,7 @@ void GLGSRender::flip(const rsx::display_flip_info_t& info)
 			}
 			else
 			{
-				m_frame->present_frame(sshot_frame, buffer_width * 4, buffer_width, buffer_height, false);
+				m_frame->present_frame(std::move(sshot_frame), buffer_width * 4, buffer_width, buffer_height, false);
 			}
 		}
 
