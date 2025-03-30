3de24014b8fb1f52c07d9c68f52c43aea0787a16
Megamouse <studienricky89@googlemail.com>
2025-03-31 00:46:08 +0200

    cellSaveData: properly initialize SaveDataEntry

--- rpcs3/rpcs3qt/save_manager_dialog.cpp
+++ rpcs3/rpcs3qt/save_manager_dialog.cpp
@@ -270,13 +270,12 @@ std::vector<SaveDataEntry> save_manager_dialog::GetSaveEntries(const std::string
 			return;
 		}
 
-		SaveDataEntry save_entry2;
+		SaveDataEntry save_entry2 {};
 		save_entry2.dirName = psf::get_string(psf, "SAVEDATA_DIRECTORY");
 		save_entry2.listParam = psf::get_string(psf, "SAVEDATA_LIST_PARAM");
 		save_entry2.title = psf::get_string(psf, "TITLE");
 		save_entry2.subtitle = psf::get_string(psf, "SUB_TITLE");
 		save_entry2.details = psf::get_string(psf, "DETAIL");
-		save_entry2.size = 0;
 
 		for (const auto& entry2 : fs::dir(base_dir + entry.name))
 		{
