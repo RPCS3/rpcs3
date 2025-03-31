35bca9ed8a5875f6ebfede7deebe9d4f23af10ff
kd-11 <karokidii@gmail.com>
2025-03-31 12:36:29 +0300

    vk: Add automatic heap management

---
+++ rpcs3/Emu/RSX/VK/VKFrameContextManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <util/types.hpp>
+
+#include <unordered_map>
+
+namespace vk
+{
+	class data_heap;
+
+	namespace frame_context_manager
+	{
+		using managed_heap_snapshot_t = std::unordered_map<const vk::data_heap*, s64>;
+
+		// Submit ring buffer for management
+		void register_ring_buffer(vk::data_heap& heap);
+
+		// Bulk registration
+		void register_ring_buffers(std::initializer_list<std::reference_wrapper<vk::data_heap>> heaps);
+
+		// Capture managed ring buffers snapshot at current time
+		managed_heap_snapshot_t get_heap_snapshot();
+
+		// Synchronize heap with snapshot
+		void restore_snapshot(const managed_heap_snapshot_t& snapshot);
+
+		// Cleanup
+		void reset();
+	}
+}
--- rpcs3/Emu/RSX/VK/VKGSRenderTypes.hpp
+++ rpcs3/Emu/RSX/VK/VKGSRenderTypes.hpp
@@ -2,6 +2,7 @@
 
 #include "vkutils/commands.h"
 #include "vkutils/descriptors.h"
+#include "VKFrameContextManager.h"
 #include "VKResourceManager.h"
 
 #include "Emu/RSX/Common/simple_array.hpp"
@@ -186,19 +187,7 @@ namespace vk
 		u32 present_image = -1;
 		command_buffer_chunk* swap_command_buffer = nullptr;
 
-		// Heap pointers
-		s64 attrib_heap_ptr = 0;
-		s64 vtx_env_heap_ptr = 0;
-		s64 frag_env_heap_ptr = 0;
-		s64 frag_const_heap_ptr = 0;
-		s64 vtx_const_heap_ptr = 0;
-		s64 vtx_layout_heap_ptr = 0;
-		s64 frag_texparam_heap_ptr = 0;
-		s64 index_heap_ptr = 0;
-		s64 texture_upload_heap_ptr = 0;
-		s64 rasterizer_env_heap_ptr = 0;
-		s64 instancing_heap_ptr = 0;
-
+		frame_context_manager::managed_heap_snapshot_t heap_snapshot;
 		u64 last_frame_sync_time = 0;
 
 		// Copy shareable information
@@ -209,17 +198,7 @@ namespace vk
 			descriptor_set.swap(other.descriptor_set);
 			flags = other.flags;
 
-			attrib_heap_ptr = other.attrib_heap_ptr;
-			vtx_env_heap_ptr = other.vtx_env_heap_ptr;
-			frag_env_heap_ptr = other.frag_env_heap_ptr;
-			vtx_layout_heap_ptr = other.vtx_layout_heap_ptr;
-			frag_texparam_heap_ptr = other.frag_texparam_heap_ptr;
-			frag_const_heap_ptr = other.frag_const_heap_ptr;
-			vtx_const_heap_ptr = other.vtx_const_heap_ptr;
-			index_heap_ptr = other.index_heap_ptr;
-			texture_upload_heap_ptr = other.texture_upload_heap_ptr;
-			rasterizer_env_heap_ptr = other.rasterizer_env_heap_ptr;
-			instancing_heap_ptr = other.instancing_heap_ptr;
+			heap_snapshot = other.heap_snapshot;
 		}
 
 		// Exchange storage (non-copyable)
@@ -228,23 +207,9 @@ namespace vk
 			std::swap(buffer_views_to_clean, other.buffer_views_to_clean);
 		}
 
-		void tag_frame_end(
-			s64 attrib_loc, s64 vtxenv_loc, s64 fragenv_loc, s64 vtxlayout_loc,
-			s64 fragtex_loc, s64 fragconst_loc, s64 vtxconst_loc, s64 index_loc,
-			s64 texture_loc, s64 rasterizer_loc, s64 instancing_loc)
+		void tag_frame_end()
 		{
-			attrib_heap_ptr = attrib_loc;
-			vtx_env_heap_ptr = vtxenv_loc;
-			frag_env_heap_ptr = fragenv_loc;
-			vtx_layout_heap_ptr = vtxlayout_loc;
-			frag_texparam_heap_ptr = fragtex_loc;
-			frag_const_heap_ptr = fragconst_loc;
-			vtx_const_heap_ptr = vtxconst_loc;
-			index_heap_ptr = index_loc;
-			texture_upload_heap_ptr = texture_loc;
-			rasterizer_env_heap_ptr = rasterizer_loc;
-			instancing_heap_ptr = instancing_loc;
-
+			heap_snapshot = frame_context_manager::get_heap_snapshot();
 			last_frame_sync_time = rsx::get_shared_tag();
 		}
 
