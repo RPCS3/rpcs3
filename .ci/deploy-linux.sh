2438e975e730789db12b41452458f9ae0565922f
Ani <ani-leo@outlook.com>
2025-04-06 11:46:27 +0200

    appimage: Do not bundle unused Qt6 libraries

--- .ci/deploy-linux.sh
+++ .ci/deploy-linux.sh
@@ -25,6 +25,10 @@ if [ "$DEPLOY_APPIMAGE" = "true" ]; then
     # Remove libvulkan because it causes issues with gamescope
     rm -f ./AppDir/usr/lib/libvulkan.so*
 
+    # Remove unused Qt6 libraries
+    rm -f ./AppDir/usr/lib/libQt6Qml*.so*
+    rm -f ./AppDir/usr/lib/libQt6Quick.so*
+
     # Remove git directory containing local commit history file
     rm -rf ./AppDir/usr/share/rpcs3/git
 
