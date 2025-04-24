85a4c5eb9394983a2df9da5b9ae0373afae8eb9b
Megamouse <studienricky89@googlemail.com>
2025-04-24 18:21:00 +0200

    Remove some obsolete pragmas

--- rpcs3/Emu/Cell/Modules/sys_lv2dbg.cpp
+++ rpcs3/Emu/Cell/Modules/sys_lv2dbg.cpp
@@ -65,38 +65,33 @@ void fmt_class_string<CellLv2DbgError>::format(std::string& out, u64 arg)
 	});
 }
 
-// Temporarily
-#ifndef _MSC_VER
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#endif
-
 error_code sys_dbg_read_ppu_thread_context(u64 id, vm::ptr<sys_dbg_ppu_thread_context_t> ppu_context)
 {
-	sys_lv2dbg.todo("sys_dbg_read_ppu_thread_context()");
+	sys_lv2dbg.todo("sys_dbg_read_ppu_thread_context(id=0x%x, ppu_context=*0x%x)", id, ppu_context);
 	return CELL_OK;
 }
 
 error_code sys_dbg_read_spu_thread_context(u32 id, vm::ptr<sys_dbg_spu_thread_context_t> spu_context)
 {
-	sys_lv2dbg.todo("sys_dbg_read_spu_thread_context()");
+	sys_lv2dbg.todo("sys_dbg_read_spu_thread_context(id=0x%x, spu_context=*0x%x)", id, spu_context);
 	return CELL_OK;
 }
 
 error_code sys_dbg_read_spu_thread_context2(u32 id, vm::ptr<sys_dbg_spu_thread_context2_t> spu_context)
 {
-	sys_lv2dbg.todo("sys_dbg_read_spu_thread_context2()");
+	sys_lv2dbg.todo("sys_dbg_read_spu_thread_context2(id=0x%x, spu_context=*0x%x)", id, spu_context);
 	return CELL_OK;
 }
 
 error_code sys_dbg_set_stacksize_ppu_exception_handler(u32 stacksize)
 {
-	sys_lv2dbg.todo("sys_dbg_set_stacksize_ppu_exception_handler()");
+	sys_lv2dbg.todo("sys_dbg_set_stacksize_ppu_exception_handler(stacksize=0x%x)", stacksize);
 	return CELL_OK;
 }
 
 error_code sys_dbg_initialize_ppu_exception_handler(s32 prio)
 {
-	sys_lv2dbg.todo("sys_dbg_initialize_ppu_exception_handler()");
+	sys_lv2dbg.todo("sys_dbg_initialize_ppu_exception_handler(prio=0x%x)", prio);
 	return CELL_OK;
 }
 
@@ -108,7 +103,7 @@ error_code sys_dbg_finalize_ppu_exception_handler()
 
 error_code sys_dbg_register_ppu_exception_handler(vm::ptr<dbg_exception_handler_t> callback, u64 ctrl_flags)
 {
-	sys_lv2dbg.todo("sys_dbg_register_ppu_exception_handler()");
+	sys_lv2dbg.todo("sys_dbg_register_ppu_exception_handler(callback=*0x%x, ctrl_flags=0x%x)", callback, ctrl_flags);
 	return CELL_OK;
 }
 
@@ -120,170 +115,163 @@ error_code sys_dbg_unregister_ppu_exception_handler()
 
 error_code sys_dbg_signal_to_ppu_exception_handler(u64 flags)
 {
-	sys_lv2dbg.todo("sys_dbg_signal_to_ppu_exception_handler()");
+	sys_lv2dbg.todo("sys_dbg_signal_to_ppu_exception_handler(flags=0x%x)", flags);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_mutex_information(u32 id, vm::ptr<sys_dbg_mutex_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_mutex_information()");
+	sys_lv2dbg.todo("sys_dbg_get_mutex_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_cond_information(u32 id, vm::ptr<sys_dbg_cond_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_cond_information()");
+	sys_lv2dbg.todo("sys_dbg_get_cond_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_rwlock_information(u32 id, vm::ptr<sys_dbg_rwlock_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_rwlock_information()");
+	sys_lv2dbg.todo("sys_dbg_get_rwlock_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_event_queue_information(u32 id, vm::ptr<sys_dbg_event_queue_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_event_queue_information()");
+	sys_lv2dbg.todo("sys_dbg_get_event_queue_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_semaphore_information(u32 id, vm::ptr<sys_dbg_semaphore_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_semaphore_information()");
+	sys_lv2dbg.todo("sys_dbg_get_semaphore_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_lwmutex_information(u32 id, vm::ptr<sys_dbg_lwmutex_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_lwmutex_information()");
+	sys_lv2dbg.todo("sys_dbg_get_lwmutex_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_lwcond_information(u32 id, vm::ptr<sys_dbg_lwcond_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_lwcond_information()");
+	sys_lv2dbg.todo("sys_dbg_get_lwcond_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_event_flag_information(u32 id, vm::ptr<sys_dbg_event_flag_information_t> info)
 {
-	sys_lv2dbg.todo("sys_dbg_get_event_flag_information()");
+	sys_lv2dbg.todo("sys_dbg_get_event_flag_information(id=0x%x, info=*0x%x)", id, info);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_ppu_thread_ids(vm::ptr<u64> ids, vm::ptr<u64> ids_num, vm::ptr<u64> all_ids_num)
 {
-	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_ids()");
+	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_ids(ids=*0x%x, ids_num=*0x%x, all_ids_num=*0x%x)", ids, ids_num, all_ids_num);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_spu_thread_group_ids(vm::ptr<u32> ids, vm::ptr<u64> ids_num, vm::ptr<u64> all_ids_num)
 {
-	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_ids()");
+	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_ids(ids=*0x%x, ids_num=*0x%x, all_ids_num=*0x%x)", ids, ids_num, all_ids_num);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_spu_thread_ids(u32 group_id, vm::ptr<u32> ids, vm::ptr<u64> ids_num, vm::ptr<u64> all_ids_num)
 {
-	sys_lv2dbg.todo("sys_dbg_get_spu_thread_ids()");
+	sys_lv2dbg.todo("sys_dbg_get_spu_thread_ids(group_id=0x%x, ids=*0x%x, ids_num=*0x%x, all_ids_num=*0x%x)", group_id, ids, ids_num, all_ids_num);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_ppu_thread_name(u64 id, vm::ptr<char> name)
 {
-	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_name()");
+	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_name(id=0x%x, name=*0x%x)", id, name);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_spu_thread_name(u32 id, vm::ptr<char> name)
 {
-	sys_lv2dbg.todo("sys_dbg_get_spu_thread_name()");
+	sys_lv2dbg.todo("sys_dbg_get_spu_thread_name(id=0x%x, name=*0x%x)", id, name);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_spu_thread_group_name(u32 id, vm::ptr<char> name)
 {
-	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_name()");
+	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_name(id=0x%x, name=*0x%x)", id, name);
 	return CELL_OK;
 }
 
-
 error_code sys_dbg_get_ppu_thread_status(u64 id, vm::ptr<u32> status)
 {
-	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_status()");
+	sys_lv2dbg.todo("sys_dbg_get_ppu_thread_status(id=0x%x, status=*0x%x)", id, status);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_spu_thread_group_status(u32 id, vm::ptr<u32> status)
 {
-	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_status()");
+	sys_lv2dbg.todo("sys_dbg_get_spu_thread_group_status(id=0x%x, status=*0x%x)", id, status);
 	return CELL_OK;
 }
 
-
 error_code sys_dbg_enable_floating_point_enabled_exception(u64 id, u64 flags, u64 opt1, u64 opt2)
 {
-	sys_lv2dbg.todo("sys_dbg_enable_floating_point_enabled_exception()");
+	sys_lv2dbg.todo("sys_dbg_enable_floating_point_enabled_exception(id=0x%x, flags=0x%x, opt1=0x%x, opt2=0x%x)", id, flags, opt1, opt2);
 	return CELL_OK;
 }
 
 error_code sys_dbg_disable_floating_point_enabled_exception(u64 id, u64 flags, u64 opt1, u64 opt2)
 {
-	sys_lv2dbg.todo("sys_dbg_disable_floating_point_enabled_exception()");
+	sys_lv2dbg.todo("sys_dbg_disable_floating_point_enabled_exception(id=0x%x, flags=0x%x, opt1=0x%x, opt2=0x%x)", id, flags, opt1, opt2);
 	return CELL_OK;
 }
-
 
 error_code sys_dbg_vm_get_page_information(u32 addr, u32 num, vm::ptr<sys_vm_page_information_t> pageinfo)
 {
-	sys_lv2dbg.todo("sys_dbg_vm_get_page_information()");
+	sys_lv2dbg.todo("sys_dbg_vm_get_page_information(addr=0x%x, num=0x%x, pageinfo=*0x%x)", addr, num, pageinfo);
 	return CELL_OK;
 }
 
-
 error_code sys_dbg_set_address_to_dabr(u64 addr, u64 ctrl_flag)
 {
-	sys_lv2dbg.todo("sys_dbg_set_address_to_dabr()");
+	sys_lv2dbg.todo("sys_dbg_set_address_to_dabr(addr=0x%x, spu_context=0x%x)", addr, ctrl_flag);
 	return CELL_OK;
 }
 
 error_code sys_dbg_get_address_from_dabr(vm::ptr<u64> addr, vm::ptr<u64> ctrl_flag)
 {
-	sys_lv2dbg.todo("sys_dbg_get_address_from_dabr()");
+	sys_lv2dbg.todo("sys_dbg_get_address_from_dabr(addr=*0x%x, spu_context=*0x%x)", addr, ctrl_flag);
 	return CELL_OK;
 }
-
 
 error_code sys_dbg_signal_to_coredump_handler(u64 data1, u64 data2, u64 data3)
 {
-	sys_lv2dbg.todo("sys_dbg_signal_to_coredump_handler()");
+	sys_lv2dbg.todo("sys_dbg_signal_to_coredump_handler(data1=0x%x, data2=0x%x, data3=0x%x)", data1, data2, data3);
 	return CELL_OK;
 }
 
-
 error_code sys_dbg_mat_set_condition(u32 addr, u64 cond)
 {
-	sys_lv2dbg.todo("sys_dbg_mat_set_condition()");
+	sys_lv2dbg.todo("sys_dbg_mat_set_condition(addr=0x%x, cond=0x%x)", addr, cond);
 	return CELL_OK;
 }
 
 error_code sys_dbg_mat_get_condition(u32 addr, vm::ptr<u64> condp)
 {
-	sys_lv2dbg.todo("sys_dbg_mat_get_condition()");
+	sys_lv2dbg.todo("sys_dbg_mat_get_condition(addr=0x%x, condp=*0x%x)", addr, condp);
 	return CELL_OK;
 }
 
-
 error_code sys_dbg_get_coredump_params(vm::ptr<s32> param)
 {
-	sys_lv2dbg.todo("sys_dbg_get_coredump_params()");
+	sys_lv2dbg.todo("sys_dbg_get_coredump_params(param=*0x%x)", param);
 	return CELL_OK;
 }
 
 error_code sys_dbg_set_mask_to_ppu_exception_handler(u64 mask, u64 flags)
 {
-	sys_lv2dbg.todo("sys_dbg_set_mask_to_ppu_exception_handler()");
+	sys_lv2dbg.todo("sys_dbg_set_mask_to_ppu_exception_handler(mask=0x%x, flags=0x%x)", mask, flags);
 	return CELL_OK;
 }
 
