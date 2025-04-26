254a0a988483a14c4435a68734661b9c25bd53a6
RipleyTom <RipleyTom@users.noreply.github.com>
2025-04-26 20:17:16 +0200

    Avoid passing -fuse-ld to non-linking step

--- .ci/build-linux-aarch64.sh
+++ .ci/build-linux-aarch64.sh
@@ -22,15 +22,16 @@ else
     export CXX="${CLANGXX_BINARY}"
     export LINKER="${LLD_BINARY}"
 fi
-export CFLAGS="$CFLAGS -fuse-ld=${LINKER}"
-export CXXFLAGS="$CXXFLAGS -fuse-ld=${LINKER}"
+
+export LINKER_FLAG="-fuse-ld=${LINKER}"
 
 cmake ..                                               \
     -DCMAKE_INSTALL_PREFIX=/usr                        \
     -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
     -DUSE_PRECOMPILED_HEADERS=OFF                      \
-    -DCMAKE_C_FLAGS="$CFLAGS"                          \
-    -DCMAKE_CXX_FLAGS="$CFLAGS"                        \
+    -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAG}"          \
+    -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAG}"       \
+    -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAG}"       \
     -DUSE_SYSTEM_CURL=ON                               \
     -DUSE_SDL=ON                                       \
     -DUSE_SYSTEM_SDL=ON                                \
