2ee1f22c50ca143bf8caa8daa07b74b9b7be557b
kd-11 <karokidii@gmail.com>
2025-05-13 03:06:32 +0300

    rsx: Don't trigger surface swap-in/swap-out handlers when replacing self

--- rpcs3/Emu/RSX/VK/VKTextureCache.h
+++ rpcs3/Emu/RSX/VK/VKTextureCache.h
@@ -47,7 +47,10 @@ namespace vk
 
 		void create(u16 w, u16 h, u16 depth, u16 mipmaps, vk::image* image, u32 rsx_pitch, bool managed, u32 gcm_format, bool pack_swap_bytes = false)
 		{
-			if (vram_texture && !managed_texture && get_protection() == utils::protection::no)
+			auto new_texture = static_cast<vk::viewable_image*>(image);
+			ensure(!exists() || !is_managed() || vram_texture == new_texture);
+
+			if (vram_texture != new_texture && !managed_texture && get_protection() == utils::protection::no)
 			{
 				// In-place image swap, still locked. Likely a color buffer that got rebound as depth buffer or vice-versa.
 				vk::as_rtt(vram_texture)->on_swap_out();
@@ -59,8 +62,6 @@ namespace vk
 				}
 			}
 
-			auto new_texture = static_cast<vk::viewable_image*>(image);
-			ensure(!exists() || !is_managed() || vram_texture == new_texture);
 			vram_texture = new_texture;
 
 			ensure(rsx_pitch);
