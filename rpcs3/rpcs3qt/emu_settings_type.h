5ff0d0d4325080e7640d2d209f0c821371f951fe
adas20 <dasar-24@rhodes.edu>
2025-06-16 15:52:03 -0400

    Built with set GameMode Param. Will implement toggle logic

--- rpcs3/rpcs3qt/emu_settings_type.h
+++ rpcs3/rpcs3qt/emu_settings_type.h
@@ -187,6 +187,8 @@ enum class emu_settings_type
 	WindowTitleFormat,
 	PauseDuringHomeMenu,
 
+	//EnableGamemode,				  // Only available if Feral Interactive GameMode is installed
+
 	// Network
 	InternetStatus,
 	DNSAddress,
@@ -388,6 +390,7 @@ inline static const std::map<emu_settings_type, cfg_location> settings_location 
 	{ emu_settings_type::SilenceAllLogs,                  { "Miscellaneous", "Silence All Logs" }},
 	{ emu_settings_type::WindowTitleFormat,               { "Miscellaneous", "Window Title Format" }},
 	{ emu_settings_type::PauseDuringHomeMenu,             { "Miscellaneous", "Pause Emulation During Home Menu" }},
+	//{ emu_settings_type::EnableGamemode,             	  { "Miscellaneous", "Enable GameMode" }}
 
 	// Networking
 	{ emu_settings_type::InternetStatus, { "Net", "Internet enabled"}},
