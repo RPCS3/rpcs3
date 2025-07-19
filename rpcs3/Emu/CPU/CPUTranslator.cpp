8b9c9f677af558ab55934cfa37968a0216500715
Yahfz <asfix1@hotmail.com>
2025-07-19 18:38:31 -0300

    ARL wasn't using FMA/VNNI/GFNI

--- rpcs3/Emu/CPU/CPUTranslator.cpp
+++ rpcs3/Emu/CPU/CPUTranslator.cpp
@@ -135,7 +135,8 @@ void cpu_translator::initialize(llvm::LLVMContext& context, llvm::ExecutionEngin
 		cpu == "bdver4" ||
 		cpu == "znver1" ||
 		cpu == "znver2" ||
-		cpu == "znver3")
+		cpu == "znver3" ||
+		cpu == "arrowlake")
 	{
 		m_use_fma = true;
 		m_use_avx = true;
@@ -157,7 +158,8 @@ void cpu_translator::initialize(llvm::LLVMContext& context, llvm::ExecutionEngin
 		cpu == "cooperlake" ||
 		cpu == "alderlake" ||
 		cpu == "raptorlake" ||
-		cpu == "meteorlake")
+		cpu == "meteorlake" ||
+		cpu == "arrowlake")
 	{
 		m_use_vnni = true;
 	}
@@ -167,7 +169,8 @@ void cpu_translator::initialize(llvm::LLVMContext& context, llvm::ExecutionEngin
 		cpu == "gracemont" ||
 		cpu == "alderlake" ||
 		cpu == "raptorlake" ||
-		cpu == "meteorlake")
+		cpu == "meteorlake" ||
+		cpu == "arrowlake")
 	{
 		m_use_gfni = true;
 	}
