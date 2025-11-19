235ef86e5248759f7d446a456db06def15592fcd
FeTetra <fetetra@protonmail.com>
2025-11-19 18:03:00 -0500

    Fix styling

--- rpcs3/Emu/Cell/PPUThread.cpp
+++ rpcs3/Emu/Cell/PPUThread.cpp
@@ -827,7 +827,8 @@ extern void ppu_register_function_at(u32 addr, u32 size, ppu_intrp_func_t ptr = 
 		return;
 	}
 
-	if (size % 4 != 0) {
+	if (size % 4 != 0) 
+	{
 		if (g_cfg.core.ppu_debug)
 		{
 			ppu_log.error("ppu_register_function_at(0x%x): size not word aligned", addr);
