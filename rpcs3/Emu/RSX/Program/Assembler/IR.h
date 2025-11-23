5d6f8040845cf456a808adfc7712e4e5047843da
kd-11 <karokidii@gmail.com>
2025-11-23 21:11:05 +0300

    rsx/cfg: Add support for multi-slot instructions with literals
    - Also fix pred edge direction bug

--- rpcs3/Emu/RSX/Program/Assembler/IR.h
+++ rpcs3/Emu/RSX/Program/Assembler/IR.h
@@ -33,11 +33,20 @@ namespace rsx::assembler
 
 	struct Instruction
 	{
-		// Raw data. Every instruction is max 128 bits
-		u32 bytecode[4];
+		// Raw data. Every instruction is max 128 bits.
+		// Each instruction can also have 128 bits of literal/embedded data.
+		u32 bytecode[8]{ {} };
+		u32 addr = 0;
 
 		// Decoded
 		u32 opcode = 0;
+		u8  length = 4;  // Length in dwords
+
+		// Padding
+		u8  reserved0 = 0;
+		u16 reserved1 = 0;
+
+		// References
 		std::vector<RegisterRef> srcs;
 		std::vector<RegisterRef> dsts;
 	};
@@ -49,7 +58,7 @@ namespace rsx::assembler
 		ELSE,
 		ENDIF,
 		LOOP,
-		ENDLOOP
+		ENDLOOP,
 	};
 
 	struct FlowEdge
@@ -78,7 +87,7 @@ namespace rsx::assembler
 
 		FlowEdge* insert_pred(BasicBlock* b, EdgeType type = EdgeType::NONE)
 		{
-			FlowEdge e{ .type = type, .from = this, .to = b };
+			FlowEdge e{ .type = type, .from = b, .to = this };
 			pred.push_back(e);
 			return &pred.back();
 		}
