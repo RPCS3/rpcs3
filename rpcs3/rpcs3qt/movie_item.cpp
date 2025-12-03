b9a9c1af07e85d3d542416039accaef3e1020fc8
Megamouse <studienricky89@googlemail.com>
2025-12-03 20:38:33 +0100

    Qt: revert setting UserRole. this doesn't seem to have been the culprit for multiselection

--- rpcs3/rpcs3qt/movie_item.cpp
+++ rpcs3/rpcs3qt/movie_item.cpp
@@ -3,15 +3,12 @@
 
 movie_item::movie_item() : QTableWidgetItem(), movie_item_base()
 {
-	setData(Qt::UserRole, {}); // Set any value to UserRole for proper indexing (e.g. for multiselection)
 }
 
 movie_item::movie_item(const QString& text, int type) : QTableWidgetItem(text, type), movie_item_base()
 {
-	setData(Qt::UserRole, {}); // Set any value to UserRole for proper indexing (e.g. for multiselection)
 }
 
 movie_item::movie_item(const QIcon& icon, const QString& text, int type) : QTableWidgetItem(icon, text, type), movie_item_base()
 {
-	setData(Qt::UserRole, {}); // Set any value to UserRole for proper indexing (e.g. for multiselection)
 }
