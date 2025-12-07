0f1d516d9ae6a1725e4db5553c99463b1aa6d821
Antonino Di Guardo <64427768+digant73@users.noreply.github.com>
2025-12-07 14:38:13 +0100

    Check available and required size when installing a package (#17829)
    Show and check available and required size when installing a package.
    Installation is not allowed, by disabling `Yes` and/or `Install` button,
    if not enough disk space is detected.
    On multi installation dialog box, installation size for each package is
    reported in the list.
    
    fixes #14440
    
    </br>
    </br>
    
    ### Single PKG installation - Not enough space available
    
    ![pr_single_no_space](https://github.com/user-attachments/assets/3fc83d3e-8219-4dca-a4ae-a351f3c4c662)
    
    </br>
    </br>
    
    ### Single PKG installation - Required space available
    
    ![pr_single_space](https://github.com/user-attachments/assets/84b6b4a0-ee4a-430e-b84f-60a754e0a4fa)
    
    </br>
    </br>
    
    ### Multi PKG installation - Not enough space available
    
    ![pr_no_space](https://github.com/user-attachments/assets/56af51a1-cd78-4ad5-818d-7707c0c45de4)
    
    </br>
    </br>
    
    ### Multi PKG installation - Required space available
    
    ![pr_space](https://github.com/user-attachments/assets/43cc66dd-7e98-46de-8dd3-859f8ebb2b05)
    
    ---------
    
    Co-authored-by: Megamouse <studienricky89@googlemail.com>

--- rpcs3/rpcs3qt/pkg_install_dialog.cpp
+++ rpcs3/rpcs3qt/pkg_install_dialog.cpp
@@ -2,6 +2,10 @@
 #include "game_compatibility.h"
 #include "numbered_widget_item.h"
 #include "richtext_item_delegate.h"
+#include "qt_utils.h"
+
+#include "Emu/system_utils.hpp"
+#include "Utilities/File.h"
 
 #include <QDialogButtonBox>
 #include <QPushButton>
@@ -17,6 +21,7 @@ enum Roles
 	TitleRole       = Qt::UserRole + 2,
 	TitleIdRole     = Qt::UserRole + 3,
 	VersionRole     = Qt::UserRole + 4,
+	DataSizeRole    = Qt::UserRole + 5,
 };
 
 pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibility* compat, QWidget* parent)
@@ -89,7 +94,8 @@ pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibil
 		append_comma();
 		accumulated_info += file_info.fileName();
 
-		const QString text = tr("<b>%0</b> (%2)", "Package text").arg(info.title.simplified()).arg(accumulated_info);
+		const QString text = tr("<b>%0</b> (%1) - %2", "Package text").arg(info.title.simplified())
+			.arg(accumulated_info).arg(gui::utils::format_byte_size(info.data_size));
 
 		QListWidgetItem* item = new numbered_widget_item(text, m_dir_list);
 		item->setData(Roles::FullPathRole, info.path);
@@ -97,6 +103,7 @@ pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibil
 		item->setData(Roles::TitleRole, info.title);
 		item->setData(Roles::TitleIdRole, info.title_id);
 		item->setData(Roles::VersionRole, info.version);
+		item->setData(Roles::DataSizeRole, static_cast<qulonglong>(info.data_size));
 		item->setToolTip(tooltip);
 		item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
 		item->setCheckState(Qt::Checked);
@@ -105,6 +112,10 @@ pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibil
 	m_dir_list->sortItems();
 	m_dir_list->setCurrentRow(0);
 	m_dir_list->setMinimumWidth((m_dir_list->sizeHintForColumn(0) * 125) / 100);
+
+	// Create contextual label (updated in connect(m_dir_list, &QListWidget::itemChanged ...))
+	QLabel* installation_info = new QLabel();
+	installation_info->setTextFormat(Qt::RichText); // Support HTML tags
 
 	// Create buttons
 	QDialogButtonBox* buttons = new QDialogButtonBox(QDialogButtonBox::Cancel | QDialogButtonBox::Ok);
@@ -123,19 +134,9 @@ pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibil
 		}
 	});
 
-	connect(m_dir_list, &QListWidget::itemChanged, this, [this, buttons](QListWidgetItem*)
+	connect(m_dir_list, &QListWidget::itemChanged, this, [this, installation_info, buttons](QListWidgetItem*)
 	{
-		bool any_checked = false;
-		for (int i = 0; i < m_dir_list->count(); i++)
-		{
-			if (m_dir_list->item(i)->checkState() == Qt::Checked)
-			{
-				any_checked = true;
-				break;
-			}
-		}
-
-		buttons->button(QDialogButtonBox::Ok)->setEnabled(any_checked);
+		UpdateInfo(installation_info, buttons);
 	});
 
 	QToolButton* move_up = new QToolButton;
@@ -159,11 +160,41 @@ pkg_install_dialog::pkg_install_dialog(const QStringList& paths, game_compatibil
 	vbox->addWidget(description);
 	vbox->addLayout(hbox);
 	vbox->addWidget(m_dir_list);
+	vbox->addWidget(installation_info);
 	vbox->addWidget(buttons);
 
 	setLayout(vbox);
 	setWindowTitle(tr("Batch PKG Installation"));
 	setObjectName("pkg_install_dialog");
+	UpdateInfo(installation_info, buttons); // Just to show and check available and required size
+}
+
+void pkg_install_dialog::UpdateInfo(QLabel* installation_info, QDialogButtonBox* buttons) const
+{
+	u64 data_size = 0;
+	u64 free_space = 0;
+
+	// Retrieve disk space info on data path's drive
+	if (fs::device_stat stat{}; fs::statfs(rpcs3::utils::get_hdd0_game_dir(), stat))
+	{
+		free_space = stat.avail_free;
+	}
+
+	for (int i = 0; i < m_dir_list->count(); i++)
+	{
+		if (m_dir_list->item(i)->checkState() == Qt::Checked)
+		{
+			data_size += m_dir_list->item(i)->data(Roles::DataSizeRole).toULongLong();
+		}
+	}
+
+	installation_info->setText(gui::utils::make_paragraph(
+		tr("Installation path: %0\nAvailable disk space: %1%2\nRequired disk space: %3")
+		.arg(rpcs3::utils::get_hdd0_game_dir())
+		.arg(gui::utils::format_byte_size(free_space))
+		.arg(data_size <= free_space ? QString() : tr(" - <b>NOT ENOUGH SPACE</b>"))
+		.arg(gui::utils::format_byte_size(data_size))));
+	buttons->button(QDialogButtonBox::Ok)->setEnabled(data_size && (data_size <= free_space));
 }
 
 void pkg_install_dialog::MoveItem(int offset) const
