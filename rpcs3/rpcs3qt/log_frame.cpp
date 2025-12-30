24464879c899842ac608ee5119d3b827885b4258
digant73 <diguardo@tiscali.it>
2025-12-30 23:38:58 +0100

    minor cleanup

--- rpcs3/rpcs3qt/log_frame.cpp
+++ rpcs3/rpcs3qt/log_frame.cpp
@@ -293,7 +293,7 @@ void log_frame::CreateAndConnectActions()
 		});
 	});
 
-	m_perform_goto_on_debugger = new QAction(tr("Go-To On The Debugger"), this);
+	m_perform_goto_on_debugger = new QAction(tr("Go to Debugger"), this);
 	connect(m_perform_goto_on_debugger, &QAction::triggered, [this]()
 	{
 		QPlainTextEdit* pte = (m_tabWidget->currentIndex() == 1 ? m_tty : m_log);
@@ -315,7 +315,7 @@ void log_frame::CreateAndConnectActions()
 		memory_viewer_panel::ShowAtPC(static_cast<u32>(pc));
 	});
 
-	m_perform_goto_thread_on_debugger = new QAction(tr("Show Thread On The Debugger"), this);
+	m_perform_goto_thread_on_debugger = new QAction(tr("Show Thread on Debugger"), this);
 	connect(m_perform_goto_thread_on_debugger, &QAction::triggered, [this]()
 	{
 		QPlainTextEdit* pte = (m_tabWidget->currentIndex() == 1 ? m_tty : m_log);
