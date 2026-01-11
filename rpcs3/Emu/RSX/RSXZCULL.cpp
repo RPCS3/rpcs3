03cde837f0934cbbccea40a93d33cb5cb4800f8d
kd-11 <karokidii@gmail.com>
2026-01-11 23:26:49 +0300

    rsx/zcull: Rename write_enabled to surface_active to make the meaning clear

--- rpcs3/Emu/RSX/RSXZCULL.cpp
+++ rpcs3/Emu/RSX/RSXZCULL.cpp
@@ -82,7 +82,7 @@ namespace rsx
 		{
 			// NOTE: Only enable host queries if pixel count is active to save on resources
 			// Can optionally be enabled for either stats enabled or zpass enabled for accuracy
-			const bool data_stream_available = zpass_count_enabled; // write_enabled && (zpass_count_enabled || stats_enabled);
+			const bool data_stream_available = zpass_count_enabled; // surface_active && (zpass_count_enabled || stats_enabled);
 			if (host_queries_active && !data_stream_available)
 			{
 				// Stop
@@ -106,7 +106,7 @@ namespace rsx
 
 		void ZCULL_control::set_status(class ::rsx::thread* ptimer, bool surface_active, bool zpass_active, bool zcull_stats_active, bool flush_queue)
 		{
-			write_enabled = surface_active;
+			surface_active = surface_active;
 			zpass_count_enabled = zpass_active;
 			stats_enabled = zcull_stats_active;
 
@@ -340,14 +340,14 @@ namespace rsx
 				}
 				break;
 			case CELL_GCM_ZCULL_STATS3:
-				value = (value || !write_enabled || !stats_enabled) ? 0 : u16{ umax };
+				value = (value || !surface_active || !stats_enabled) ? 0 : u16{ umax };
 				break;
 			case CELL_GCM_ZCULL_STATS2:
 			case CELL_GCM_ZCULL_STATS1:
 			case CELL_GCM_ZCULL_STATS:
 			default:
 				// Not implemented
-				value = (write_enabled && stats_enabled) ? -1 : 0;
+				value = (surface_active && stats_enabled) ? -1 : 0;
 				break;
 			}
 
