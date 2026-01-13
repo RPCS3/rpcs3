d7b723cd7c5ddcc785076687772a536a4525918e
Daniel López Guimaraes <danielectra@outlook.com>
2026-01-13 23:01:42 +0000

    cellVdec: Add MPEG4 profile 3
    This profile is used in SingStar recordings while in a song on some
    versions.

--- rpcs3/Emu/Cell/Modules/cellVdec.h
+++ rpcs3/Emu/Cell/Modules/cellVdec.h
@@ -390,6 +390,7 @@ struct CellVdecAvcInfo
 enum DIVX_level : u8
 {
 	CELL_VDEC_MPEG4_PROFILE_1                = 1, // SingStar Vol.2 / Vol.3
+	CELL_VDEC_MPEG4_PROFILE_3                = 3, // Used for SingStar recordings
 	CELL_VDEC_MPEG4_PROFILE_4                = 4, // SingStar Pop
 	CELL_VDEC_DIVX_QMOBILE                   = 10,
 	CELL_VDEC_DIVX_MOBILE                    = 11,
