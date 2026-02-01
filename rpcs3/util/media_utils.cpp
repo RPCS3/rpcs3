2fe32f5d3cece20d11617dacbf66cd113f08af5a
Timi Rautamäki <timi.rautamaki@gmail.com>
2026-02-01 20:33:41 +0200

    Switch from FFmpeg av_opt_set_* to swr_alloc_set_opts2
    Fixes:
    E Media: audio_decoder: Failed to set resampler options: Error: -1414549496='Option not found'
    E SYS: 'cellMusicDecodeRead2' failed with 0x8002c107 : CELL_MUSIC_DECODE_ERROR_DECODE_FAILURE [1]
    E SYS: 'cellMusicDecodeRead2' failed with 0x8002c107 : CELL_MUSIC_DECODE_ERROR_DECODE_FAILURE [2]
    E SYS: 'cellMusicDecodeRead2' failed with 0x8002c107 : CELL_MUSIC_DECODE_ERROR_DECODE_FAILURE [3]

--- rpcs3/util/media_utils.cpp
+++ rpcs3/util/media_utils.cpp
@@ -588,15 +588,11 @@ namespace utils
 			const AVChannelLayout dst_channel_layout = AV_CHANNEL_LAYOUT_STEREO;
 			const AVSampleFormat dst_format = AV_SAMPLE_FMT_FLT;
 
-			int set_err = 0;
-			if ((set_err = av_opt_set_int(av.swr, "in_channel_count", stream->codecpar->ch_layout.nb_channels, 0)) ||
-				(set_err = av_opt_set_int(av.swr, "out_channel_count", dst_channels, 0)) ||
-				(set_err = av_opt_set_chlayout(av.swr, "in_channel_layout", &stream->codecpar->ch_layout, 0)) ||
-				(set_err = av_opt_set_chlayout(av.swr, "out_channel_layout", &dst_channel_layout, 0)) ||
-				(set_err = av_opt_set_int(av.swr, "in_sample_rate", stream->codecpar->sample_rate, 0)) ||
-				(set_err = av_opt_set_int(av.swr, "out_sample_rate", sample_rate, 0)) ||
-				(set_err = av_opt_set_sample_fmt(av.swr, "in_sample_fmt", static_cast<AVSampleFormat>(stream->codecpar->format), 0)) ||
-				(set_err = av_opt_set_sample_fmt(av.swr, "out_sample_fmt", dst_format, 0)))
+			int set_err = swr_alloc_set_opts2(&av.swr, &dst_channel_layout, dst_format,
+				sample_rate, &stream->codecpar->ch_layout,
+				static_cast<AVSampleFormat>(stream->codecpar->format),
+				stream->codecpar->sample_rate, 0, nullptr);
+			if (set_err < 0)
 			{
 				media_log.error("audio_decoder: Failed to set resampler options: Error: %d='%s'", set_err, av_error_to_string(set_err));
 				has_error = true;
