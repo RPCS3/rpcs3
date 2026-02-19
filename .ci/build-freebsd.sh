1c8ac692e9c39caa039b5f81cc4e58310b335283
Niram7777 <marin.baron.pro@gmail.com>
2026-02-19 13:24:08 +0100

    CI FreeBSD linker-devel
    [864/1032] Linking CXX static library rpcs3/Emu/librpcs3_emu.a
      warning: 'cache_utils.cpp.o': Unknown attribute kind (102) (Producer: 'LLVM22.0.0git' Reader: 'LLVM 19.1.7')

--- .ci/build-freebsd.sh
+++ .ci/build-freebsd.sh
@@ -6,7 +6,7 @@
 git config --global --add safe.directory .
 git submodule -q update --init --depth 1 $(awk '/path/ && !/llvm/ && !/opencv/ && !/libpng/ && !/libsdl-org/ && !/curl/ && !/zlib/ && !/libusb/ && !/feralinteractive/ { print $3 }' .gitmodules)
 
-CONFIGURE_ARGS="
+=(
 	-DWITH_LLVM=ON
 	-DUSE_SDL=OFF
 	-DUSE_PRECOMPILED_HEADERS=OFF
@@ -16,13 +16,13 @@ CONFIGURE_ARGS="
 	-DUSE_SYSTEM_LIBPNG=ON
  	-DUSE_SYSTEM_LIBUSB=ON
 	-DUSE_SYSTEM_OPENCV=ON
-"
-
 # base Clang workaround (missing clang-scan-deps)
-CONFIGURE_ARGS="$CONFIGURE_ARGS -DCMAKE_CXX_SCAN_FOR_MODULES=OFF"
+	-DCMAKE_CXX_SCAN_FOR_MODULES=OFF
+	-DCMAKE_LINKER_TYPE=lld-devel
+)
 
 # shellcheck disable=SC2086
-cmake -B build -G Ninja $CONFIGURE_ARGS
+cmake -B build -G Ninja ${CONFIGURE_ARGS[@]}
 cmake --build build
 
 ccache --show-stats
