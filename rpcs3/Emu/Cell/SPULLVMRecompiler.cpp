e1a578d0f15a0f916fff526df85b417852292a59
Elad <18193363+elad335@users.noreply.github.com>
2026-02-26 15:50:37 +0200

    SPU LLVM: Expand SPU Profiling to blocks

--- rpcs3/Emu/Cell/SPULLVMRecompiler.cpp
+++ rpcs3/Emu/Cell/SPULLVMRecompiler.cpp
@@ -2160,6 +2160,14 @@ public:
 					}
 				}
 
+				if (bb.preds.size() >= 2)
+				{
+					if (g_cfg.core.spu_prof || g_cfg.core.spu_debug)
+					{
+						m_ir->CreateStore(m_ir->getInt64((m_hash_start & -65536) | (baddr >> 2)), spu_ptr(&spu_thread::block_hash));
+					}
+				}
+
 				// State check at the beginning of the chunk
 				if (need_check || (bi == 0 && g_cfg.core.spu_block_size != spu_block_size_type::safe))
 				{
