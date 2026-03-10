146b071fb8680c05cc4b4c9c07d27db8c6ff1ba6
kd-11 <karokidii@gmail.com>
2026-03-11 00:13:32 +0300

    overlays/home: Add icons repository wrapper

---
+++ rpcs3/Emu/RSX/Overlays/HomeMenu/overlay_home_icons.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "overlay_home_icons.h"
+
+#include <unordered_map>
+
+namespace rsx::overlays::home_menu
+{
+	std::unordered_map<fa_icon, std::unique_ptr<image_info>> g_icons_cache;
+
+	static const char* fa_icon_to_filename(fa_icon icon)
+	{
+		switch (icon)
+		{
+		case fa_icon::home:
+			return "home.png";
+		case fa_icon::settings:
+			return "settings.png";
+		case fa_icon::back:
+			return "circle-left-solid.png";
+		case fa_icon::floppy:
+			return "floppy-disk-solid.png";
+		case fa_icon::maximize:
+			return "maximize-solid.png";
+		case fa_icon::play:
+			return "play-button-arrowhead.png";
+		case fa_icon::poweroff:
+			return "power-off-solid.png";
+		case fa_icon::restart:
+			return "rotate-left-solid.png";
+		case fa_icon::screenshot:
+			return "screenshot.png";
+		case fa_icon::video_camera:
+			return "video-camera.png";
+		}
+	}
+
+	void load_icon(fa_icon icon)
+	{
+		if (!g_icons_cache.empty())
+		{
+			return;
+		}
+
+		const std::string image_path = fmt::format("%s/Icons/ui/home/%s", fs::get_config_dir(), fa_icon_to_filename(icon));
+		g_icons_cache[icon] = std::make_unique<image_info>(image_path);
+	}
+
+	const image_info* get_icon(fa_icon icon)
+	{
+		auto found = g_icons_cache.find(icon);
+		if (found != g_icons_cache.end())
+		{
+			return found->second.get();
+		}
+
+		load_icon(icon);
+		return g_icons_cache.at(icon).get();
+	}
+}
