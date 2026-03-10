3194bd3fb42c6a9ca4ef88d4a2295c8cb326e6a5
kd-11 <karokidii@gmail.com>
2026-03-11 00:13:32 +0300

    overlays/home: Add icons repository wrapper

---
+++ rpcs3/Emu/RSX/Overlays/HomeMenu/overlay_home_icons.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Emu/RSX/Overlays/overlays.h"
+#include "Emu/RSX/Overlays/overlay_controls.h"
+
+namespace rsx::overlays::home_menu
+{
+	enum class fa_icon
+	{
+		home = 0,
+		settings,
+		back,
+		floppy,
+		maximize,
+		play,
+		poweroff,
+		restart,
+		screenshot,
+		video_camera
+	};
+
+	const image_info* get_icon(fa_icon icon);
+}
