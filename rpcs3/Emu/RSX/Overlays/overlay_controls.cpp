852317a071cdb2f5fa6e0b58d84d99ef0f8d673f
kd-11 <karokidii@gmail.com>
2026-03-19 23:24:20 +0300

    overlays: Shrink the shader notification popup

--- rpcs3/Emu/RSX/Overlays/overlay_controls.cpp
+++ rpcs3/Emu/RSX/Overlays/overlay_controls.cpp
@@ -407,6 +407,12 @@ namespace rsx
 
 		void overlay_element::set_text(localized_string_id id)
 		{
+			if (id == localized_string_id::INVALID)
+			{
+				set_text("");
+				return;
+			}
+
 			set_unicode_text(get_localized_u32string(id));
 		}
 
