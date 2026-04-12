1cc5e3841363fc1a88b0081567b8ec0266be1826
luci4 <142809264+l00sy4@users.noreply.github.com>
2026-04-12 20:42:18 +0100

    Add context parameter to get_backtrace
    Added Windows-specific backtrace functionality.

--- Utilities/stack_trace.h
+++ Utilities/stack_trace.h
@@ -2,6 +2,11 @@
 #include <util/types.hpp>
 #include <util/logs.hpp>
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
 namespace utils
 {
 	namespace stack_trace
@@ -30,7 +35,12 @@ namespace utils
 		};
 	}
 
+#ifdef _WIN32
+	std::vector<void*> get_backtrace(int max_depth = 255, PCONTEXT ctx = nullptr);
+#else
 	std::vector<void*> get_backtrace(int max_depth = 255);
+#endif
+
 	std::vector<std::string> get_backtrace_symbols(const std::vector<void*>& stack);
 
 	FORCE_INLINE void print_trace(stack_trace::Logger auto& logger, int max_depth = 255)
