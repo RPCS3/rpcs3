664cbbddab396e92b1e7446e886d609b3045c280
Joshua de Reeper <joshua@dereeper.co.nz>
2026-05-17 23:37:19 +0200

    Dialog List Displaying

--- rpcs3/Emu/RSX/Overlays/USBDevices/overlay_skylander_dialog.h
+++ rpcs3/Emu/RSX/Overlays/USBDevices/overlay_skylander_dialog.h
@@ -10,16 +10,15 @@ namespace rsx
 		struct skylander_dialog : public user_interface
 		{
 		private:
+			void reload();
+
 			std::unique_ptr<overlay_element> m_dim_background;
 			std::unique_ptr<list_view> m_list;
 			std::unique_ptr<label> m_description;
-			std::unique_ptr<image_button> m_show_hidden_trophies_button;
 
 			animation_color_interpolate fade_animation;
 
 			atomic_t<bool> m_list_dirty{true};
-			bool m_show_hidden_trophies = false;
-			bool m_show_hidden_trophies_last = false;
 
 		public:
 			skylander_dialog();
