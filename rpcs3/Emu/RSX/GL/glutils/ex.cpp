00c57be1edc510ff362c35696c685b0d93950877
kd-11 <karokidii@gmail.com>
2026-06-14 21:19:06 +0300

    gl: Fix linux build

--- rpcs3/Emu/RSX/GL/glutils/ex.cpp
+++ rpcs3/Emu/RSX/GL/glutils/ex.cpp
@@ -5,8 +5,8 @@ namespace gl::ex
 {
 	void glNamedBufferStorageEX(GLuint buffer, GLenum target, GLsizeiptr size, const void* data, GLbitfield flags)
 	{
-		GLint restore = GL_NONE;
-		glGetIntegerv(target, &restore);
+		GLuint restore = GL_NONE;
+		glGetIntegerv(target, utils::bless<GLint>(&restore));
 
 		glBindBuffer(target, buffer);
 		glBufferStorage(target, size, data, flags);
