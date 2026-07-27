aca86a1f5deab1ac7445c21ddb5f69158dbe0e82
kd-11 <karokidii@gmail.com>
2026-07-27 18:09:10 +0300

    rsx: Fix potential section leak when discarding locked framebuffer region

--- rpcs3/Emu/RSX/Common/texture_cache.h
+++ rpcs3/Emu/RSX/Common/texture_cache.h
@@ -1816,14 +1816,11 @@ namespace rsx
 		template <typename ...Args>
 		void discard_framebuffer_memory_region(commandbuffer_type& /*cmd*/, const address_range32& rsx_range, Args&&... /*extras*/)
 		{
-			if (g_cfg.video.write_color_buffers || g_cfg.video.write_depth_buffer)
+			auto* region_ptr = find_cached_texture(rsx_range, { .gcm_format = RSX_GCM_FORMAT_IGNORED }, false, false, false);
+			if (region_ptr && region_ptr->is_locked() && region_ptr->get_context() == texture_upload_context::framebuffer_storage)
 			{
-				auto* region_ptr = find_cached_texture(rsx_range, { .gcm_format = RSX_GCM_FORMAT_IGNORED }, false, false, false);
-				if (region_ptr && region_ptr->is_locked() && region_ptr->get_context() == texture_upload_context::framebuffer_storage)
-				{
-					ensure(region_ptr->get_protection() == utils::protection::no);
-					region_ptr->discard(false);
-				}
+				ensure(region_ptr->get_protection() == utils::protection::no);
+				region_ptr->discard(false);
 			}
 		}
 
