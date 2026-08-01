be24da68afa0c71c8d36e698b7abe528da394f5f
digant73 <diguardo@tiscali.it>
2026-08-01 14:12:28 +0200

    minor cleanup

--- rpcs3/Emu/RSX/VK/VKFragmentProgram.cpp
+++ rpcs3/Emu/RSX/VK/VKFragmentProgram.cpp
@@ -464,7 +464,6 @@ void VKFragmentDecompilerThread::insertMainStart(std::stringstream & OS)
 
 	if (m_prog.two_sided_lighting)
 	{
-		// diff_color0/spec_color0 are COL0/COL1 (o[1]/o[2], front), diff_color1/spec_color1 are BFC0/BFC1 (o[3]/o[4], back)
 		if (properties.in_register_mask & in_diff_color)
 			OS << "	vec4 diff_color = gl_FrontFacing ? diff_color0 : diff_color1;\n";
 
