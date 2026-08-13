e300d7afaaa79d3b6739d5f4f7b82534087333ba
Ani <ani-leo@outlook.com>
2026-08-13 23:40:29 +0200

    Revert "[SPU LLVM] Decrease `SHUFB`'s constant generation target requirements"
    This reverts commit 26e37d8c8ca758fc81dda57521ed8f9a68d042fd.

--- rpcs3/Emu/Cell/SPULLVMRecompiler.cpp
+++ rpcs3/Emu/Cell/SPULLVMRecompiler.cpp
@@ -7639,7 +7639,7 @@ public:
 		{
 			idx_consts = eval(splat<u8[16]>(0));
 		}
-		else if (m_use_gfni)
+		else if (m_use_avx512_icl)
 		{
 			const auto gfni = gf2p8affineqb(c, build<u8[16]>(0x40, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x40, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20), 0x7f);
 			idx_consts = eval(select(noncast<s8[16]>(gfni) >= 0, splat<u8[16]>(0), gfni));
