c3835f300159e74155996d1568c3121c147b13a1
digant73 <diguardo@tiscali.it>
2026-08-15 15:36:50 +0200

    Enrich game list title

--- rpcs3/rpcs3qt/game_list_frame.cpp
+++ rpcs3/rpcs3qt/game_list_frame.cpp
@@ -346,6 +346,40 @@ bool game_list_frame::IsEntryVisible(const game_info& game, bool search_fallback
 	return is_visible && matches_category() && SearchMatchesApp(QString::fromStdString(game->info.name), serial, search_fallback);
 }
 
+// Show the amount of listed games (and the Disc/HDD/Other share) in the dock title
+void game_list_frame::UpdateWindowTitle(const std::vector<game_info>& matching_apps)
+{
+	usz disc_games = 0;
+	usz hdd_games = 0;
+	usz other_games = 0;
+
+	for (const auto& app : matching_apps)
+	{
+		if (!app) continue;
+
+		const QString category = QString::fromStdString(app->info.category);
+
+		if (category == cat::cat_disc_game)
+		{
+			disc_games++;
+		}
+		else if (category == cat::cat_hdd_game)
+		{
+			hdd_games++;
+		}
+		else
+		{
+			other_games++;
+		}
+	}
+
+	setWindowTitle(tr("Game List (%0) - Disc: %1 | HDD: %2 | Other: %3")
+		.arg(matching_apps.size())
+		.arg(disc_games)
+		.arg(hdd_games)
+		.arg(other_games));
+}
+
 void game_list_frame::push_path(const std::string& path, std::vector<std::string>& legit_paths)
 {
 	{
@@ -510,6 +544,8 @@ void game_list_frame::Refresh(const bool from_drive, const std::vector<std::stri
 			}
 		}
 	}
+
+	UpdateWindowTitle(matching_apps);
 
 	if (m_is_list_layout)
 	{
