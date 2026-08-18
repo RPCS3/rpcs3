91cd82a0c2e8a82380a558e63415a10a021a14b3
kd-11 <karokidii@gmail.com>
2026-08-18 03:19:04 +0300

    gl: Extend copy image API to allow explicit 3D offsets

--- rpcs3/Emu/RSX/GL/glutils/blitter.h
+++ rpcs3/Emu/RSX/GL/glutils/blitter.h
@@ -38,11 +38,26 @@ namespace gl
 			gl::command_context& cmd,
 			const texture* src,
 			texture* dst,
+			const coord3i& src_rect,         //<- Note, negative w/h/d is allowed for flipping
+			const coord3i& dst_rect,         //<- Note, negative w/h/d is allowed for flipping
+			bool linear_interpolation,
+			const rsx::typeless_xfer& xfer_info,
+			const rsx::image_copy_subresource_layers& mip_layers = {});
+
+		void scale_image(
+			gl::command_context& cmd,
+			const texture* src,
+			texture* dst,
 			areai src_rect,
 			areai dst_rect,
 			bool linear_interpolation,
 			const rsx::typeless_xfer& xfer_info,
-			const rsx::image_copy_subresource_layers& mip_layers = {});
+			const rsx::image_copy_subresource_layers& mip_layers = {})
+		{
+			const auto src_region = coord3i{ src_rect.x1, src_rect.y1, 0, src_rect.x2 - src_rect.x1, src_rect.y2 - src_rect.y1, 1 };
+			const auto dst_region = coord3i{ dst_rect.x1, dst_rect.y1, 0, dst_rect.x2 - dst_rect.x1, dst_rect.y2 - dst_rect.y1, 1 };
+			scale_image(cmd, src, dst, src_region, dst_region, linear_interpolation, xfer_info, mip_layers);
+		}
 
 		void copy_image(
 			gl::command_context& cmd,
