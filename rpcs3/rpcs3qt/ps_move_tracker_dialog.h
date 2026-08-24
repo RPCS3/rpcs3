8a3061d175d75a2ad833ad2ebee089f1d402beb9
Megamouse <studienricky89@googlemail.com>
2026-08-24 20:10:11 +0200

    Qt: add ps move default brightness
    The RGB values will not be at 1% anymore, but the default brightness.

--- rpcs3/rpcs3qt/ps_move_tracker_dialog.h
+++ rpcs3/rpcs3qt/ps_move_tracker_dialog.h
@@ -29,6 +29,7 @@ private:
 	bool eventFilter(QObject* object, QEvent* event) override;
 
 	void update_color(bool update_rgb_sliders, bool update_hue_slider);
+	void update_default_brightness(bool update_slider);
 	void update_hue(bool update_hue_slider, bool update_rgb_sliders, bool no_signal);
 	void update_hue_threshold(bool update_slider = false);
 	void update_saturation_threshold(bool update_slider = false);
