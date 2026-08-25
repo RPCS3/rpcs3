b57ad5047f331355d64901b47fea7135c39c3154
Elad <18193363+elad335@users.noreply.github.com>
2026-08-25 22:45:28 +0300

    sys_condolw_write logging

--- rpcs3/Emu/Cell/lv2/sys_console.h
+++ rpcs3/Emu/Cell/lv2/sys_console.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "Emu/Memory/vm_ptr.h"
+#include "Emu/Cell/ErrorCodes.h"
 
 // SysCalls
+class ppu_thread;
 
-error_code sys_console_write(vm::cptr<char> buf, u32 len);
+error_code sys_console_write(ppu_thread& ppu, vm::cptr<char> buf, u32 len);
 constexpr auto sys_console_write2 = sys_console_write;
