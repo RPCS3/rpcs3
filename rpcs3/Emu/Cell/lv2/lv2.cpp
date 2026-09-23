b4b6feae6d9d8903bf3ace672414f28713bcd815
Elad <18193363+elad335@users.noreply.github.com>
2026-09-23 18:12:34 +0300

    LV2: Increase HW PPU Thread Signal Delay
    Co-Authored-By: Antonino Di Guardo <64427768+digant73@users.noreply.github.com>

--- rpcs3/Emu/Cell/lv2/lv2.cpp
+++ rpcs3/Emu/Cell/lv2/lv2.cpp
@@ -1871,7 +1871,11 @@ bool lv2_obj::awake_unlocked(cpu_thread* cpu, s32 prio)
 			{
 				if (!current_ppu->state.test_and_set(cpu_flag::yield) || current_ppu->hw_sleep_time != 0)
 				{
-					current_ppu->hw_sleep_time += (is_create_thread ? 51 : 35);
+#ifdef _WIN32
+					current_ppu->hw_sleep_time += (is_create_thread ? 600 : 200);
+#else
+					current_ppu->hw_sleep_time += 100;
+#endif
 				}
 				else
 				{
